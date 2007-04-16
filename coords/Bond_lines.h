64f5582ebeeef4239eaffdfbc6d3ca086a27164e
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2007-04-16 15:37:53 +0000

    Another traunche of Copyright notice corrections.

--- coords/Bond_lines.h
+++ coords/Bond_lines.h
@@ -1,7 +1,8 @@
 // -*-c++-*-
 /* coords/Bond_lines.h
  * 
- * Copyright 2002, 2003, 2004, 2005 by Paul Emsley, The University of York
+ * Copyright 2002, 2003, 2004, 2005 by The University of York
+ * Author: Paul Emsley
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +16,8 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc.,  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
  */
 
 //#include "mmdb_manager.h" 
--- coot-surface/coot-surface.cc
+++ coot-surface/coot-surface.cc
@@ -1,6 +1,7 @@
-/* src/main.cc
+/* coot-surface/coot-surface.cc
  * 
- * Copyright 2001, 2002, 2003, 2004, 2006 by Paul Emsley, The University of York
+ * Copyright 2005 The University of York
+ * Author: Martin Noble, Jan Gruber, Paul Emsley
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
