398934a8ac407c53c00d2d20c9079884acba7f4c
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2007-05-01 18:24:11 +0000

    Do the SCM_STRING_CHARS substitution depending on the version of guile.

--- trunk/src/post-process-coot-wrap-guile.sh
+++ trunk/src/post-process-coot-wrap-guile.sh
@@ -1,10 +1,28 @@
 
-if [ $# != 2 ] ; then
-   echo must provide 2 file arguments
+if [ $# != 3 ] ; then
+   echo must provide guile version and 2 file arguments
+   echo we got: $*
    exit 2
 fi
 
-pre="$1"
-post="$2"
+pre="$2"
+post="$3"
 
-sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' $pre > $post
+guile_version=$1
+
+case $guile_version in
+
+   1.6*) 
+    echo cp $pre $post
+    cp $pre $post
+    ;; 
+   
+   1.8*)
+   echo sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' $pre $post
+   sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' $pre > $post
+   ;; 
+
+esac
+
+
+
