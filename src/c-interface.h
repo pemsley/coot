c21e4a71b93c56cc0bc7a248575c6a74dc3918a3
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2007-06-08 07:00:58 +0000

    Add scripting level function change_chain_id().

--- src/c-interface.h
+++ src/c-interface.h
@@ -2098,6 +2098,9 @@ void change_chain_ids_mol_option_menu_item_activate(GtkWidget *item,
 void change_chain_ids_chain_menu_item_activate(GtkWidget *item,
 					       GtkPositionType pos);
 
+void  change_chain_id(int imol, const char *from_chain_id, const char *to_chain_id, 
+		      short int use_res_range_flag, int from_resno, int to_resno);
+
 /*  ----------------------------------------------------------------------- */
 /*                  scripting                                               */
 /*  ----------------------------------------------------------------------- */
