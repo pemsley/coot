5acf56bcf9d8ffdd4319cb56a60a31a99782b265
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2007-07-13 16:26:31 +0000

    No sensible HOF cif file - remove the test for it.

--- greg-tests/02-cif.scm
+++ greg-tests/02-cif.scm
@@ -1,4 +1,7 @@
 
+;; NOTE: hof.fcf has  _refln_A_calc and _refln_B_calc, not fcalc and phase.
+;; This will not work.  Why is it here?
+;; 
 (define hof-fcf "data/shelx/egs/hof.fcf")
 (define hof-res "data/shelx/egs/HOF.RES")
 
@@ -21,28 +24,28 @@
 	   (throw 'untested)))))
 
 
-(greg-testcase "read shelx fcf" #t 
-   (lambda ()
+;(greg-testcase "read shelx fcf" #t 
+;   (lambda ()
 
-     ;; close imol-hof-res too if it was set properly.
-     (if (not (string? hof-fcf))
-	 (begin
-	   (format #t "hof-fcf not defined - skipping test~%")
-	   (throw 'untested))
-	 (begin
-	   (if (not (file-exists? hof-fcf))
-	       (begin
-		 (format #t "~s does not exist - skipping test~%" hof-fcf)
-		 (throw 'untested))
+;     ;; close imol-hof-res too if it was set properly.
+;     (if (not (string? hof-fcf))
+;	 (begin
+;	   (format #t "hof-fcf not defined - skipping test~%")
+;	   (throw 'untested))
+;	 (begin
+;	   (if (not (file-exists? hof-fcf))
+;	       (begin
+;		 (format #t "~s does not exist - skipping test~%" hof-fcf)
+;		 (throw 'untested))
 	       
-	       (let ((imol (handle-shelx-fcf-file hof-fcf)))
-		 (let ((success (valid-map-molecule? imol)))
-		   (if success
-		       (begin
-			 (rotate-y-scene 1000 0.1)
-			 (if success
-			     (close-molecule imol))))
-		   (if (valid-model-molecule? imol-hof-res)
-		       (close-molecule imol-hof-res))
-		   success)))))))
+;	       (let ((imol (handle-shelx-fcf-file hof-fcf)))
+;		 (let ((success (valid-map-molecule? imol)))
+;		   (if success
+;		       (begin
+;			 (rotate-y-scene 1000 0.1)
+;			 (if success
+;			     (close-molecule imol))))
+;		   (if (valid-model-molecule? imol-hof-res)
+;		       (close-molecule imol-hof-res))
+;		   success)))))))
 
