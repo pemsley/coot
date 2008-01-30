8331aaa9903a7536fa9e97a785d27da472864bc4
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-01-30 15:53:27 +0000

    Freetype2 building updates.

--- src/molecule-class-info.cc
+++ src/molecule-class-info.cc
@@ -3,6 +3,7 @@
  * 
  * Copyright 2002, 2003, 2004, 2005, 2006, 2007 by The University of York
  * Copyright 2007 by Paul Emsley
+ * Copyright 2007, 2008 by The University of Oxford
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
