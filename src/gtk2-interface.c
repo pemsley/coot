e5340c735d5dc5f935255dcae338b2ebe5f579e5
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-03-05 08:42:14 +0000

    update splash screen.

--- src/gtk2-interface.c
+++ src/gtk2-interface.c
@@ -10981,7 +10981,7 @@ create_splash_screen_window (void)
   gtk_window_set_position (GTK_WINDOW (splash_screen_window), GTK_WIN_POS_CENTER);
   gtk_window_set_type_hint (GTK_WINDOW (splash_screen_window), GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
 
-  image6025 = create_pixmap (splash_screen_window, "coot-0.4.xpm");
+  image6025 = create_pixmap (splash_screen_window, "coot-0.5-pre.xpm");
   gtk_widget_show (image6025);
   gtk_container_add (GTK_CONTAINER (splash_screen_window), image6025);
 
