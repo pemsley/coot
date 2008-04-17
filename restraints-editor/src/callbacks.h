0a49ade4f1a0559c527748e6e176462ebef13a60
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-04-17 20:09:26 +0000

    Add the restraints editor (0.5) to the repo.

---
+++ restraints-editor/src/callbacks.h
@@ -0,0 +1,22 @@
+#include <gtk/gtk.h>
+
+
+void
+on_restraint_editor_add_restraint_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_restraints_editor_close_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_restraints_editor_apply_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_restraint_editor_delete_restraint_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data);
