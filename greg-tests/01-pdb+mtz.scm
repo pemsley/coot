d1bcbbfbe9b1feff7fc33611585920ed9ccc7a42
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-05-22 16:15:02 +0000

    New glycosidic bond test (new test pdb file).

--- greg-tests/01-pdb+mtz.scm
+++ greg-tests/01-pdb+mtz.scm
@@ -906,24 +906,29 @@
 (greg-testcase "Make a glycosidic linkage" #t 
    (lambda ()
 
-     (let ((imol (read-pdb (append-dir-file greg-data-dir "multi-carbo-coot-1.pdb"))))
-       (let ((atom-1 (get-atom imol "A" 1 " O4 "))
-	     (atom-2 (get-atom imol "A" 2 " C1 ")))
-	 
-	 (format #t "bond-length: ~s: ~%"
-		 (bond-length (list-ref atom-1 2) (list-ref atom-2 2)))
+     (let ((imol (greg-pdb "multi-carbo-coot-2.pdb")))
 
-	 (let ((s (dragged-refinement-steps-per-frame)))
-	   (set-dragged-refinement-steps-per-frame 300)
-	   (with-auto-accept
-	    (regularize-zone imol "A" 1 2 ""))
-	   (set-dragged-refinement-steps-per-frame s))
+       (if (not (valid-model-molecule? imol))
+	   #f 
+	   
+	   (let ((atom-1 (get-atom imol "A" 1 " O4 "))
+		 (atom-2 (get-atom imol "A" 2 " C1 ")))
+	     
+	     (format #t "bond-length: ~s: ~%"
+		     (bond-length (list-ref atom-1 2) (list-ref atom-2 2)))
 
-	 (let ((atom-1 (get-atom imol "A" 1 " O4 "))
-	       (atom-2 (get-atom imol "A" 2 " C1 ")))
+	     (let ((s (dragged-refinement-steps-per-frame)))
+	       (set-dragged-refinement-steps-per-frame 300)
+	       (with-auto-accept
+		(regularize-zone imol "A" 1 2 ""))
+	       (set-dragged-refinement-steps-per-frame s))
 
-	   (format #t "bond-length: ~s: ~%"
-		   (bond-length (list-ref atom-1 2) (list-ref atom-2 2)))
+	     (let ((atom-1 (get-atom imol "A" 1 " O4 "))
+		   (atom-2 (get-atom imol "A" 2 " C1 ")))
 
-	   (bond-length-within-tolerance? atom-1 atom-2 1.439 0.04))))))
+	       (format #t "bond-length: ~s: ~%"
+		       (bond-length (list-ref atom-1 2) (list-ref atom-2 2)))
+
+	       (bond-length-within-tolerance? atom-1 atom-2 1.439 0.04)))))))
+
 
