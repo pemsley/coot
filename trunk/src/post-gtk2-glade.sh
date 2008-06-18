d387b09056894df01952b43be747c53a8fa778b2
b.lohkamp <b.lohkamp@f7134757-782b-0410-8fd1-2f704457d451>
2008-06-18 15:41:23 +0000

    fixed 'o' button in python builds include revision (and python) in About dialog (GTK2) removed bug on windows' backup_maybe()

--- trunk/src/post-gtk2-glade.sh
+++ trunk/src/post-gtk2-glade.sh
@@ -5,6 +5,7 @@ sed -e 's/#include "callbacks.h.gtk2"/#if (GTK_MAJOR_VERSION > 1)\n\n#include "c
     -e 's/interface.h.gtk2/interface.h/' \
     -e 's/support.h.gtk2/support.h/' \
     -e 's/#include <unistd.h>/#ifndef _MSC_VER\n#include <unistd.h>\n#endif/' \
+    -e 's/ VERSION/ coot_version()/' \
     gtk2-interface.c > gtk2-interface.tmp
 echo '#endif /* (GTK_MAJOR_VERSION > 1) */' >> gtk2-interface.tmp
 
