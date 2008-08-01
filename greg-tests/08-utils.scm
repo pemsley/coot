a5ff709341ddeed6bb7fb9349b31cd221a639a58
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-08-01 12:10:41 +0000

    Add test python command for unknown python command. Fix up safe_python_command_with_return to handle unknown python command.

--- greg-tests/08-utils.scm
+++ greg-tests/08-utils.scm
@@ -33,8 +33,14 @@
 		       ;; coot does not convert a tuple.
 		       (let ((rv2 (run-python-command "test_val_2 = (1,2,3)")))
 			 (run-python-command "test_val_2") ; doesn't crash?
-			 #t)))))))))
-
+			 (format #t "   test_val_2 passed~%")
+			 ;; test c.f. running cootaneer_gui_bl() when we don't 
+			 ;; have pygtk loaded.  Shouldn't crash. (Roni Gordon bug).
+			 (let ((test_val_3 (run-python-command "rr_not_found_thing()")))
+			   ;; test_val_3 should be unspecified.
+			   (format #t "   test_val_3 passed~%") ; no crash
+			   #t))))))))))
+			   
 		     
 (greg-testcase "Internal/External Molecule Numbers match" #t 
    (lambda ()
