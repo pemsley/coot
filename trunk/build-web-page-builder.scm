3c3ce450c0a4a0b9f7434bd78fd34ac0912cbb7b
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-11-01 01:03:27 +0000

    Adjust settings for setsuko.

--- trunk/build-web-page-builder.scm
+++ trunk/build-web-page-builder.scm
@@ -690,10 +690,10 @@
 	      "Linux-setsuko.chem.york.ac.uk/gtk2" #t)
 
 	(list "binary-Linux-i386-fedora-6"
-	      "Linux-setsuko.chem.york.ac.uk/gtk2" #f)
+	      "Linux-setsuko.chem.york.ac.uk/gtk1" #f)
 
 	(list "binary-Linux-i386-fedora-6-python"
-	      "Linux-setsuko.chem.york.ac.uk/gtk2" #t)
+	      "Linux-setsuko.chem.york.ac.uk/gtk1-python" #t)
 
 	(list "binary-Linux-i386-fedora-8"
 	      "Linux-dragon.chem.york.ac.uk/gtk1" #f)
