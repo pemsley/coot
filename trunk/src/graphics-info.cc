eba2a8ee76a5e68f51baac00cb88099a7a8a70dc
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2008-11-09 15:29:30 +0000

    When molecule is in coot::CA_BONDS_PLUS_LIGANDS allow    regularize_object_bonds_box to be drawn in with do_Ca_plus_ligands_bonds()    args to a bonds_box, rather than do_Ca_bonds(). Allow the torsion    of ligands in CA+Ligands mode [Ingo Korndoefer].

--- trunk/src/graphics-info.cc
+++ trunk/src/graphics-info.cc
@@ -2907,12 +2907,20 @@ graphics_info_t::make_moving_atoms_graphics_object(const atom_selection_containe
        molecules[imol_moving_atoms].Bonds_box_type() == coot::CA_BONDS_PLUS_LIGANDS ||
        molecules[imol_moving_atoms].Bonds_box_type() == coot::CA_BONDS_PLUS_LIGANDS_SEC_STRUCT_COLOUR ||
        molecules[imol_moving_atoms].Bonds_box_type() == coot::COLOUR_BY_RAINBOW_BONDS) {
-      
-      Bond_lines_container bonds;
-      bonds.do_Ca_bonds(*moving_atoms_asc, 1.0, 4.7);
-      std::cout << "done CA bonds" << std::endl;
-      regularize_object_bonds_box.clear_up();
-      regularize_object_bonds_box = bonds.make_graphical_bonds();
+
+      if (molecules[imol_moving_atoms].Bonds_box_type() == coot::CA_BONDS_PLUS_LIGANDS) { 
+	 Bond_lines_container bonds;
+	 bonds.do_Ca_plus_ligands_bonds(*moving_atoms_asc, 1.0, 4.7);
+	 // std::cout << "done CA bonds" << std::endl;
+	 regularize_object_bonds_box.clear_up();
+	 regularize_object_bonds_box = bonds.make_graphical_bonds();
+      } else { 
+	 Bond_lines_container bonds;
+	 bonds.do_Ca_bonds(*moving_atoms_asc, 1.0, 4.7);
+	 // std::cout << "done CA bonds" << std::endl;
+	 regularize_object_bonds_box.clear_up();
+	 regularize_object_bonds_box = bonds.make_graphical_bonds();
+      }
       
    } else {
       int draw_hydrogens_flag = 0;
