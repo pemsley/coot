5a6f5108790caaea555b1cb85830bb2ef26a1086
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-01-06 14:44:33 +0000

    Revert to 1716 for main trunk.

--- trunk/src/c-interface-python.hh
+++ trunk/src/c-interface-python.hh
@@ -39,7 +39,6 @@
 PyObject *display_python(PyObject *o);
 
 std::pair<bool, coot::atom_spec_t> make_atom_spec_py(PyObject *spec);
-std::pair<bool, coot::residue_spec_t> make_residue_spec_py(PyObject *spec);
 
 #endif  // USE_PYTHON
 
