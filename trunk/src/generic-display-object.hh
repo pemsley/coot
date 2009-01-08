962457b81184b89c10b3fe0a4c11c1c724fbae04
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-01-08 21:07:04 +0000

    Add clear method for generic object - needs a test.

--- trunk/src/generic-display-object.hh
+++ trunk/src/generic-display-object.hh
@@ -55,33 +55,36 @@ namespace coot {
    class generic_display_object_t {
 
    public:
-     short int is_displayed_flag;
-     short int is_closed_flag; // don't make buttons for closed display objects
-     std::string name; // name the object, use for indexing, perhaps
-     std::vector<generic_display_line_set_t> lines_set;
-     std::vector<generic_display_point_set_t> points_set;
-     std::vector<int> GL_display_list_handles;
-     generic_display_object_t(const std::string &n) { 
-       name = n;
-       is_displayed_flag = 0;
-       is_closed_flag = 0;
-     }
-     void add_line(const coot::colour_holder &colour_in,
-		   const std::string &colour_name,
-		   const int &width_in, 
-		   const std::pair<clipper::Coord_orth, clipper::Coord_orth> &coords_in);
-     void add_point(const coot::colour_holder &colour_in,
+      short int is_displayed_flag;
+      short int is_closed_flag; // don't make buttons for closed display objects
+      std::string name; // name the object, use for indexing, perhaps
+      std::vector<generic_display_line_set_t> lines_set;
+      std::vector<generic_display_point_set_t> points_set;
+      std::vector<int> GL_display_list_handles;
+      generic_display_object_t(const std::string &n) { 
+	 name = n;
+	 is_displayed_flag = 0;
+	 is_closed_flag = 0;
+      }
+      void add_line(const coot::colour_holder &colour_in,
 		    const std::string &colour_name,
-		    const int &size_in, 
-		    const clipper::Coord_orth &coords_in);
-     void raster3d(std::ofstream &render_stream) const;
-     void close_yourself() { 
-       std::string name = "Closed Generic Display Object";
-       lines_set.clear();
-       points_set.clear();
-       is_closed_flag = 1;
-     } 
-     static coot::colour_holder colour_values_from_colour_name(const std::string &colour_name);
+		    const int &width_in, 
+		    const std::pair<clipper::Coord_orth, clipper::Coord_orth> &coords_in);
+      void add_point(const coot::colour_holder &colour_in,
+		     const std::string &colour_name,
+		     const int &size_in, 
+		     const clipper::Coord_orth &coords_in);
+      void raster3d(std::ofstream &render_stream) const;
+      void clear() {
+	 lines_set.clear();
+	 points_set.clear();
+      } 
+      void close_yourself() { 
+	 std::string name = "Closed Generic Display Object";
+	 clear();
+	 is_closed_flag = 1;
+      } 
+      static coot::colour_holder colour_values_from_colour_name(const std::string &colour_name);
    };
 
    class generic_text_object_t { 
