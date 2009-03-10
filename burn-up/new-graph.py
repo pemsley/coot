dfc998b39d1f944bfd279dd04c2a87e3e4bc1c7c
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-03-10 11:55:56 +0000

    Extra annotation for burn-up graph.

--- burn-up/new-graph.py
+++ burn-up/new-graph.py
@@ -81,9 +81,14 @@ def describeEvent(days, label, off):
     
 describeEvent(5, "Christmas\nholidays start", -10)
 
-tb = text_box.T(loc=(40, 37), text="Boston", shadow=(1,-1,fill_style.gray70))
+tb = text_box.T(loc=(28, 40), text="Boston", shadow=(1,-1,fill_style.gray70))
 # tb.add_arrow((ar.x_pos(data[33][0]), ar.y_pos(data[33][1])), "cb")
 tb.add_arrow((ar.x_pos(data[33][0]), ar.y_pos(data[33][1])), "c")
 tb.draw()
+
+tb = text_box.T(loc=(70, 73), text="Papers & Happy", shadow=(1,-1,fill_style.gray70))
+tb.add_arrow((ar.x_pos(data[74][0]), ar.y_pos(data[74][1])), "c")
+tb.draw()
+
 
 
