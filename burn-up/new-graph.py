4c2ec8b79d9ca906d71218bcba597172b6526b10
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-03-31 07:49:52 +0000

    Days to 110

--- burn-up/new-graph.py
+++ burn-up/new-graph.py
@@ -30,7 +30,7 @@ data = chart_data.read_csv("burn-up.tab", delim=" ")
 # and numbers are printed as integers ("%d").
 #
 
-x_day_range = 100
+x_day_range = 110
 
 x_tick_interval = 2
 if (x_day_range > 40):
