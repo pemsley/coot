1f7fa36586a9ee11eca7c834eca428b67f6c0f1b
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-05-11 13:16:22 +0000

    Fix Tim Grune bug - shelx molecules should honour the "don't recentre"    option-menu.

--- trunk/src/c-interface-build.cc
+++ trunk/src/c-interface-build.cc
@@ -5279,19 +5279,19 @@ int get_reset_b_factor_moved_atoms_state() {
 
 /* section SHELXL Functions */
 // return 
-int read_shelx_ins_file(const char *filename) {
+int read_shelx_ins_file(const char *filename, short int recentre_flag) {
 
    int istat = -1;
    graphics_info_t g;
    if (filename) { 
       int imol = graphics_info_t::create_molecule();
 
-      // 20080518 Why did I want to not centre on a new shelx
-      // molecule? I forget now.  So comment out these lines of code
-      // and the resetting of recentre_on_read_pdb flag later.
+      // ugly method to recente the molecule on read
+      // (save, set and reset state).
+      // 
+      short int reset_centre_flag = g.recentre_on_read_pdb;
+      g.recentre_on_read_pdb = recentre_flag;
       
-//       short int reset_centre_flag = g.recentre_on_read_pdb;
-//       g.recentre_on_read_pdb = 0;
       istat = g.molecules[imol].read_shelx_ins_file(std::string(filename));
       if (istat != 1) {
 	 graphics_info_t::erase_last_molecule();
@@ -5310,7 +5310,7 @@ int read_shelx_ins_file(const char *filename) {
 	 command_strings.push_back(single_quote(coot::util::intelligent_debackslash(filename)));
 	 add_to_history(command_strings);
       }
-      //       g.recentre_on_read_pdb = reset_centre_flag;
+      g.recentre_on_read_pdb = reset_centre_flag;
    } else {
       std::cout << "ERROR:: null filename in read_shelx_ins_file" << std::endl;
    }
