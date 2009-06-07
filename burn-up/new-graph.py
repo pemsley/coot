70c48de6cae201bbc247680d12d633ea200ef0bc
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-06-07 15:20:19 +0000

    Make the license clearly version 3.

--- burn-up/new-graph.py
+++ burn-up/new-graph.py
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2000-2005 by Yasushi Saito (yasushi.saito@gmail.com)
+# Copyright 2009 by The University of Oxford
 # 
 # Pychart is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
--- ligand/test-helix-placement.cc
+++ ligand/test-helix-placement.cc
@@ -1,10 +1,11 @@
 /* ligand/test-helix-placement.cc
  * 
- * Copyright 2005 by Paul Emsley, The University of York
+ * Copyright 2005 by The University of York
+ * Author: Paul Emsley
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
+ * the Free Software Foundation; either version 3 of the License, or (at
  * your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but
