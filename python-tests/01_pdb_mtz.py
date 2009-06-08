eef35986e4b5315a99ca8ac7c39db3bc0ea870c0
b.lohkamp <b.lohkamp@f7134757-782b-0410-8fd1-2f704457d451>
2009-06-08 06:25:30 +0000

    fix typo in new TER test

--- python-tests/01_pdb_mtz.py
+++ python-tests/01_pdb_mtz.py
@@ -55,8 +55,8 @@ try:
 	#global have_ccp4_qm
 	ccp4_master = os.getenv("CCP4_MASTER")
 	if (os.path.isdir(ccp4_master)):
-		print "==== It seems CCP$_MASTER is setup ==="
-		print "==== CCP$_MASTER:", ccp4_master
+		print "==== It seems CCP4_MASTER is setup ==="
+		print "==== CCP4_MASTER:", ccp4_master
 		have_ccp4_qm = True
 except:
 	print "BL INFO:: Dont have CCP4 master"
@@ -1355,7 +1355,7 @@ class PdbMtzTestFunctions(unittest.TestCase):
 	    # not.
 	    #
 	    opdb = "auto-waters.pdb"
-	    write_pdb_file(imol_model)
+	    write_pdb_file(imol_model, opdb)
 	    fin = open(opdb, 'r')
 	    lines = fin.readlines()
 	    fin.close()
