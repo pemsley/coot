d6c7c341b16814bdd10ad82e02e5259868345bbd
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-06-18 12:44:03 +0000

    Updates for updating binaries. Tested on pre-release.    GTK2+ only (because of threads).    Now we call the sever and check the type-binary*latest.txt file    to find the latest built version.    The check-for-updates extension is not added in (as yet).

---
+++ trunk/update-binary/check-for-updates.scm
@@ -0,0 +1,192 @@
+
+;; a thread handling function
+(define (coot-updates-error-handler key . args)
+  (format #t "error: finding updates: error in ~s with args ~s~%" key args))
+
+;; The file name of the phone home script, executed with guile -s
+;; FIXME - need to be installed in xxx/etc (or some such).
+(define phone-home-cmd
+  (string-append (getenv "HOME")
+		 "/Projects/coot/update-binary/phone-home.scm"))
+
+;; The file name of the phone home script, executed with guile -s
+;; FIXME
+;; 
+(define download-bin-cmd
+  (string-append (getenv "HOME")
+		 "/Projects/coot/update-binary/download-binary-cmd.scm"))
+
+(define (get-revision-from-string str)
+  ;; e.g. str is "coot-0.6-pre-1-revision-2060" (with a newline at the
+  ;; end too).  We want to return 2060 (a number) from here (or #f).
+  (let* ((s (sans-final-newline str))
+	 (ls (separate-fields-discarding-char #\- s list)))
+    (string->number (car (reverse ls)))))
+
+;; Is this true?
+(define (get-stable-release-from-server-string str)
+  str)
+
+;; Needs testing.
+(define (get-stable-release-from-coot-version)
+  (let* ((s (coot-version))
+	 (ls (separate-fields-discarding-char #\space s list)))
+    (car ls)))
+
+
+; Return #t or #f 
+(define (new-version-on-server? str is-pre-release?)
+
+  (if is-pre-release?
+
+      ;; pre-releases are handle with svn revision numbers (as ints).
+      ;; 
+      (let ((server-rev (get-revision-from-string str)))
+	;; for pre-release
+	(if (number? server-rev)
+	    (> server-rev (svn-revision))))
+
+      ;; For a stable release: 
+      ;; 
+      ;; The Coot release number (e.g. "0.6.4") is a string that can
+      ;; be compared lexographically.
+      ;; 
+      (let ((server-version (get-stable-release-from-server-string str))
+	    (this-build-version (get-stable-release-from-coot-version)))
+	(if (string? server-version)
+	    (string>? server-version this-build-version)))))
+
+
+(define (notify-of-new-version str)
+  (let* ((ls (split-before-char #\c str list))
+	 (ls2 (split-before-char #\" (car (reverse ls)) list)))
+
+    (download-binary-dialog (car ls2))))
+
+;; version-string is something like: "coot-0.6-pre-1-revision-2060"
+(define (download-binary-dialog version-string)
+
+
+  (let ((s (string-append "New revision available " 
+			  "for this binary type:\n"
+			  (coot-sys-build-type)
+			  "\n"
+			  version-string)))
+
+    (let ((window (gtk-window-new 'toplevel))
+	  (dialog-name "Download binary")
+	  (main-vbox (gtk-vbox-new #f 6))
+	  (cancel-button (gtk-button-new-with-label "  Cancel  "))
+	  (ok-button (gtk-button-new-with-label "  Download  "))
+	  (buttons-hbox (gtk-hbox-new #f 6))
+	  (h-sep (gtk-hseparator-new))
+	  (info-string (gtk-label-new s)))
+
+      (gtk-box-pack-start buttons-hbox ok-button #t #f 6)
+      (gtk-box-pack-start buttons-hbox cancel-button #t #f 6)
+
+      (gtk-box-pack-start main-vbox info-string  #t #f 6)
+      (gtk-box-pack-start main-vbox h-sep        #t #f 6)
+      (gtk-box-pack-start main-vbox buttons-hbox #t #f 6)
+      
+      (gtk-container-add window main-vbox)
+
+      (gtk-signal-connect cancel-button "clicked"
+			  (lambda ()
+			    (gtk-widget-destroy window)))
+
+      (gtk-signal-connect ok-button "clicked"
+			  (lambda () 
+			    (let* ((args-1 (list "-s" 
+						 download-bin-cmd
+						 "binary" (coot-sys-build-type)))
+				   (st (coot-version))
+				   (args-2 (if (string-match "-pre-" st)
+					       (append args-1
+						       (list "pre-release"))
+					       args-1))
+				   (args (append 
+					  args-2
+					  (list "version" version-string))))
+			      
+			      (goosh-command "guile" args '() 
+					     "tmp-coot-download-cmd.log" #f))))
+
+      (gtk-widget-show-all window))))
+	  
+
+
+
+
+(let ((menu (coot-menubar-menu "Updates")))
+  (add-simple-coot-menu-menuitem
+   menu "Check for updates..."
+   (lambda ()
+
+     (let* ((server-info-status 'pending)
+	    (thread (call-with-new-thread
+		     (lambda ()
+		       (format #t "get updates info thread~%")
+		       ;; here we construct args to goosh-command,
+		       ;; adding in "pre-release" if this binary is a
+		       ;; pre-release.
+		       ;; args ends up as something like:
+		       ;; ("-s" "xxx/phone-home.scm" "pre-release" 
+                       ;;  "binary" "Linux-1386-fedora-10-python-gtk2"
+		       ;;  "command-line" "/home/xx/coot/bin/coot")
+		       (let* ((update-coot-log "tmp-update-coot.log")
+			      (args-1 (list "binary" (coot-sys-build-type)
+					    "command-line" (car (command-line))))
+			      (st (coot-version))
+			      (args-2 (if (string-match "-pre-" st)
+					(cons "pre-release" args-1)
+					args-1))
+			      (args-3 (append 
+				       (list "-s" phone-home-cmd) args-2)))
+			 (if (file-exists? update-coot-log)
+			     (delete-file update-coot-log))
+			 (goosh-command "guile" args-3 '() update-coot-log #f)
+			 (if (file-exists? update-coot-log)
+			     (begin
+			       ;; OK, so the server said something.
+			       ;; Set the status here, so that the
+			       ;; function that looks to see whether
+			       ;; or not the server responded is
+			       ;; notified.
+			       ;; 
+			       (call-with-input-file update-coot-log
+				 (lambda (port)
+				   (let ((line (read-line port)))
+				     (format #t "got line ~s~%" line)
+				     ;; recall that we can't do GUI
+				     ;; things in a sub-thread.
+				     (set! server-info-status line))))
+			       (delete-file update-coot-log)))))
+                               ;;; thread ends here.
+       
+		    coot-updates-error-handler)))
+
+       (let ((is-pre-release? (string-match "-pre-" (coot-version))))
+	 (let ((count 0))
+	   (gtk-idle-add
+	    (lambda ()
+	      (cond
+	       ((> count 2000) ;; try for 20 seconds, otherwise timeout.
+		;; fail-with-timeout
+		(format #t "final fail: server-info-status: ~s~%" server-info-status)
+		#f) ;; stop running this idle function
+	       ((string? server-info-status) 
+		(if (new-version-on-server? server-info-status is-pre-release?)
+		    (notify-of-new-version server-info-status)
+		    (let ((s (string-append "No version newer than this revision ("
+					    (number->string (svn-revision))
+					    ").")))
+		      (info-dialog s)))
+		#f) ;; stop running idle function
+	       (else 
+		(usleep 10000) 
+		;; (format #t "server-info-status: ~s~%" server-info-status)
+		(set! count (+ count 1))
+		#t))))))))))
+
+	  
--- trunk/update-binary/phone-home.scm
+++ trunk/update-binary/phone-home.scm
@@ -1,4 +1,5 @@
 
+(use-modules (oop goops) (oop goops describe) (net http))
 
 ;;; We are given the binary sys type from the command line, and also
 ;;; whether or not this is a pre-release ("pre-release" is passed on
@@ -6,8 +7,6 @@
 ;;; 
 ;;; We need to construct the right url for the binary and go and check
 ;;; if it is there.
-
-(format #t "command line args: ~s~%" (command-line))
 
 
 ;; Return a boolean for whether or not pre-release was passed as a
@@ -20,40 +19,74 @@
      (else 
       (loop (cdr ls))))))
 
+
+;; Return the argument after interesting-string, or null if no such
+;; arg.
+(define (extract-next-arg-from-command-line interesting-string cla)
+  (let loop ((ls cla))
+    (cond
+     ((null? ls) #f)
+     ((string=? (car ls) interesting-string)
+      (if (null? (cdr ls))
+	  #f
+	  (car (cdr ls))))
+     (else 
+      (loop (cdr ls))))))
+  
 ;; Return the binary type as a string (return #f for this part on
 ;; string-not-found).  The binary string is any string that is
 ;; proceeded with an arg of "binary"
 (define (extract-binary-type cla)
-  (let loop ((ls cla))
-    (cond 
-     ((null? ls) #f)
-     ((string=? (car ls) "binary")
-      (if (null? (cdr ls))
-	  #f
-	  (car (cdr ls)))) ;; the string we want
-     (else 
-      (loop (cdr ls))))))
-     
+  (extract-next-arg-from-command-line "binary" cla))
+
+;; Return what we presume to be the first argument in the command line
+;; for the program that called this program.  We do this so that we
+;; can find the bin dir and bin/.. dir and test whether we can write
+;; into it.
+;; 
+(define (extract-coot-command-line cla)
+  (extract-next-arg-from-command-line "command-line" cla))
+
+
+(define get-url-as-string
+  (lambda (my-url)
+    
+    (call-with-output-string
+      (lambda (port)
+
+	(let ((initial-output-port (current-output-port)))
+
+	  (set-current-output-port port)
+	  (http-get my-url)
+	  (set-current-output-port initial-output-port))))))
 
 
 (let ((pre-release-flag (extract-pre-release-flag (command-line)))
       (binary-type (extract-binary-type (command-line))))
 
 
-  (format #t "pre-release: ~s~%" pre-release-flag)
-  (format #t "binary-type: ~s~%" binary-type)
+;;   (format #t "pre-release: ~s~%" pre-release-flag)
+;;   (format #t "binary-type: ~s~%" binary-type)
 
   (if (string? binary-type)
       
       (let ((check-url 
 	     (if pre-release-flag
 		 (string-append
-		  "http://www.ysbl.york.ac.uk/~emsley/software/binaries/pre-release/"
+		  "http://www.ysbl.york.ac.uk/"
+		  "~emsley/software/binaries/nightlies/pre-release/"
+		  "type-binary-"
 		  binary-type
 		  "-latest.txt")
 		 (string-append
-		  "http://www.ysbl.york.ac.uk/~emsley/software/binaries/release/"
+		  "http://www.ysbl.york.ac.uk/"
+		  "~emsley/software/binaries/release/"
+		  "type-binary-"
 		  binary-type
 		  "-latest.txt"))))
 
-	(format #t "Now get: ~s~%" check-url))))
+	;; (format #t "Now get: ~s~%" check-url)
+	
+	(let ((s (get-url-as-string check-url)))
+
+	  (format #t "LATEST-BUILD-ON-SERVER: ~s~%" s)))))
