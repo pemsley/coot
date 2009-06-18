cbbc743d52b84114fa958cb1a2731103d2274c4f
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-06-18 23:07:00 +0000

    update teh svn-revision.cc file, now we seem to need extern "C".

--- generate-svn-revision-cc.sh
+++ generate-svn-revision-cc.sh
@@ -2,8 +2,14 @@
 svn -qu status | tail -1 | awk '
 BEGIN {svn_revision_cc = "src/svn-revision.cc"}
 {
+# Adding this extern "C" is just magic.  Baah. 
+# If it is not here then (in coot_version()):
+#    c-interface.cc:174: undefined reference to `svn_revision'
+#
+print "extern \"C\" {"           > svn_revision_cc 
 print "int svn_revision() { "  > svn_revision_cc 
 print "   return ", $NF ";"    > svn_revision_cc 
+print "}"                      > svn_revision_cc;
 print "}"                      > svn_revision_cc;
 print ""                       > svn_revision_cc;  
 }'
