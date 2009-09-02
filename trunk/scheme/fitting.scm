1a96091d83f3f48b6387a2dc190953c9c2db7b43
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-09-02 16:11:07 +0000

    Document backrub rotamers. Set Spin Speed in Extensions now in the Settings submenu. Don't do rotamers of HOHs in fit-protein-fit-function. In the coot news, get RELEASE-NOTES, not release-notes (to do with new web    server, I guess).

--- trunk/scheme/fitting.scm
+++ trunk/scheme/fitting.scm
@@ -142,15 +142,19 @@
 	    (format #t "centering on ~s ~s ~s~%" chain-id res-no "CA")
 	    (set-go-to-atom-chain-residue-atom-name chain-id res-no "CA")
 	    (rotate-y-scene 10 0.3) ; n-frames frame-interval(degrees)
-	    (if (string=? alt-conf "")
-		(auto-fit-best-rotamer res-no alt-conf ins-code chain-id imol 
-				       imol-map 1 0.1))
-	    (if (>= imol-map 0)
-		(begin
-		  ;; (refine-auto-range imol chain-id res-no "")
-		  (refine-zone imol chain-id res-no res-no alt-conf)
-		  (accept-regularizement)))
-	    (rotate-y-scene 10 0.3)))
+	    
+	    (let ((res-name (residue-name imol chain-id res-no ins-code)))
+	      (if (not (string=? res-name "HOH"))
+		  (begin
+		    (if (string=? alt-conf "")
+			(auto-fit-best-rotamer res-no alt-conf ins-code chain-id imol 
+					       imol-map 1 0.1))
+		    (if (>= imol-map 0)
+			(begin
+			  ;; (refine-auto-range imol chain-id res-no "")
+			  (refine-zone imol chain-id res-no res-no alt-conf)
+			  (accept-regularizement)))
+		    (rotate-y-scene 10 0.3))))))
 	 (residue-alt-confs imol chain-id res-no ins-code))))
 
 
