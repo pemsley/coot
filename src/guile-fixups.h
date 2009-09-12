678493ed09830daa3ee21227c3bcce50fa38b627
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-09-12 22:02:04 +0000

    fixups for new python code and for old guile.

--- src/guile-fixups.h
+++ src/guile-fixups.h
@@ -5,6 +5,7 @@
 #else    
 #define scm_to_int gh_scm2int
 #define scm_to_locale_string SCM_STRING_CHARS
+#define scm_from_locale_string scm_makfrom0str
 #define scm_to_double  gh_scm2double
 #define scm_is_true gh_scm2bool
 #define scm_is_string gh_string_p
