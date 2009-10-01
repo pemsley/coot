ccef47c92c8b1ec3b46c1fea1defca8649bae961
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-10-01 07:20:27 +0000

    Change the way the coot_wrap files are handled.  We no longer make   lib_coot_wrap* libraries and now the GTK1 and GTK2 versions are distinguished.

--- trunk/src/post-process-coot-wrap-guile.sh
+++ trunk/src/post-process-coot-wrap-guile.sh
@@ -1,25 +1,36 @@
 
-if [ $# != 3 ] ; then
-   echo must provide guile version and 2 file arguments
+if [ $# != 4 ] ; then
+   echo must provide guile-version gtk2-flag and 2 file arguments
    echo we got: $*
    exit 2
 fi
 
-pre="$2"
-post="$3"
+pre="$3"
+post="$4"
 
 guile_version=$1
+gtk2=$2 
 
 case $guile_version in
 
    1.6*) 
    echo sed -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre $post
-   sed -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre > $post
+        sed -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre > $post
     ;; 
    
    1.8*)
-   echo sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre $post
-   sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' -e '/.libguile.h./{x;s/.*/#include <cstdio>/;G;}' $pre > $post
+   echo sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' -e '/.libguile.h./{x;s/.*/#include <cstdio>/;G;}' $pre ..to.. $post
+        sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' -e '/.libguile.h./{x;s/.*/#include <cstdio>/;G;}' $pre > $pre.tmp
+   echo adding gtk2 and guile ifdefs
+   echo "#ifdef USE_GUILE"  > $post
+   if [ "$gtk2" = gtk2 ] ; then
+      echo "#ifdef COOT_USE_GTK2_INTERFACE"  >> $post
+   else 
+      echo "#ifndef COOT_USE_GTK2_INTERFACE"  >> $post
+   fi    
+   cat $pre.tmp >> $post
+   echo "#endif // COOT_USE_GTK2_INTERFACE" >> $post
+   echo "#endif // USE_GUILE" >> $post
    ;; 
 
 esac
