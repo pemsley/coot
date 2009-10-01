e6a348cab3580ac52d137bbb5308723c8069f259
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-10-01 09:27:55 +0000

    Use bash, not pathetic sh to post-process coot_wrap files.  Make sure that    the ifdefs are added to the guile 1.6 path too.

--- trunk/src/post-process-coot-wrap-guile.sh
+++ trunk/src/post-process-coot-wrap-guile.sh
@@ -1,9 +1,31 @@
 
+function add_ifdefs { 
+
+pre_tmp=$1
+post=$2
+    
+echo adding gtk2 and guile ifdefs from $pre_tmp $post
+echo "#ifdef USE_GUILE"  > $post
+if [ "$gtk2" = gtk2 ] ; then
+   echo "#ifdef COOT_USE_GTK2_INTERFACE"  >> $post
+else 
+   echo "#ifndef COOT_USE_GTK2_INTERFACE"  >> $post
+fi    
+cat $pre_tmp >> $post
+echo "#endif // COOT_USE_GTK2_INTERFACE" >> $post
+echo "#endif // USE_GUILE" >> $post
+
+}
+
+
+#####  start ################
+
 if [ $# != 4 ] ; then
    echo must provide guile-version gtk2-flag and 2 file arguments
    echo we got: $*
    exit 2
 fi
+
 
 pre="$3"
 post="$4"
@@ -15,22 +37,14 @@ case $guile_version in
 
    1.6*) 
    echo sed -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre $post
-        sed -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre > $post
-    ;; 
+        sed -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' $pre > $pre.tmp
+   add_ifdefs $pre.tmp $post
+   ;; 
    
    1.8*)
    echo sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' -e '/.libguile.h./{x;s/.*/#include <cstdio>/;G;}' $pre ..to.. $post
         sed -e 's/SCM_STRING_CHARS/scm_to_locale_string/' -e 's/static char .gswig_const_COOT_SCHEME_DIR/static const char *gswig_const_COOT_SCHEME_DIR/' -e '/.libguile.h./{x;s/.*/#include <cstdio>/;G;}' $pre > $pre.tmp
-   echo adding gtk2 and guile ifdefs
-   echo "#ifdef USE_GUILE"  > $post
-   if [ "$gtk2" = gtk2 ] ; then
-      echo "#ifdef COOT_USE_GTK2_INTERFACE"  >> $post
-   else 
-      echo "#ifndef COOT_USE_GTK2_INTERFACE"  >> $post
-   fi    
-   cat $pre.tmp >> $post
-   echo "#endif // COOT_USE_GTK2_INTERFACE" >> $post
-   echo "#endif // USE_GUILE" >> $post
+   add_ifdefs $pre $post
    ;; 
 
 esac
