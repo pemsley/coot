1a1be511b3e7c2145ebe0466d377c3a253b95546
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-10-01 07:20:27 +0000

    Change the way the coot_wrap files are handled.  We no longer make   lib_coot_wrap* libraries and now the GTK1 and GTK2 versions are distinguished.

--- src/post-process-coot-wrap-python.sh
+++ src/post-process-coot-wrap-python.sh
@@ -1,13 +1,20 @@
 
-if [ $# != 2 ] ; then
-   echo must provide guile version and 2 file arguments
+if [ $# != 3 ] ; then
+   echo must provide gtk-version and 2 file arguments
    echo we got: $*
    exit 2
 fi
 
-pre="$1"
-post="$2"
+pre="$2"
+post="$3"
+gtk2="$1"
 
 echo "#ifdef USE_PYTHON " > "$post"
+if [ "$gtk2" = gtk2 ] ; then
+   echo "#ifdef COOT_USE_GTK2_INTERFACE"  >> $post
+else 
+   echo "#ifndef COOT_USE_GTK2_INTERFACE"  >> $post
+fi    
 cat "$pre" >> "$post" 
+echo "#endif // COOT_USE_GTK2_INTERFACE" >> $post
 echo "#endif // USE_PYTHON " >> "$post"
