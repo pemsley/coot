ede2c383493df1e59146651f66cee94484c75739
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2009-12-08 20:20:20 +0000

    Stop using phone-home.  Still doesn't work.

--- update-binary/check-for-updates.scm
+++ update-binary/check-for-updates.scm
@@ -5,9 +5,9 @@
 
 ;; The file name of the phone home script, executed with guile -s
 ;; FIXME - need to be installed in xxx/etc (or some such).
-(define phone-home-cmd
-  (string-append (getenv "HOME")
-		 "/Projects/coot/update-binary/phone-home.scm"))
+;; (define phone-home-cmd
+;;   (string-append (getenv "HOME")
+;; 		 "/Projects/coot/update-binary/phone-home.scm"))
 
 
 (define (get-revision-from-string str)
@@ -51,15 +51,19 @@
 	    (string>? server-version this-build-version)))))
 
 
+;; return a string with no trailing newline.
+;; 
 (define (notify-of-new-version str)
   (format #t "notify-of-new-version given string str: ~s~%" str)
   (let* ((ls (split-before-char #\c str list))
-	 (ls2 (split-before-char #\" (car (reverse ls)) list)))
+	 (ls-2 (split-before-char #\" (car (reverse ls)) list))
+	 (ls-3 (split-before-char #\newline (car ls-2) list)))
 
     (format #t "notify-of-new-version str: ~s~%" str)
     (format #t "ls: ~s~%" ls)
-    (format #t "ls2: ~s~%" ls2)
-    (download-binary-dialog (car ls2))))
+    (format #t "ls-2: ~s~%" ls-2)
+    (format #t "ls-3: ~s~%" ls-3)
+    (download-binary-dialog (car ls-3))))
 
 ;; version-string is something like: "coot-0.6-pre-1-revision-2060"
 (define (download-binary-dialog version-string)
@@ -91,6 +95,7 @@
       (gtk-container-border-width main-vbox 6)
       
       (gtk-container-add window main-vbox)
+      (gtk-container-border-width window 6)
 
       (gtk-signal-connect cancel-button "clicked"
 			  (lambda ()
@@ -98,44 +103,89 @@
 
       (gtk-signal-connect ok-button "clicked"
 			  (lambda () 
-			    (run-download-binary-curl revision)))
+			    (run-download-binary-curl revision version-string)))
 
       (gtk-widget-show-all window))))
+
+;; return success status
+(define (install-coot-tar-file tar-file-name)
+  'xx)
+
+;; 
+(define (directory-is-modifiable? prefix-dir)
+  #f)
+
+;; return as a symbol, or #f
+(define (get-target-md5-string file-name)
+  (if (not (file-exists? file-name))
+      #f
+      (call-with-input-file file-name
+	(lambda (port)
+	  (read port)))))
+
+(define (get-md5sum-string file-name)
+  (if (not (file-exists? file-name))
+      #f
+      (let ((s (shell-command-to-string (string-append "md5sum " file-name))))
+	(format #t "debug: s is ~s~%" s))))
+
+  
+	   
+  
+
+(define (match-md5sums tar-file-name target-md5sum-file-name)
+  (if (not (file-exists? tar-file-name))
+      #f
+      (if (not (file-exists? target-md5sum-file-name))
+	  #f
+	  (let ((target-md5-string (get-target-md5-string target-md5sum-file-name))
+		(md5-string (get-md5sum-string tar-file-name)))
+	    (if (not (string? target-md5-string)
+		#f
+		(if (not (string? md5-string))
+		    #f
+		    (string=? target-md5-string md5-string))))))))
+
   
 ;; get the binary
-(define (run-download-binary-curl revision)
-  (format #t "::::: run-download-binary-curl....~%")
-  (let* ((install-prefix "something") ;; to get curl binary
-	 (pre-release-flag (string-match "-pre-" (coot-version)))
-	 (host-dir "www.biop.ox.ac.uk/coot/software/binaries/")
-	 (binary-type (coot-sys-build-type))
-	 (url (if pre-release-flag
-		  (string-append
-		   "http://" 
-		   host-dir "/pre-releases/"
-		   "coot-"
-		   version
-		   "-"
-		   revision
-		   "-binary-"
-		   binary-type
-		   ".tar.gz")
+;; 
+;; 
+(define (run-download-binary-curl revision version-string)
+  (format #t "::::: run-download-binary-curl.... with revision ~s with version-string ~s~%" 
+	  revision version-string)
+  (let ((prefix (getenv "COOT_PREFIX")))
+    (if (string? prefix)
+	(let* ((curl-exe (string-append prefix "/bin/curl")) ;; to get curl binary
+	       (pre-release-flag (string-match "-pre" (coot-version)))
+	       (host-dir "www.biop.ox.ac.uk/coot/software/binaries/")
+	       (binary-type (coot-sys-build-type))
+	       (url (if pre-release-flag
+			(string-append
+			 "http://" 
+			 host-dir "pre-releases/"
+			 version-string
+			 "-binary-"
+			 binary-type
+			 ".tar.gz")
 
-		  ;; stable
-		  (string-append
-		   "http://" 
-		   host-dir "/releases/"
-		   "coot-"
-		   new-version
-		   "-binary-"
-		   binary-type
-		   ".tar.gz"))))
+			;; stable
+			(string-append
+			 "http://" 
+			 host-dir "releases/"
+			 "coot-"
+			 new-version
+			 "-binary-"
+			 binary-type
+			 ".tar.gz")))
+	       (md5-url (string-append url ".md5sum")))
 
-    (format #t "url for curl: ~s~%" url)
-	 
-    (goosh-command (string-append install-prefix "/bin/curl")
-		   (list url)
-		   '() #t "tmp-download-coot.log")))
+	  (format #t "url for curl: ~s~%" url)
+	  (goosh-command curl-exe (list md5-url) '() #t "tmp-download-coot-md5sum.log")
+	  (goosh-command curl-exe (list url) '() #t "tmp-download-coot.log")
+	  (if (match-md5sums tar-file-name md5-tar-file-name)
+	      (let ((success (install-coot-tar-file tar-file-name)))
+		(if success
+		    (restart-dialog))))))))
 				  
 	      
   
@@ -148,52 +198,54 @@
    menu "Check for updates..."
    (let ((server-info-status #f))
 
+     ;; return a boolean
+     (define (pre-release?)
+       (string-match "-pre" (coot-version)))
+
+     (define (handle-latest-version-server-response txt-from-server)
+       ;; OK, so the server said something.
+       ;; Set the status here, so that the
+       ;; function that looks to see whether
+       ;; or not the server responded is
+       ;; notified.
+       ;; 
+       (if (string-match "The requested URL was not found on this server" txt-from-server)
+	   (set! server-info-status 'file-not-found)
+	   (set! server-info-status txt-from-server)))
+
+     ;; here we construct args to goosh-command,
+     ;; adding in "pre-release" if this binary is a
+     ;; pre-release.
+     ;; args ends up as something like:
+     ;; ("-s" "xxx/phone-home.scm" "pre-release" 
+     ;;  "binary" "Linux-1386-fedora-10-python-gtk2"
+     ;;  "command-line" "/home/xx/coot/bin/coot")
+     ;; 
+     (define (make-latest-version-url)
+       (let ((build-type (coot-sys-build-type)))
+	 
+	 ;; hack!
+	 ;; 
+	 (set! build-type "Linux-i386-centos-4-gtk2")
+
+	 (string-append 
+	  "http://www.biop.ox.ac.uk/coot/software/binaries/"
+	  (if (pre-release?)
+	      "pre-releases"
+	      "releases")
+	  "/"
+	  "type-binary-"
+	  build-type
+	  "-latest.txt")))
 
      (define (get-server-info-status-thread)
        (call-with-new-thread
 	(lambda ()
-	  (format #t "get updates info thread~%")
-	  ;; here we construct args to goosh-command,
-	  ;; adding in "pre-release" if this binary is a
-	  ;; pre-release.
-	  ;; args ends up as something like:
-	  ;; ("-s" "xxx/phone-home.scm" "pre-release" 
-	  ;;  "binary" "Linux-1386-fedora-10-python-gtk2"
-	  ;;  "command-line" "/home/xx/coot/bin/coot")
-	  (let* ((update-coot-log "tmp-update-coot.log")
-		 (args-1 (list "binary" (coot-sys-build-type)
-			       "command-line" (car (command-line))))
-		 (st (coot-version))
-		 (args-2 (if (string-match "-pre-" st)
-			     (cons "pre-release" args-1)
-			     args-1))
-		 (args-3 (append 
-			  (list "-s" phone-home-cmd) args-2)))
-	    (if (file-exists? update-coot-log)
-		(delete-file update-coot-log))
-	    (format #t "about to: guile ~s~%" args-3)
-	    (goosh-command "guile" args-3 '() update-coot-log #f)
-	    (format #t "done: guile ~s~%" args-3)
-	    (if (file-exists? update-coot-log)
-		(begin
-		  ;; OK, so the server said something.
-		  ;; Set the status here, so that the
-		  ;; function that looks to see whether
-		  ;; or not the server responded is
-		  ;; notified.
-		  ;; 
-		  (call-with-input-file update-coot-log
-		    (lambda (port)
-		      (let ((line (read-line port)))
-			(format #t "got line ~s from ~s~%" line update-coot-log)
-			;; recall that we can't do GUI
-			;; things in a sub-thread.
-			(format #t "setting server-info-status to ~s~%" line)
-			(set! server-info-status line))))
-		  ;; x(delete-file update-coot-log)
-		  ))))
-                               ;;; thread ends here.
-	
+	  (let* ((url (make-latest-version-url))
+		 (nov (format #t "DEBUG:: attempting to get URL ~s~%" url))
+		 (latest-version-server-response (coot-get-url-as-string url)))
+	    (handle-latest-version-server-response latest-version-server-response)))
+	;; the error handler
 	coot-updates-error-handler))
 
 
@@ -201,7 +253,7 @@
 
        (get-server-info-status-thread)
 
-       (let ((is-pre-release? (string-match "-pre-" (coot-version))))
+       (let ((is-pre-release? (pre-release?)))
 	 (let ((count 0))
 	   (gtk-idle-add
 	    (lambda ()
@@ -210,7 +262,15 @@
 		;; fail-with-timeout
 		(format #t "final fail: server-info-status: ~s~%" server-info-status)
 		#f) ;; stop running this idle function
-	       ((string? server-info-status) 
+	       ((eq? server-info-status 'file-not-found)
+		(let ((s (string-append "No " 
+					(if is-pre-release? "pre-release" "release")
+					" binary for this system ("
+					(coot-sys-build-type) 
+					") on the binary server")))
+		  (info-dialog s)
+		  #f)) ;; stop running this idle function
+	       ((string? server-info-status)
 		(if (new-version-on-server? server-info-status is-pre-release?)
 		    (notify-of-new-version server-info-status)
 		    (let ((s (string-append "No version newer than this revision ("
