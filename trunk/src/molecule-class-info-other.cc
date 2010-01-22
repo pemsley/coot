93db1409e014006036771c7c2d249e2067dda89a
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2010-01-22 16:10:00 +0000

    Use the utilities sort function, not the implicit one by using a minimol.    Headers are preserved.

--- trunk/src/molecule-class-info-other.cc
+++ trunk/src/molecule-class-info-other.cc
@@ -7217,15 +7217,13 @@ molecule_class_info_t::scale_cell(float fac_u, float fac_v, float fac_w) {
    return retval; 
 } 
 
+
 void
 molecule_class_info_t::sort_chains() {
 
-   coot::minimol::molecule mol(atom_sel.mol);
-   mol.sort_chains();
-   atom_sel.mol->DeleteSelection(atom_sel.SelectionHandle);
-   delete atom_sel.mol;
-   atom_sel = make_asc(mol.pcmmdbmanager());
-   update_molecule_after_additions();
+   if (atom_sel.mol) {
+      coot::sort_chains(atom_sel.mol);
+   }
 }
 
 std::vector<coot::residue_spec_t>
