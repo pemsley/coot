a5a93aef30e4aa374856aa7af2a2ba3a4fed171a
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2010-02-12 16:44:31 +0000

    We also need to #ifdef protect gtk_tool_item_set_tooltip() for the    rotate/translate gtk_menu_tool_button_new().

--- trunk/src/post-gtk2-glade.sh
+++ trunk/src/post-gtk2-glade.sh
@@ -43,7 +43,7 @@ sed -e 's/#include "callbacks.h.gtk2"/#if (GTK_MAJOR_VERSION > 1)\n\n#include "c
         /png/s/);/, GTK_ICON_SIZE_BUTTON);/
         }' \
     -e 's/tmp_image.*rtz.svg/#ifdef GTK_TYPE_MENU_TOOL_BUTTON\n  &/' \
-    -e 's/gtk_container_add.*model_toolbar_rot_trans_toolbutton);/&\n#endif\n/' \
+    -e 's/set_tooltip .*model_toolbar_rot_trans_toolbutton.*;/&\n#endif\n/' \
     -e 's/ *GLADE_HOOKUP_OBJECT .*model_toolbar_rot_trans_toolbutton.*/#ifdef GTK_TYPE_MENU_TOOL_BUTTON\n  &\n#endif/' \
     gtk2-interface.c > gtk2-interface.tmp
 echo '#endif /* (GTK_MAJOR_VERSION > 1) */' >> gtk2-interface.tmp
