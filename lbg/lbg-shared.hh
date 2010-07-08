76c8b1620b8d459c3be6ecf1f023cf2470e71df0
b.lohkamp <b.lohkamp@f7134757-782b-0410-8fd1-2f704457d451>
2010-07-08 12:31:17 +0000

    add lbg-shared.hh (as instucted by Paul) add first draft of scripting manual/tutorial change default sigma in water search to 1.8 (as in tooltip)

---
+++ lbg/lbg-shared.hh
@@ -0,0 +1,24 @@
+
+
+namespace coot {
+
+
+   class bash_distance_t {
+   public:
+      double dist;
+      bool limited;
+      bash_distance_t() {
+	 limited = 0;
+	 dist = -1;
+      }
+      bash_distance_t(double d) {
+	 limited = 1;
+	 dist = d;
+      }
+      bool unlimited() const {
+	 return !limited;
+      } 
+      friend std::ostream& operator<< (std::ostream& s, const bash_distance_t &bd);
+   };
+   std::ostream& operator<< (std::ostream& s, const bash_distance_t &bd);
+}
