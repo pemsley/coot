feb89b435ca351ebaa73dc18c8d2ab4fbd2e5293
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2010-10-27 20:33:30 +0000

    Add missing files.

---
+++ src/coot-hydrogens.cc
@@ -0,0 +1,25 @@
+
+#ifdef MAKE_ENTERPRISE_TOOLS
+#include "rdkit-interface.hh"
+#endif
+
+#include <mmdb_manager.h>
+
+#include "protein-geometry.hh"
+#include "coot-hydrogens.hh"
+
+
+bool
+coot::add_hydrogens(CResidue *residue_p,
+		    const coot::dictionary_residue_restraints_t &restraints) {
+
+#ifdef MAKE_ENTERPRISE_TOOLS
+   return add_hydrogens_with_rdkit(residue_p, restraints);
+#else    
+   // return add_hydrogens_with_ccp4_tools(residue_p, restraints);
+   return 0;
+#endif   
+
+}
+
+
