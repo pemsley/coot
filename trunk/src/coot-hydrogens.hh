0dae4f9d39a89de7f80be9960cad27ef84f1bb9b
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2010-10-27 20:33:30 +0000

    Add missing files.

---
+++ trunk/src/coot-hydrogens.hh
@@ -0,0 +1,10 @@
+
+namespace coot {
+   // generic interface - using CCP4 tools.  Maybe this should be
+   // declared in src somewhere then?
+   // 
+   bool add_hydrogens(CResidue *residue_p,
+		      const dictionary_residue_restraints_t &restraints);
+
+
+}
