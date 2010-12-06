c9e0471cfbbe2fc845f5d71a3598c291ceedf6c7
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2010-12-06 15:12:30 +0000

    Add function match_ligand_atom_names(), a molecule_info_t function to use it    and the coot-utils function that acutally does it.  Currently hydrogens    are not in the name map (and hence do not have their names changed).

--- branches/release-0.6.2/src/molecule-class-info-residues.cc
+++ branches/release-0.6.2/src/molecule-class-info-residues.cc
@@ -141,3 +141,21 @@ molecule_class_info_t::assign_hetatms() {
    }
    return r;
 }
+
+void
+molecule_class_info_t::match_ligand_atom_names(const std::string &chain_id, int res_no, const std::string &ins_code,
+					       CResidue *res_ref) {
+
+   CResidue *res_mov = get_residue(chain_id, res_no, ins_code);
+
+   if (! res_mov) {
+      std::cout << "No residue for moving atom names:  " << chain_id << " " << res_no << " "  << ins_code
+		<< std::endl;
+      
+   } else { 
+      coot::graph_match_info_t gm = coot::graph_match(res_mov, res_ref, 0);
+      gm.match_names(res_mov);
+   } 
+
+} 
+
--- branches/release-0.6.2/src/testing.hh
+++ branches/release-0.6.2/src/testing.hh
@@ -84,6 +84,7 @@ int test_translate_close_to_origin();
 int test_flev_aromatics();
 int test_map_segmentation();
 int test_copy_cell_symm_orig_scale_headers();
+int test_residue_atom_renaming();
 
 // uses greg data test data
 int test_phi_psi_values();
