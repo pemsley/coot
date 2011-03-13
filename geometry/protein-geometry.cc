8f6e09a145193309a37bb2ebfcb340cc6f9543ab
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2011-03-13 23:27:29 +0000

    Merge rev 3428 to trunk.

--- geometry/protein-geometry.cc
+++ geometry/protein-geometry.cc
@@ -2543,8 +2543,33 @@ coot::protein_geometry::link_add_plane(const std::string &link_id,
 	 for (unsigned int ip=0; ip<dict_link_res_restraints[i].link_plane_restraint.size(); ip++) {
 	    if (dict_link_res_restraints[i].link_plane_restraint[ip].plane_id == plane_id) {
 	       ifound = 1;
-	       dict_link_res_restraints[i].link_plane_restraint[ip].atom_ids.push_back(atom_id);
-	       dict_link_res_restraints[i].link_plane_restraint[ip].atom_comp_ids.push_back(atom_comp_id);
+
+	       // now check the atom_id and comp_id of that atom are not already in this restraint.
+	       // Only add this atom to the plane restraint if there is not already an atom there.
+	       // If there is, then replace the atom in the restraint.
+	       //
+	       
+	       int found_atom_index = coot::protein_geometry::UNSET_NUMBER;
+
+	       for (int i_rest_at=0; i_rest_at<dict_link_res_restraints[i].link_plane_restraint[ip].n_atoms(); i_rest_at++) {
+		  if (dict_link_res_restraints[i].link_plane_restraint[ip].atom_ids[i_rest_at] == atom_id) { 
+		     if (dict_link_res_restraints[i].link_plane_restraint[ip].atom_comp_ids[i_rest_at] == atom_comp_id) {
+			found_atom_index = i_rest_at;
+			break;
+		     }
+		  } 
+	       }
+
+	       if (found_atom_index != coot::protein_geometry::UNSET_NUMBER) {
+		  // replace it then
+		  dict_link_res_restraints[i].link_plane_restraint[ip].atom_ids[found_atom_index] = atom_id;		  
+		  dict_link_res_restraints[i].link_plane_restraint[ip].atom_comp_ids[found_atom_index] = atom_comp_id;
+	       } else {
+
+		  dict_link_res_restraints[i].link_plane_restraint[ip].atom_ids.push_back(atom_id);
+		  dict_link_res_restraints[i].link_plane_restraint[ip].atom_comp_ids.push_back(atom_comp_id);
+	       }
+	       
 	       break;
 	    }
 	 } 
@@ -2556,7 +2581,7 @@ coot::protein_geometry::link_add_plane(const std::string &link_id,
 	 }
       }
    }
-
+   
    // It was not there.  This should only happen when plane restraints
    // are declared in the dictionary *before* the bonds (or angles).
    // Which is never, thanks to Alexei.
@@ -4272,6 +4297,7 @@ coot::protein_geometry::replace_monomer_restraints(std::string monomer_type,
       if (dict_res_restraints[i].comp_id == monomer_type) {
 	 dict_res_restraints[i] = mon_res;
 	 s = 1;
+	 break;
       }
    }
 
