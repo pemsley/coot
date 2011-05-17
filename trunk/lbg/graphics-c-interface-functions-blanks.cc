a027af356a68ffb85a9f6460db28a52d36825a72
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2011-05-17 08:50:23 +0000

    Add formal charges holder to lbg_molfile_molecule atoms. Spell equivalence correctly.

--- trunk/lbg/graphics-c-interface-functions-blanks.cc
+++ trunk/lbg/graphics-c-interface-functions-blanks.cc
@@ -1,13 +1,14 @@
 
 extern "C" {
    
-/* \brief display/undisplay the given additional representation  */
-void set_show_additional_representation(int imol, int representation_number, int on_off_flag) {}
+   /* \brief display/undisplay the given additional representation  */
+   void set_show_additional_representation(int imol, int representation_number, int on_off_flag) {}
+   
+   /* \brief display/undisplay all the additional representations for the given molecule  */
+   void set_show_all_additional_representations(int imol, int on_off_flag) {}
 
-/* \brief display/undisplay all the additional representations for the given molecule  */
-void set_show_all_additional_representations(int imol, int on_off_flag) {}
-
-void all_additional_representations_off_except(int imol, int representation_number) {}
+   void all_additional_representations_off_except(int imol, int representation_number,
+						  short int ball_and_sticks_off_too_flag) {}
 
 }
 
