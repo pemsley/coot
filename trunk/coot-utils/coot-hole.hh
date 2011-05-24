c0f6a86f3f65341410269c4329aea7b7127b0772
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2011-05-24 08:57:24 +0000

    Add hole/channel finding class - preliminary version.

---
+++ trunk/coot-utils/coot-hole.hh
@@ -0,0 +1,33 @@
+
+#include "mmdb_manager.h"
+#include "clipper/core/coords.h"
+
+#include "protein-geometry.hh"
+
+namespace coot {
+
+   class hole {
+      CMMDBManager *mol;
+      int radius_handle; // set by assign_vdw_radii;
+      clipper::Coord_orth from_pt;
+      clipper::Coord_orth to_pt;
+      clipper::Coord_orth v_hat;
+      
+      void make_atom_selection(int selhnd, const clipper::Coord_orth &pt,
+			       double radius_prev) const;
+      std::pair<clipper::Coord_orth, double>
+      optimize_point(const clipper::Coord_orth &pt, int selhnd);
+      double sphere_size(const clipper::Coord_orth &pt, int selhnd) const;
+      void assign_vdw_radii(const coot::protein_geometry &geom);
+      void write_probe_path(const std::vector<std::pair<clipper::Coord_orth, double> > &probe_path) const;
+      std::pair<double, double> x_y(const double &r, const double &theta) const;
+      
+   public:
+      hole(CMMDBManager *mol, int selHnd,
+	   const clipper::Coord_orth &from_pt,
+	   const clipper::Coord_orth &to_pt,
+	   const coot::protein_geometry &geom_in);
+      
+      void generate(); 
+   };
+} 
