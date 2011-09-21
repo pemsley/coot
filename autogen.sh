8d2d3d98407c6b3b244442aaeba451ff1d8f5323
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2011-09-21 17:01:18 +0000

    Clean up autogen.sh.  Don't use old macros.

--- autogen.sh
+++ autogen.sh
@@ -19,7 +19,8 @@ else
    # but we can only add the directories if they exist, otherwise aclocal barfs (sigh)
 
     aclocal_extras=
-    dir_list="$HOME/libtool/share/aclocal $HOME/gettext/share/aclocal $HOME/glade/share/aclocal $HOME/autobuild/Linux/Coot-0.1/share/aclocal $HOME/autotools/share/aclocal-1.9 $HOME/automake/share/aclocal-1.9 $HOME/autobuild/$OS-$hostname/share/aclocal $HOME/autobuild/$OS-$hostname-pre-release/share/aclocal $HOME/autobuild/$OS-$hostname-pre-release-gtk2/share/aclocal /usr/share/aclocal $HOME/test/gtkglext/share/aclocal $HOME/build/share/aclocal $HOME/autobuild/Linux-$HOST/share/aclocal $HOME/gtk-1.2/share/aclocal $HOME/gtk-1/share/aclocal"
+    # dir_list="$HOME/libtool/share/aclocal $HOME/gettext/share/aclocal $HOME/glade/share/aclocal $HOME/autobuild/Linux/Coot-0.1/share/aclocal $HOME/autotools/share/aclocal-1.9 $HOME/automake/share/aclocal-1.9 $HOME/autobuild/$OS-$hostname/share/aclocal $HOME/autobuild/$OS-$hostname-pre-release/share/aclocal $HOME/autobuild/$OS-$hostname-pre-release-gtk2/share/aclocal /usr/share/aclocal $HOME/test/gtkglext/share/aclocal $HOME/build/share/aclocal $HOME/autobuild/Linux-$HOST/share/aclocal $HOME/gtk-1.2/share/aclocal $HOME/gtk-1/share/aclocal"
+    dir_list="$HOME/autobuild/Linux/Coot-0.1/share/aclocal $HOME/test/gtkglext/share/aclocal $HOME/build/share/aclocal"
 
     for dir in $dir_list
     do 
