e704f291c1ae5d3307769445dc8236098d1f9fc9
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2011-12-04 14:30:14 +0000

    Update generate-svn-revision-cc.sh to handle non-svn repo.

--- generate-svn-revision-cc.sh
+++ generate-svn-revision-cc.sh
@@ -1,5 +1,14 @@
 
-svn -qu status | tail -1 | awk '
+
+revline=$(svn -qu status | tail -1)
+
+if [ -z "$revline" ] ; then 
+   echo no svn status, using html
+   wget --quiet -O svn-rev.html http://coot.googlecode.com/svn/
+   revline=$(awk '/Revision/ {if ($1 == "<h2>coot") { sub(":", "", $4); print $4 } }' svn-rev.html)
+fi
+
+echo $revline | awk '
 BEGIN {svn_revision_cc = "src/svn-revision.cc"}
 {
 # Adding this extern "C" is just magic.  Baah. 
@@ -13,5 +22,9 @@ print "}"                      > svn_revision_cc;
 print "}"                      > svn_revision_cc;
 print ""                       > svn_revision_cc;  
 }'
-tail -4 src/svn-revision.cc | head -1 | awk '{split($2, arr, ";"); print arr[1]}'
+if test ! -e src/svn-revision.cc ; then
+   echo Missing src/svn-revision.cc
+else 
+   tail -4 src/svn-revision.cc | head -1 | awk '{split($2, arr, ";"); print arr[1]}'
+fi
 
