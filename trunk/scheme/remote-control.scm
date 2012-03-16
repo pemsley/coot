fa7e9d437c09a22d0aeb52cec6fb5307d8044a34
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2012-03-16 13:35:49 +0000

    Bug fixes for scheme-based remote control handling.

--- trunk/scheme/remote-control.scm
+++ trunk/scheme/remote-control.scm
@@ -131,8 +131,10 @@
 
   (define evaluate-char-list
     (lambda (read-bits)
+
+      ;; (format #t "evaluate-char-list given ~s~%" read-bits)
       
-      (let ((s (list->string (reverse read-bits))))
+      (let ((s (list->string read-bits)))
 	;; is it python?
 	(if (and (>= (string-length s) 8) ; (checked in order)
 		 (string-match "# PYTHON" (substring s 0 8)))
@@ -157,28 +159,36 @@
   (define strip-tail 
     (lambda (read-bits)
 
-      (if (> (length read-bits) (string-length end-transmition-string))
-	  (let loop ((read-bits read-bits)
-		     (match-bits (string->list end-transmition-string)))
-	    (cond 
-	     ((null? match-bits) read-bits)
-	     (else 
-	      (loop (cdr read-bits) (cdr match-bits)))))
-	  '())))
+      (let ((l (length read-bits))
+	    (etsl (string-length end-transmition-string)))
+	(if (> l etsl)
+	  (list-head read-bits (- l etsl))
+	  '()))))
 
   ;; return #t or #f
-  (define (hit-end-transmit? read-bits end-transmition-string)
+  ;; 
+  ;; read-bits are passed in reverse order (i.e. with the newline (if
+  ;; it exits) at the the beginning)
+  ;; 
+  (define (hit-end-transmit-inner? read-bits end-transmition-string)
     (if (< (length read-bits)
 	   (string-length end-transmition-string))
-	#f
-	(let loop ((read-chars read-bits)
-		   (match-chars (reverse (string->list end-transmition-string))))
-	  (cond 
-	   ((null? match-chars) #t)
-	   ((not (eq? (car match-chars)
-		      (car read-chars))) #f)
-	   (else
-	    (loop (cdr read-chars) (cdr match-chars)))))))
+	(begin
+	  #f)
+	(begin
+	  (let* ((char-list-ref (reverse (string->list end-transmition-string)))
+		 (n-chars (length char-list-ref))
+		 (char-list-test (list-head read-bits n-chars)))
+	    (let ((ls-1 char-list-ref)
+		  (ls-2 char-list-test))
+	 ;;      (format #t "hit-end test: comparing.... \n     ~s and \n     ~s~%" ls-1 ls-2)
+	      (equal? ls-1 ls-2))))))
+	    
+  ;; 
+  (define (hit-end-transmit? read-bits end-transmition-string)
+    (let ((var (hit-end-transmit-inner? read-bits end-transmition-string)))
+      ;; (format #t "hit-end-transmit? on ~s returns ~s~%" read-bits var)
+      var))
 
 
   ; (format #t "listening...~%")
@@ -190,11 +200,12 @@
 	;; (format #t "nothing on the line...~%")
 	#t)
       (begin
-	(format #t "there *is* a ready-char!~%" soc)
-	(format #t "about to read from socket soc: ~s~%" soc)
+	;; (format #t "there *is* a ready-char!~%" soc)
+	;; (format #t "about to read from socket soc: ~s~%" soc)
 	(let f ((c (read-char soc))
 		(read-bits '()))
-	  (format #t "socket read char ~s~%" c)
+	  ;; (format #t "socket read char ~s~%" c)
+	  ;; (format #t "read-bits: ~s~%" read-bits)
 	  (cond
 	   ((eof-object? c) (format #t "server gone\n") #f)
 	   ((hit-end-transmit? (cons c read-bits)
@@ -202,9 +213,13 @@
 	    (format #t "  ~s found in ~s~%" 
 		    end-transmition-string
 		    (list->string (reverse (cons c read-bits))))
-	    (evaluate-char-list (strip-tail (cons c read-bits)))
-	    (format #t "=== char list was evaluated... back to listen-coot-listener-socket-v3~%") 
-	    #t)
+	    (let* ((chars (reverse (cons c read-bits)))
+		   (tail-stripped (strip-tail chars)))
+	      ;; (format #t "chars: ~s~%" chars) 
+	      ;; (format #t "tail-stripped: ~s~%" tail-stripped)
+	      (evaluate-char-list tail-stripped)
+	      ;; (format #t "=== char list was evaluated... back to listen-coot-listener-socket-v3~%") 
+	      #t))
 
 	   (else 
 	    ; (format #t "loop f, reading new char...~%")
