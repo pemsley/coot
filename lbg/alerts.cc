4c1ada5d3c71b87722ca4d9244434e3e314d1cf3
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2012-06-14 08:39:47 +0000

    Name alerts where I can, add a text label on the gui for the alert name. Show alerts. initialize draw_flev_annotations_flag. Improve memory management for query molecule. Add atom index properties to atoms (as strings).

--- lbg/alerts.cc
+++ lbg/alerts.cc
@@ -5,27 +5,31 @@
 
 // return a vector of alert matches
 // 
-std::vector<std::pair<std::string, RDKit::MatchVectType> >
+std::vector<lbg_info_t::alert_info_t>
 lbg_info_t::alerts(const RDKit::ROMol &mol) const {
 
-   std::vector<std::pair<std::string, RDKit::MatchVectType> > v;
+   std::vector<lbg_info_t::alert_info_t> v;
 
-   std::vector<std::string> patterns = alert_smarts();
+   std::vector<std::pair<std::string, std::string> > patterns = alert_smarts();
    for (unsigned int ipat=0; ipat<patterns.size(); ipat++) { 
       // std::cout << "checking pattern " << patterns[ipat] << std::endl;
-      RDKit::ROMol *query = RDKit::SmartsToMol(patterns[ipat]);
+      RDKit::ROMol *query = RDKit::SmartsToMol(patterns[ipat].first);
       std::vector<RDKit::MatchVectType>  matches;
       bool recursionPossible = true;
       bool useChirality = true;
       bool uniquify = true;
-      int matched = RDKit::SubstructMatch(mol,*query,matches,uniquify,recursionPossible, useChirality);
+      int matched = RDKit::SubstructMatch(mol, *query, matches, uniquify,
+					  recursionPossible, useChirality);
       if (matched) { 
 	 // std::cout << "...... ALERT!" << std::endl;
 	 for (unsigned int im=0; im<matches.size(); im++) {
-	    std::pair<std::string, RDKit::MatchVectType> p(patterns[ipat], matches[im]);
-	    v.push_back(p);
+	    alert_info_t alert(patterns[ipat].first,
+			       patterns[ipat].second,
+			       matches[im]);
+	    v.push_back(alert);
 	 }
       }
+      delete query;
    }
    return v;
 }
