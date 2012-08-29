d74513c4e6c5005221894096d28d4691cd2cbbec
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2012-08-29 11:13:31 +0000

    Use "svn info" to get revision number.

--- generate-svn-revision-cc.sh
+++ generate-svn-revision-cc.sh
@@ -1,27 +1,24 @@
+# -*-shell-script-mode-*-
 
+rev_no=$(svn info | awk '/^Revision:/{print $NF}')
 
-revline=$(svn -qu status | tail -1)
-
-if [ -z "$revline" ] ; then 
+if [ -z "$rev_no" ] ; then 
+   # should never happen
    echo no svn status, using html
    wget --quiet -O svn-rev.html http://coot.googlecode.com/svn/
-   revline=$(awk '/Revision/ {if ($1 == "<h2>coot") { sub(":", "", $4); print $4 } }' svn-rev.html)
+   rev_no=$(awk '/Revision/ {if ($1 == "<h2>coot") { sub(":", "", $4); print $4 } }' svn-rev.html)
 fi
 
-echo $revline | awk '
-BEGIN {svn_revision_cc = "src/svn-revision.cc"}
-{
-# Adding this extern "C" is just magic.  Baah. 
-# If it is not here then (in coot_version()):
-#    c-interface.cc:174: undefined reference to "svn_revision"
-#
+awk -v rev_no=$rev_no '
+BEGIN {svn_revision_cc = "src/svn-revision.cc"
 print "extern \"C\" {"         > svn_revision_cc 
 print "int svn_revision() { "  > svn_revision_cc 
-print "   return ", $NF ";"    > svn_revision_cc 
-print "}"                      > svn_revision_cc;
-print "}"                      > svn_revision_cc;
-print ""                       > svn_revision_cc;  
+print "   return ", rev_no ";" > svn_revision_cc 
+print "}"                      > svn_revision_cc
+print "}"                      > svn_revision_cc
+print ""                       > svn_revision_cc  
 }'
+
 if test ! -e src/svn-revision.cc ; then
    echo Missing src/svn-revision.cc
 else 
