5bbb0e02aeebeafe76114aebd0ce7ebc065287f8
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2013-01-22 21:58:26 +0000

    Reference for 0xx are new from Fei. Add trace_path() methods to find all rings for atoms that are in fused rings. Remove debugging.

--- trunk/lidia-core/cod-types.cc
+++ trunk/lidia-core/cod-types.cc
@@ -3,6 +3,8 @@
 
 #include "coot-utils.hh"
 #include "cod-types.hh"
+#include "GraphMol/Rings.h"
+#include "GraphMol/RingInfo.h"
 
 // can throw a std::runtime_error
 std::vector<std::string>
@@ -38,22 +40,196 @@ cod::get_cod_atom_types(RDKit::ROMol &rdkm) {
 	 }
       }
    }
+
+   boost::dynamic_bitset<> fusDone(n_rings);
+   int curr = 0;
+   RDKit::INT_INT_VECT_MAP neighMap; // std::map<int, std::vector<int> >
+   RingUtils::makeRingNeighborMap(atomRings, neighMap);
+   RDKit::INT_VECT fused;
+   while (curr < n_rings) {
+      fused.clear();
+      RingUtils::pickFusedRings(curr, neighMap, fused, fusDone);
+      std::vector<std::vector<int> > fused_rings;
+      fused_rings.reserve(fused.size());
+      for (RDKit::INT_VECT_CI rid = fused.begin(); rid != fused.end(); ++rid) {
+	 fused_rings.push_back(atomRings[*rid]);
+      }
+
+      if (fused_rings.size() > 1)
+	 handle_bigger_rings_from_fused_rings(rdkm, fused_rings);
+      curr++;
+   }
+   
    
    RDKit::ROMol::AtomIterator ai;
    for(ai=rdkm.beginAtoms(); ai!=rdkm.endAtoms(); ai++) {
-      
-      std::string name;
-      (*ai)->getProp("name", name);
-      std::cout << "\n%%%%%%%%%%%%%%%%%\n%%%%% new parent: " << name << "\n"
-		<< "%%%%%%%%%%%%%%%%%" << std::endl;
-      
       std::string s = get_cod_atom_type(0, *ai, rdkm);
       v.push_back(s);
    }
+   
    if (v.size() != rdkm.getNumAtoms())
       throw std::runtime_error("mismatch size in get_cod_atom_types()");
+      
    return v;
 }
+
+// ret
+void
+cod::handle_bigger_rings_from_fused_rings(RDKit::ROMol &rdkm,
+					  const std::vector<std::vector<int> > &fused_rings) {
+
+
+   // first make a connection "tree".  What is bonded to what (in the
+   // rings).  bond map is a list of atom indices that are bonded to
+   // the key (an atom index)
+   //
+   std::map<int, std::vector<int> > bond_map;
+   std::map<int, std::vector<int> >::iterator it;
+
+   for (unsigned int iat=0; iat<rdkm.getNumAtoms(); iat++) {
+      if (is_ring_member(iat, fused_rings)) {
+	 RDKit::Atom *this_at = rdkm[iat].get();
+	 unsigned int idx_c = this_at->getIdx();
+	 RDKit::ROMol::ADJ_ITER nbrIdx, endNbrs;
+	 boost::tie(nbrIdx, endNbrs) = rdkm.getAtomNeighbors(this_at);
+	 while(nbrIdx != endNbrs) {
+	    if (is_ring_member(*nbrIdx, fused_rings)) { 
+	       RDKit::ATOM_SPTR at = rdkm[*nbrIdx];
+	       RDKit::Bond *bond = rdkm.getBondBetweenAtoms(idx_c, *nbrIdx);
+	       if (bond) {
+		  it = bond_map.find(iat);
+		  if (it == bond_map.end()) {
+		     // not there, add it.
+		     bond_map[iat].push_back(*nbrIdx);
+		  } else {
+		     std::vector<int> &v = it->second;
+		     if (std::find(v.begin(), v.end(), *nbrIdx) == v.end())
+			v.push_back(*nbrIdx);
+		  }
+	       }
+	    }
+	    nbrIdx++;
+	 }
+      }
+   }
+
+   // check the bond map (debugging output)
+   //
+   if (0) { 
+      for(it=bond_map.begin(); it!=bond_map.end(); it++) {
+	 try {
+	    std::string name;
+	    rdkm[it->first]->getProp("name", name);
+	    std::cout << "   " << it->first << " " << name <<  " : ";
+	    for (unsigned int i=0; i<it->second.size(); i++)
+	       std::cout << it->second[i] << " ";
+	    std::cout << " :::: ";
+	    for (unsigned int i=0; i<it->second.size(); i++) {
+	       rdkm[it->second[i]]->getProp("name", name);
+	       std::cout << name << " ";
+	    }
+	    std::cout << std::endl;
+	 }
+	 catch (KeyErrorException) {
+	 }
+      }
+   }
+
+
+   unsigned int n_max_bonds = 6; // max ring size of interest (for COD)
+   
+   // Return all the paths by which we get back to idx (using upto
+   // (and including) n_max_bonds bonds (include backward paths also).
+   // 
+   for(it=bond_map.begin(); it!=bond_map.end(); it++) {
+      unsigned int idx = it->first;
+      // this returns all the paths! (in forward and backward directions)
+      std::vector<std::vector<int> > paths = trace_path(idx, bond_map, n_max_bonds);
+      if (paths.size() > 1) { 
+	 std::vector<int> path_sizes(paths.size());
+	 for (unsigned int ii=0; ii<paths.size(); ii++)
+	    path_sizes[ii] = paths[ii].size();
+	 std::sort(path_sizes.begin(), path_sizes.end());
+	 // dangerous interger arithmetic?
+	 // we want every other path, not 5(f), 5(b), 6(f), 6(b), so
+	 // sort them (smallest first) and take every other one.
+	 std::vector<int> filtered_path_sizes(path_sizes.size()/2);
+	 for (unsigned int ii=0; ii<path_sizes.size(); ii++) // every other
+	    filtered_path_sizes[ii] = path_sizes[ii*2];
+	 rdkm[it->first]->setProp("ring", filtered_path_sizes);
+      }
+   }
+}
+
+bool
+cod::is_ring_member(unsigned int iat,   const std::vector<std::vector<int> > &fused_rings) {
+
+   for (unsigned int ir=0; ir<fused_rings.size(); ir++) { 
+      for (unsigned int ii=0; ii<fused_rings[ir].size(); ii++) {
+	 if (fused_rings[ir][ii] == iat)
+	    return true;
+      }
+   }
+   return false;
+}
+
+std::vector<std::vector<int> >
+cod::trace_path(unsigned int idx,
+		const std::map<int, std::vector<int> > &bond_map,
+		unsigned int n_max_bonds) {
+
+   std::vector<int> in_path;
+   return trace_path(idx, in_path, idx, bond_map, n_max_bonds);
+}
+
+std::vector<std::vector<int> > 
+cod::trace_path(unsigned int idx,
+		std::vector<int> in_path_indices,
+		unsigned int target_idx,
+		const std::map<int, std::vector<int> > &bond_map,
+		unsigned int level) {
+
+   std::vector<std::vector<int> > vr;
+   if (level == 0) {
+      return vr; // empty
+   } else {
+
+      in_path_indices.push_back(idx);
+      std::map<int, std::vector<int> >::const_iterator it_1 = bond_map.find(idx);
+      std::vector<int> neighbs = it_1->second;
+      for (unsigned int in=0; in<neighbs.size(); in++) {
+
+	 bool do_recursion = false; // clumsy setting of "shall we look deeper"
+
+	 if (neighbs[in] == target_idx) {
+	    if (in_path_indices.size() > 2) { // not just out and back
+	       vr.push_back(in_path_indices);
+	       return vr;
+	    }
+	 } else { 
+	    if (std::find(in_path_indices.begin(), in_path_indices.end(), neighbs[in]) ==
+		in_path_indices.end())
+	       do_recursion = true; // only if the forward neighbor is not in current path shall
+	                            // we continue deeper.
+	 }
+	 
+	 if (do_recursion) {
+	    std::vector<std::vector<int> > v = trace_path(neighbs[in],
+							  in_path_indices,
+							  target_idx,
+							  bond_map,
+							  level-1);
+		  
+	    if (v.size() > 0)
+	       for (unsigned int i=0; i<v.size(); i++)
+		  vr.push_back(v[i]);
+	 }
+      }
+   }
+   return vr;
+}
+
+
 
 
 // can throw a std::runtime_error
@@ -145,18 +321,9 @@ std::string
 cod::make_cod_type(const std::string &atom_ele, const std::vector<std::string> &neighbour_types,
 		   int level) {
 
-   bool debug = true;
    std::string s;
    std::vector<std::string> n = sort_neighbours(neighbour_types, level);
 
-   if (debug) { 
-      std::cout << "      --- make_cod_type called with ele: " << atom_ele << "  "
-		<< neighbour_types.size() << " sorted-neighbs ";
-      for (unsigned int i=0; i<neighbour_types.size(); i++)
-	 std::cout << "  " << n[i];
-      std::cout << "  level: " << level << std::endl;
-   }
-   
    if (n.size() == 0) {
       s = atom_ele;
    } else {
@@ -243,9 +410,6 @@ cod::make_cod_type(const std::string &atom_ele, const std::vector<std::string> &
 	 }
       }
    }
-   if (debug)
-      std::cout << "      --- make_cod_type returns " << s << std::endl;
-      
    return s;
 } 
 
