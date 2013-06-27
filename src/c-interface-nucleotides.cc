51c2964415ab88a67fe74eee3b186dc5f789c9b3
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2013-06-27 13:49:20 +0000

    Centre nucleic acid molecule after creation.

--- src/c-interface-nucleotides.cc
+++ src/c-interface-nucleotides.cc
@@ -242,21 +242,28 @@ int ideal_nucleic_acid(const char *RNA_or_DNA, const char *form,
 	    ir.use_v3_names();
 	    CMMDBManager *mol = ir.make_molecule();
 
-	    if (mol) { 
-	       int imol = graphics_info_t::create_molecule();
-	       istat = imol;
-	       std::string label = "Ideal-" + form_str;
-	       label += "-form-";
-	       label += RNA_or_DNA_str;
-	       atom_selection_container_t asc = make_asc(mol);
-	       graphics_info_t::molecules[imol].install_model(imol, asc, label, 1);
-	       graphics_info_t::molecules[imol].translate_by(here_x, here_y, here_z);
-	       graphics_draw();
-	       if (graphics_info_t::go_to_atom_window) {
-		  graphics_info_t g;
-		  g.update_go_to_atom_window_on_new_mol();
-		  g.update_go_to_atom_window_on_changed_mol(imol);
-	       }
+	    if (mol) {
+	       std::pair<bool, clipper::Coord_orth> cm = coot::centre_of_molecule(mol);
+	       if (cm.first) { 
+		  clipper::Coord_orth mc = cm.second; // just for alias
+		  int imol = graphics_info_t::create_molecule();
+		  istat = imol;
+		  std::string label = "Ideal-" + form_str;
+		  label += "-form-";
+		  label += RNA_or_DNA_str;
+		  atom_selection_container_t asc = make_asc(mol);
+		  graphics_info_t::molecules[imol].install_model(imol, asc, label, 1);
+		  graphics_info_t::molecules[imol].translate_by(here_x-mc.x(), here_y-mc.y(), here_z-mc.z());
+		  graphics_draw();
+		  if (graphics_info_t::go_to_atom_window) {
+		     graphics_info_t g;
+		     g.update_go_to_atom_window_on_new_mol();
+		     g.update_go_to_atom_window_on_changed_mol(imol);
+		  }
+	       } else {
+		  std::cout << "WARNING:: ideal_nucleic_acid()::something bad happened "
+			    << std::endl;
+	       } 
 	    }
 	 } else {
 	    std::string s("WARNING:: Can't proceed with Idea RNA - no standard residues!");
