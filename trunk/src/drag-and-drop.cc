dc0772963fafd26e32cdc23754e19c5b74918c86
b.lohkamp <b.lohkamp@f7134757-782b-0410-8fd1-2f704457d451>
2014-05-05 18:56:17 +0000

    Rework drag and drop for files and urls to deal with spaces and allow         multiple files to be dragged too.

--- trunk/src/drag-and-drop.cc
+++ trunk/src/drag-and-drop.cc
@@ -51,17 +51,49 @@ on_drag_data_received (GtkWidget *widget,
    
    // Deal with what the source sent over
    if((selection_data != NULL) && (selection_data-> length >= 0)) {
-      if (target_type == TARGET_STRING) {
-	 std::string uri_string = (gchar*)selection_data-> data;
-	 dnd_success = handle_drag_and_drop_string(uri_string);
-     delete_selection_data = TRUE;
-      } 
+      std::string uri_string;
+      if (target_type == TEXT_URL) {
+         // we have an url to deal with
+         uri_string = (gchar *)selection_data-> data;
+         dnd_success = handle_drag_and_drop_string(uri_string);
+      }
+      else if (target_type == TEXT_URI) {
+         // we have a text uri (file!?)
+         gchar **uris;
+         gint i = 0;
+         gchar *res;
+         uris = g_uri_list_extract_uris((gchar*)selection_data-> data);
+         if (uris) {
+            while (uris[i] != 0) {
+               res = g_filename_from_uri(uris[i], NULL, NULL);
+               i++;
+               if (res != NULL) {
+                  // everything fine
+                  dnd_success = handle_drag_and_drop_single_item((gchar *)res);
+               } else {
+                  // not a file (shouldnt necessary happen - urls are dealt above and 
+                  // simple strings below
+                  uri_string = (gchar *)selection_data-> data;
+                  dnd_success = handle_drag_and_drop_string(uri_string);
+               }
+            }
+         }
+         g_free(res);
+         g_strfreev(uris);
+      }
+      else if (target_type == TARGET_STRING) {
+         // simple string could call an extra function here too
+         uri_string = (gchar *)selection_data-> data;
+         dnd_success = handle_drag_and_drop_string(uri_string);
+      }
+      delete_selection_data = TRUE;
+      
    }
    gtk_drag_finish (context, dnd_success, delete_selection_data, time);
 }
 
 
-//! \brief handle the string that get when a file or URL is dropped.
+//! \brief handle the string that get when an URL (or text from an url) is dropped.
 //
 // uri string can be a concatenation of string with \ns between them
 // (and a \n to end)
@@ -71,39 +103,13 @@ int handle_drag_and_drop_string(const std::string &uri_in) {
    int handled = FALSE;
    bool tried_already = false;
    std::string uri = uri_in;
-   // std::cout << "handle_drag_and_drop_string() :" << uri<< ":" << std::endl;
-   // was it a file://xx?
-   std::string url;
-   std::string::size_type pos = uri.find_first_of('\n');
-   while (pos != std::string::npos) {
-      url = uri.substr(0, pos-1); // front part
-      uri = uri.substr(pos+1); // back part
-      // std::cout << "Now URI is " << uri.size() << ":" << uri << ":" << std::endl;
-      // std::cout << "Now URL is " << url.size() << ":" << url << ":" << std::endl;
-      if (url.length() > 7) {
-	 if (url.substr(0,7) == "file://") {
-	    // std::cout << "---:" << url << ": was a file " << std::endl;
-	    std::string file;
-#ifdef WINDOWS_MINGW
-	    file = url.substr(8);
-#else
-	    file = url.substr(7);
-#endif
-	    handled = handle_drag_and_drop_single_item(file);
-	    tried_already = true;
-	    std::cout << "setting tried_already here 1" << std::endl;
-	 }
-      }
-      pos = uri.find_first_of('\n');
-      
-   }
+   std::string url = uri_in;
 
    if (! tried_already) {
       // OK, was it an HTTP type string?
-      std::string url = uri_in;
-      if (url.length() > 8) {
+      if (url.length() > 9) {
 
-	 if (url.substr(0,7) == "http://") {
+	 if (url.substr(0,7) == "http://" || url.substr(0,8) == "https://") {
 	    tried_already = true;
 	    int l = url.length();
 	    if (url[l-1] == '\n') { 
@@ -115,7 +121,7 @@ int handle_drag_and_drop_string(const std::string &uri_in) {
 	       std::string url_file_name_file = url;
 
 	       std::string ext = coot::util::file_name_extension(url);
-
+               
 	       if (ext == ".png") {
 		  // special rule - convert the url of the png to that
 		  // of an accession code.
@@ -140,6 +146,7 @@ int handle_drag_and_drop_string(const std::string &uri_in) {
 		  }
 
 	       } else {
+
 		  // it was coords or mtz - we presume
 		  std::string::size_type pos = url.find_last_of('/');
 		  if (pos != std::string::npos) {
@@ -190,6 +197,7 @@ int handle_drag_and_drop_single_item(const std::string &file_name) {
    }
 
    if (handled == FALSE) { 
+      std::string ext_tmp = coot::util::file_name_extension(file_name);
       if (file_type_coords(file_name.c_str())) {
 	 int imol = read_pdb(file_name.c_str());
 	 if (is_valid_model_molecule(imol))
