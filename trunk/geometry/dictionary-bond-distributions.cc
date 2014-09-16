12425004ac7fe54026a728d5b47ff2c02401bf84
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2014-09-16 08:36:22 +0000

    Add map to vector conversion for sorting output by counts.

--- trunk/geometry/dictionary-bond-distributions.cc
+++ trunk/geometry/dictionary-bond-distributions.cc
@@ -2,9 +2,46 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 #include "protein-geometry.hh"
 
 #include "analysis/stats.hh"
+
+class energy_type_key_t {
+
+public:
+   std::string energy_type_1; 
+   std::string energy_type_2;
+   std::string bond_type;
+   energy_type_key_t(const std::string &e1,
+		     const std::string &e2,
+		     const std::string &bond_type_in) {
+      bond_type = bond_type_in;
+      energy_type_1 = e1;
+      energy_type_2 = e2;
+      if (energy_type_2 < energy_type_1) {
+	 energy_type_1 = e2;
+	 energy_type_2 = e1;
+      }
+   }
+   energy_type_key_t() {} // needed?
+   bool operator<(const energy_type_key_t &k) const {
+      if (k.bond_type < bond_type) { 
+	 return true;
+      } else {
+	 if (k.energy_type_2 < energy_type_2) {
+	    return true;
+	 } else {
+	    return (k.energy_type_1 < energy_type_1);
+	 }
+      } 
+   }
+};
+
+bool bonds_vec_sorter(const std::pair<energy_type_key_t, std::vector<double> > &k1,
+		      const std::pair<energy_type_key_t, std::vector<double> > &k2) {
+   return (k1.second.size() < k2.second.size());
+} 
 
 void
 write_bond_lengths(const std::string &file_name,
@@ -35,7 +72,8 @@ int main(int argc, char **argv) {
       read_number++;
    }
 
-   std::map<std::pair<std::string, std::string>, std::vector<double> > bonds;
+   // std::map<std::pair<std::string, std::string>, std::vector<double> > bonds;
+   std::map<energy_type_key_t, std::vector<double> > bonds;
 
    std::cout << "Examining the bonds in the " << geom.size() << " entries."<< std::endl;
    for (unsigned int i=0; i<geom.size(); i++) {
@@ -44,37 +82,46 @@ int main(int argc, char **argv) {
 	 const coot::dict_bond_restraint_t &br = r.bond_restraint[ibond];
 	 if (!(r.is_hydrogen(br.atom_id_1_4c())) &&
 	     !(r.is_hydrogen(br.atom_id_2_4c()))) { 
-	    std::pair<std::string, std::string> p(r.type_energy(br.atom_id_1_4c()),
-						  r.type_energy(br.atom_id_2_4c()));
-	    bonds[p].push_back(br.value_dist());
+	    energy_type_key_t k(r.type_energy(br.atom_id_1_4c()),
+				r.type_energy(br.atom_id_2_4c()),
+				br.type());
+	    bonds[k].push_back(br.value_dist());
 	 }
       }
    }
 
-   std::map<std::pair<std::string, std::string>, std::vector<double> >::const_iterator it;
-   unsigned int min_counts = 100;
-   unsigned int n_found = 0;
-   
+   // convert to vector for sorting by N (counts)
+   //
+   std::map<energy_type_key_t, std::vector<double> >::const_iterator it;
+   std::vector<std::pair<energy_type_key_t, std::vector<double> > > bonds_vec(bonds.size());
+   std::vector<std::pair<energy_type_key_t, std::vector<double> > >::const_iterator itv;
+
    for (it=bonds.begin(); it!=bonds.end(); it++) {
-      if (it->second.size() > min_counts) {
+      std::pair<energy_type_key_t, std::vector<double> > p(it->first, it->second);
+      bonds_vec.push_back(p);
+   }
+
+   // now sort bonds_vec how you like:
+   std::sort(bonds_vec.begin(), bonds_vec.end(), bonds_vec_sorter);
+   
+
+   unsigned int min_counts = 10;
+   unsigned int n_found = 0;
+
+   for (itv=bonds_vec.begin(); itv!=bonds_vec.end(); itv++) {
+      
+      if (itv->second.size() > min_counts) {
 
 	 n_found++;
 	 
-// 	 std::cout << it->first.first << " " << it->first.second << " "
-// 		   << it->second.size() << std::endl;
+	 std::string stub = itv->first.energy_type_1 + "-" + itv->first.energy_type_2;
 
-	 std::string stub = it->first.first + "-" + it->first.second;
-
-	 // fold in symmetry
-	 if (it->first.second < it->first.first)
-	    stub = it->first.second + "-" + it->first.first;
-	 
 	 std::string file_name = stub + ".tab";
-	 write_bond_lengths(file_name, it->second);
+	 write_bond_lengths(file_name, itv->second);
 
 	 coot::stats::single stats;
-	 for (unsigned int i=0; i<it->second.size(); i++)
-	    stats.add(it->second[i]);
+	 for (unsigned int i=0; i<itv->second.size(); i++)
+	    stats.add(itv->second[i]);
 	 double var = stats.variance();
 
 
@@ -91,7 +138,9 @@ int main(int argc, char **argv) {
 	 std::cout << std::setprecision(7);
 	 std::cout.setf(std::ios::fixed, std::ios::floatfield);
 	 
-	 std::cout << std::setw(9) << stub << "   N: " << stats.size()
+	 std::cout << std::setw(9) << stub
+		   << "  "       << std::setw(6) << itv->first.bond_type
+		   << "   N: "   << std::setw(4) << std::right << stats.size()
 		   << "  mean: " << std::setw(8) << stats.mean()
 		   << "   s: "   << std::setw(9) << sqrt(var)
 		   << "   k: "   << std::setw(7) << std::right << std::setprecision(3)
