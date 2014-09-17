4b2c7938db08535a22170b2d2a1ca5a6270e7400
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2014-09-17 11:44:02 +0000

    Add atom-name info to bond lengths - so that they too can be written out    in the bond tab files.

--- geometry/dictionary-bond-distributions.cc
+++ geometry/dictionary-bond-distributions.cc
@@ -24,34 +24,83 @@ public:
 	 energy_type_2 = e1;
       }
    }
-   energy_type_key_t() {} // needed?
+   energy_type_key_t() { }
    bool operator<(const energy_type_key_t &k) const {
       if (k.bond_type < bond_type) { 
 	 return true;
       } else {
-	 if (k.energy_type_2 < energy_type_2) {
+	 if (k.energy_type_1 < energy_type_1) {
 	    return true;
 	 } else {
-	    return (k.energy_type_1 < energy_type_1);
+	    return (k.energy_type_2 < energy_type_2);
 	 }
       } 
    }
 };
 
-bool bonds_vec_sorter(const std::pair<energy_type_key_t, std::vector<double> > &k1,
-		      const std::pair<energy_type_key_t, std::vector<double> > &k2) {
+class bond_dist_info_t {
+
+public:
+   double dist;
+   std::string comp_id;
+   std::string atom_name_1;
+   std::string atom_name_2;
+   bond_dist_info_t(const double &d,
+		    const std::string &c,
+		    const std::string &a1,
+		    const std::string &a2) {
+      dist = d;
+      comp_id = c;
+      atom_name_1 = a1;
+      atom_name_2 = a2;
+   }
+};
+
+bool bonds_vec_sorter(const std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > &k1,
+		      const std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > &k2) {
+
    return (k1.second.size() < k2.second.size());
+}
+
+bool bonds_vec_k_sorter(const std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > &k1,
+			const std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > &k2) {
+
+
+   std::vector<double> v1(k1.second.size());
+   std::vector<double> v2(k2.second.size());
+   for (unsigned int i=0; i<k1.second.size(); i++) v1[i] = k1.second[i].dist;
+   for (unsigned int i=0; i<k2.second.size(); i++) v2[i] = k2.second[i].dist;
+   
+   coot::stats::single s1(v1); 
+   coot::stats::single s2(v2);
+
+   if (0) 
+      std::cout << "on sorting kurtosises: "
+		<< " key sizes: " << k1.second.size() << " " << k2.second.size() << "   "
+		<< k1.first.energy_type_1 << "-" << k1.first.energy_type_2
+		<< " " << k1.first.bond_type
+		<< " " << k1.second.size()
+		<< " " << s1.mean()
+		<< " " << s1.kurtosis() << " "
+		<< "     ======     " << k2.first.energy_type_1 << "-" << k2.first.energy_type_2
+		<< " " << k2.first.bond_type
+		<< " " << k2.second.size()
+		<< " " << s2.mean() 
+		<< " " << s2.kurtosis()
+		<< "\n";
+
+   return (s1.kurtosis() < s2.kurtosis());
 } 
 
 void
 write_bond_lengths(const std::string &file_name,
-		   const std::vector<double> &d) {
+		   const std::vector<bond_dist_info_t> &d) {
 
    std::ofstream f(file_name.c_str());
 
    if (f) {
       for (unsigned int i=0; i<d.size(); i++) { 
-	 f << d[i] << "\n";
+	 f << d[i].dist << " " << d[i].comp_id << " " << d[i].atom_name_1 << " " << d[i].atom_name_2 << "\n";
       }
    }
    f.close();
@@ -73,36 +122,42 @@ int main(int argc, char **argv) {
    }
 
    // std::map<std::pair<std::string, std::string>, std::vector<double> > bonds;
-   std::map<energy_type_key_t, std::vector<double> > bonds;
 
-   std::cout << "Examining the bonds in the " << geom.size() << " entries."<< std::endl;
+   std::map<energy_type_key_t, std::vector<bond_dist_info_t> > bonds;
+
+   if (0)
+      std::cout << "Examining the bonds in the " << geom.size() << " entries."<< std::endl;
+   
    for (unsigned int i=0; i<geom.size(); i++) {
       const coot::dictionary_residue_restraints_t r = geom.get_monomer_restraints(i);
       for (unsigned int ibond=0; ibond<r.bond_restraint.size(); ibond++) { 
 	 const coot::dict_bond_restraint_t &br = r.bond_restraint[ibond];
-	 if (!(r.is_hydrogen(br.atom_id_1_4c())) &&
-	     !(r.is_hydrogen(br.atom_id_2_4c()))) { 
-	    energy_type_key_t k(r.type_energy(br.atom_id_1_4c()),
-				r.type_energy(br.atom_id_2_4c()),
+	 std::string an1 = br.atom_id_1_4c();
+	 std::string an2 = br.atom_id_2_4c();
+	 if (!(r.is_hydrogen(an1)) &&
+	     !(r.is_hydrogen(an2))) { 
+	    energy_type_key_t k(r.type_energy(an1),
+				r.type_energy(an2),
 				br.type());
-	    bonds[k].push_back(br.value_dist());
+	    bond_dist_info_t bi(br.value_dist(), r.residue_info.comp_id, an1, an2);
+	    bonds[k].push_back(bi);
 	 }
       }
    }
 
    // convert to vector for sorting by N (counts)
    //
-   std::map<energy_type_key_t, std::vector<double> >::const_iterator it;
-   std::vector<std::pair<energy_type_key_t, std::vector<double> > > bonds_vec(bonds.size());
-   std::vector<std::pair<energy_type_key_t, std::vector<double> > >::const_iterator itv;
+   std::map<energy_type_key_t, std::vector<bond_dist_info_t> >::const_iterator it;
+   std::vector<std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > > bonds_vec;
+   std::vector<std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > >::const_iterator itv;
 
    for (it=bonds.begin(); it!=bonds.end(); it++) {
-      std::pair<energy_type_key_t, std::vector<double> > p(it->first, it->second);
+      std::pair<energy_type_key_t, std::vector<bond_dist_info_t> > p(it->first, it->second);
       bonds_vec.push_back(p);
    }
 
    // now sort bonds_vec how you like:
-   std::sort(bonds_vec.begin(), bonds_vec.end(), bonds_vec_sorter);
+   std::sort(bonds_vec.begin(), bonds_vec.end(), bonds_vec_k_sorter);
    
 
    unsigned int min_counts = 10;
@@ -116,12 +171,12 @@ int main(int argc, char **argv) {
 	 
 	 std::string stub = itv->first.energy_type_1 + "-" + itv->first.energy_type_2;
 
-	 std::string file_name = stub + ".tab";
+	 std::string file_name = stub + "-" + itv->first.bond_type + ".tab";
 	 write_bond_lengths(file_name, itv->second);
 
 	 coot::stats::single stats;
 	 for (unsigned int i=0; i<itv->second.size(); i++)
-	    stats.add(itv->second[i]);
+	    stats.add(itv->second[i].dist);
 	 double var = stats.variance();
 
 
@@ -148,8 +203,9 @@ int main(int argc, char **argv) {
       }
    }
 
-   std::cout << "Found " << n_found << " bond types with more than " << min_counts
-	     << " entries"<< std::endl;
+   if (0)
+      std::cout << "Found " << n_found << " bond types with more than " << min_counts
+		<< " entries"<< std::endl;
 
    return r;
 } 
