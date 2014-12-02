c34b1e83429c859f0cf633035def9f1ad5d7952a
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2014-12-02 11:56:16 +0000

    Add clear_surface_points() function.

--- trunk/canyon/canyon-points.scm
+++ trunk/canyon/canyon-points.scm
@@ -1,11 +1,36 @@
 
 
-(define (get-canyon-points file-name)
+(define (get-canyon-points points-file-name grid-data-file-name)
   
-  (let ((n (new-generic-object-number "canyon-grid"))
-	(g (new-generic-object-number "canyon-grid"))
+  (let ((n (new-generic-object-number "bump-canyon-points"))
+	(g (new-generic-object-number "surface-canyon-grid"))
 	(m (new-generic-object-number "path-model"))
 	(u (new-generic-object-number "unit-vectors")))
+
+    (if #f 
+	(call-with-input-file points-file-name
+	  (lambda (port)
+
+	    (let loop ((line (read-line port)))
+	      (if (not (eof-object? line))
+		  (let ((bits (string->list-of-strings line)))
+
+		    (cond 
+		     ;; bump point test
+		     ;; 
+		     ((= (length bits) 9)
+		      (let ((x (list-ref bits 5))
+			    (y (list-ref bits 6))
+			    (z (list-ref bits 7))
+			    (col (list-ref bits 8)))
+			(to-generic-object-add-point n col 3
+						     (string->number x)
+						     (string->number y)
+						     (string->number z))
+			(loop (read-line port))))
+		     (else 
+		      (loop (read-line port)))))))))
+	(set-display-generic-object n 1))
 
     (call-with-input-file grid-data-file-name
       (lambda (port)
@@ -15,37 +40,18 @@
 	      (let ((bits (string->list-of-strings line)))
 
 		(cond 
-		 ;; bump point test
-		 ;; 
-		 ((= (length bits) 9)
-		  (let ((x (list-ref bits 5))
-			(y (list-ref bits 6))
-			(z (list-ref bits 7))
-			(col (list-ref bits 8)))
-		    (to-generic-object-add-point n col 3
-						 (string->number x)
-						 (string->number y)
-						 (string->number z))
-		    (loop (read-line port))))
-		 (else 
-		  (loop (read-line port)))))))))
-    (set-display-generic-object n 1)
-
-    (call-with-input-file file-name
-      (lambda (port)
-
 		 ;; line test
 		 ;; 
-		 ((and (= (length bits) 8)
+		 ((and (= (length bits) 10)
 		       (string=? (car bits) "line"))
 
-		  (let ((x1  (string->number (list-ref bits 1)))
-			(y1  (string->number (list-ref bits 2)))
-			(z1  (string->number (list-ref bits 3)))
-			(x2  (string->number (list-ref bits 4)))
-			(y2  (string->number (list-ref bits 5)))
-			(z2  (string->number (list-ref bits 6)))
-			(col (list-ref bits 7)))
+		  (let ((x1  (string->number (list-ref bits 3)))
+			(y1  (string->number (list-ref bits 4)))
+			(z1  (string->number (list-ref bits 5)))
+			(x2  (string->number (list-ref bits 6)))
+			(y2  (string->number (list-ref bits 7)))
+			(z2  (string->number (list-ref bits 8)))
+			(col (list-ref bits 9)))
 		    (to-generic-object-add-line g col 1 x1 y1 z1 x2 y2 z2))
 		  (loop (read-line port)))
 
@@ -62,19 +68,17 @@
 		  (loop (read-line port)))
 
 		 (else 
-		  (loop (read-line port)))))
+		  (loop (read-line port)))))))))
 
     (set-display-generic-object g 1)))
-
-    ))
 
 
 (define (canyon-path-points file-name col-1 col-2)
   (call-with-input-file file-name
     (lambda (port)
 
-      (let ((pp (new-generic-object-number "actual-path-points"))
-	    (mp (new-generic-object-number "model-path-points")))
+      (let ((pp (new-generic-object-number (string-append file-name " actual-path-points")))
+	    (mp (new-generic-object-number (string-append file-name " model-path-points"))))
 
       (let loop ((line (read-line port)))
 	
@@ -105,8 +109,8 @@
   (for-each (lambda (i) (set-display-generic-object i 0)) (range (number-of-generic-objects))))
 
 
-(get-canyon-points "../../canyon/points")
-(canyon-path-points  "../../canyon/canyon-path-fit-initial.tab" "#607080" "#4070aa")
-(canyon-path-points  "../../canyon/canyon-path-fit-refined.tab" "#CC7080" "#4070cc")
+(get-canyon-points "../../coot/trunk/canyon/points" "../../coot/trunk/canyon/canyon-grid-lines.data")
+(canyon-path-points  "../../coot/trunk/canyon/canyon-path-fit-initial.tab" "#607080" "#4070aa")
+(canyon-path-points  "../../coot/trunk/canyon/canyon-path-fit-refined.tab" "#CC7080" "#4070cc")
 
 
