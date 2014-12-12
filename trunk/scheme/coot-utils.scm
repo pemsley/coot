4597b4fa6c7663a1df5f9623bc089c6443c2614d
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2014-12-12 16:42:47 +0000

    Release 0.8.1    Revert ligand builder back to cprodrg.

--- trunk/scheme/coot-utils.scm
+++ trunk/scheme/coot-utils.scm
@@ -2653,6 +2653,71 @@
 ;; Run libcheck to convert from SMILES string
 ;; 
 (define (new-molecule-by-smiles-string tlc-text smiles-text)
+
+
+  (define (use-libcheck three-letter-code)
+
+    (let* ((smiles-file (string-append "coot-" three-letter-code ".smi"))
+	   (libcheck-data-lines
+	    (list "N"
+		  (string-append "MON " three-letter-code)
+		  (string-append "FILE_SMILE " smiles-file)
+		  ""))
+	   (log-file-name (string-append "libcheck-" three-letter-code))
+	   (pdb-file-name (string-append "libcheck_" three-letter-code ".pdb"))
+	   (cif-file-name (string-append "libcheck_" three-letter-code ".cif")))
+      
+      ;; write the smiles strings to a file
+      (call-with-output-file smiles-file
+	(lambda (port)
+	  (format port "~a~%" smiles-text)))
+      
+      (let ((status (goosh-command libcheck-exe '() libcheck-data-lines log-file-name #t)))
+	;; the output of libcheck goes to libcheck.lib, we want it in
+	;; (i.e. overwrite the minimal description in cif-file-name
+	(if (number? status)
+	    (if (= status 0)
+		(begin
+		  (if (file-exists? "libcheck.lib")
+		      (rename-file "libcheck.lib" cif-file-name))
+		  (let ((sc (rotation-centre))
+			(imol (handle-read-draw-molecule-with-recentre pdb-file-name 0)))
+		    (if (valid-model-molecule? imol)
+			(let ((mc (molecule-centre imol)))
+			  (apply translate-molecule-by (cons imol (map - sc mc))))))
+		  (read-cif-dictionary cif-file-name)))
+	    (format #t "OOPs.. libcheck returned exit status ~s~%" status)))))
+
+
+  (define (use-pyrogen three-letter-code)
+
+    ;; OK, let's run pyrogen
+    (let ((log-file-name "pyrogen.log"))
+
+      ;; Embed a test for mogul
+      ;;
+      ;; 
+      (let ((goosh-status
+	     (goosh-command
+	      "pyrogen"
+	      (if *use-mogul* 
+		  (list "--residue-type" tlc-text smiles-text)
+		  (if (command-in-path? "mogul")
+		      (list              "--residue-type" tlc-text smiles-text)
+		      (list "--no-mogul" "-M" "--residue-type" tlc-text smiles-text)))
+	      '() log-file-name #t)))
+	(if (ok-goosh-status? goosh-status)
+	    (begin
+	      (let* ((pdb-file-name (string-append tlc-text "-pyrogen.pdb"))
+		     (cif-file-name (string-append tlc-text "-pyrogen.cif"))
+		     (sc (rotation-centre))
+		     (imol (handle-read-draw-molecule-with-recentre pdb-file-name 0)))
+		(if (valid-model-molecule? imol)
+		    (let ((mc (molecule-centre imol)))
+		      (apply translate-molecule-by (cons imol (map - sc mc)))))
+		(read-cif-dictionary cif-file-name)))))))
+
+
   (if (> (string-length smiles-text) 0)
 
       (let ((three-letter-code 
@@ -2665,63 +2730,10 @@
 	      (else "DUM"))))
 	
 	(if (not (enhanced-ligand-coot?))
-	    ;; OK, let's run libcheck
-	    (let* ((smiles-file (string-append "coot-" three-letter-code ".smi"))
-		   (libcheck-data-lines
-		    (list "N"
-			  (string-append "MON " three-letter-code)
-			  (string-append "FILE_SMILE " smiles-file)
-			  ""))
-		   (log-file-name (string-append "libcheck-" three-letter-code))
-		   (pdb-file-name (string-append "libcheck_" three-letter-code ".pdb"))
-		   (cif-file-name (string-append "libcheck_" three-letter-code ".cif")))
-	      
-	      ;; write the smiles strings to a file
-	      (call-with-output-file smiles-file
-		(lambda (port)
-		  (format port "~a~%" smiles-text)))
-	      
-	      (let ((status (goosh-command libcheck-exe '() libcheck-data-lines log-file-name #t)))
-		;; the output of libcheck goes to libcheck.lib, we want it in
-		;; (i.e. overwrite the minimal description in cif-file-name
-		(if (number? status)
-		    (if (= status 0)
-			(begin
-			  (if (file-exists? "libcheck.lib")
-			      (rename-file "libcheck.lib" cif-file-name))
-			  (let ((sc (rotation-centre))
-				(imol (handle-read-draw-molecule-with-recentre pdb-file-name 0)))
-			    (if (valid-model-molecule? imol)
-				(let ((mc (molecule-centre imol)))
-				  (apply translate-molecule-by (cons imol (map - sc mc))))))
-			  (read-cif-dictionary cif-file-name)))
-		    (format #t "OOPs.. libcheck returned exit status ~s~%" status))))
 
-	    ;; OK, let's run pyrogen
-	    (let ((log-file-name "pyrogen.log"))
-
-	      ;; Embed a test for mogul
-	      ;;
-	      ;; 
-	      (let ((goosh-status
-		     (goosh-command
-		      "pyrogen"
-		      (if *use-mogul* 
-			  (list "--residue-type" tlc-text smiles-text)
-			  (if (command-in-path? "mogul")
-			      (list              "--residue-type" tlc-text smiles-text)
-			      (list "--no-mogul" "-M" "--residue-type" tlc-text smiles-text)))
-		      '() log-file-name #t)))
-		(if (ok-goosh-status? goosh-status)
-		    (begin
-		      (let* ((pdb-file-name (string-append tlc-text "-pyrogen.pdb"))
-			     (cif-file-name (string-append tlc-text "-pyrogen.cif"))
-			     (sc (rotation-centre))
-			     (imol (handle-read-draw-molecule-with-recentre pdb-file-name 0)))
-			(if (valid-model-molecule? imol)
-			    (let ((mc (molecule-centre imol)))
-			      (apply translate-molecule-by (cons imol (map - sc mc)))))
-			(read-cif-dictionary cif-file-name))))))))))
+	    (use-libcheck three-letter-code)
+	    (use-pyrogen three-letter-code)))))
+	    
 
 (define (new-molecule-by-smiles-string-by-acedrg tlc-str smiles-str)
 
