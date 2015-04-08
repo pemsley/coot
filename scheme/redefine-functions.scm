f07b607b0ec86a38943af3712b26c02a7dce40fc
pemsley <pemsley@f7134757-782b-0410-8fd1-2f704457d451>
2015-04-08 18:57:44 +0000

    Fix typo in map-statistics redefine.

--- scheme/redefine-functions.scm
+++ scheme/redefine-functions.scm
@@ -103,7 +103,7 @@
 (define morph-fit-residues     morph-fit-residues-scm)
 (define density-score-residue  density-score-residue-scm)
 (define align-to-closest-chain align-to-closest-chain-scm)
-(define map-statistics         map-statistics)
+(define map-statistics         map-statistics-scm)
 
 ;; I changed the function name - save those (just a few) with scripts that I've handed out
 (define toggle-idle-ligand-interactions toggle-flev-idle-ligand-interactions)
