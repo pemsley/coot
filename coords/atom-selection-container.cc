487ef78cda7443b41ea93802921e01423fc44643
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2015-12-21 15:00:31 +0000

    Add missing file.

---
+++ coords/atom-selection-container.cc
@@ -0,0 +1,44 @@
+
+#include "atom-selection-container.hh"
+
+mmdb::Residue *
+atom_selection_container_t::get_next(mmdb::Residue *residue_in) const {
+
+   mmdb::Residue *r = NULL;
+
+   mmdb::Chain *chain = residue_in->GetChain();
+   int this_res_no = residue_in->GetSeqNum();
+   int res_no_next = this_res_no + 1;
+   for (unsigned int i=0; i<n_selected_atoms; i++) { 
+      if (atom_selection[i]->GetChain() == chain) {
+	 // for rigor we should do some testing for insertion codes here abouts
+	 // std::cout << "get_next(): comparing " << atom_selection[i]->GetSeqNum() << " "
+	 // << res_no_next << std::endl;
+	 if (atom_selection[i]->GetSeqNum() == res_no_next) {
+	    r = atom_selection[i]->GetResidue();
+	    break;
+	 }
+      } 
+   }
+   return r;
+}
+
+mmdb::Residue *
+atom_selection_container_t::get_previous(mmdb::Residue *residue_in) const {
+
+   mmdb::Residue *r = NULL;
+
+   mmdb::Chain *chain = residue_in->GetChain();
+   int this_res_no = residue_in->GetSeqNum();
+   int res_no_prev = this_res_no - 1;
+   for (unsigned int i=0; i<n_selected_atoms; i++) { 
+      if (atom_selection[i]->GetChain() == chain) {
+	 // for rigor we should do some testing for insertion codes here abouts
+	 if (atom_selection[i]->GetSeqNum() == res_no_prev) {
+	    r = atom_selection[i]->GetResidue();
+	    break;
+	 }
+      } 
+   }
+   return r;
+}
