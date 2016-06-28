0d58507dbe3d7bf6ba2fc6af8440971e2f819a62
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2016-06-28 15:49:34 +0100

    Update the location of molecule images from PDBe

--- scheme/get-recent-pdbe.scm
+++ scheme/get-recent-pdbe.scm
@@ -1014,10 +1014,14 @@
 
       ;; now do the protein icon:
       (let* ((image-size 120)
-	     (image-name-stub (string-append (string-append entry-id "_cbc" (number->string image-size) ".png")))
+	     ;; (image-name-stub (string-append (string-append entry-id "_cbc" (number->string image-size) ".png")))
+	     (image-name-stub "_deposited_chain_front_image-200x200.png")
 	     ;; (image-url (string-append "http://www.ebi.ac.uk/pdbe-srv/view/images/entry/" image-name-stub))
-	     (image-url (string-append "http://www.ebi.ac.uk/pdbe/entry-images/" image-name-stub))
-	     (entry-image-file-name (append-dir-file *coot-pdbe-image-cache-dir* image-name-stub)))
+	     ;; (image-url (string-append "http://www.ebi.ac.uk/pdbe/entry-images/" image-name-stub))
+	     (image-url (string-append "http://www.ebi.ac.uk/pdbe/static/entry/" entry-id image-name-stub))
+	     (nov (format #t ":::::::::::::::::::: image-url: ~s~%" image-url))
+	     ;; http://www.ebi.ac.uk/pdbe/static/entry/3zi7_deposited_chain_front_image-200x200.png
+	     (entry-image-file-name (append-dir-file *coot-pdbe-image-cache-dir* (string-append entry-id image-name-stub))))
 
 	(cached-or-net-get-image-func image-url entry-image-file-name protein-ribbon-hbox))))
 
