ddeee2b9395ca0d812f2dbb72635ec81dabb124c
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2016-08-08 13:21:56 +0100

    Add utils uri_to_file_name function

--- utils/win-compat.cc
+++ utils/win-compat.cc
@@ -47,3 +47,20 @@ coot::is_regular_file(const std::string & file_name) {
        r = true;
    return r;
 }
+
+std::string
+coot::uri_to_file_name(const std::string &uri) {
+
+   std::string r = uri;
+
+   // Why this? https://en.wikipedia.org/wiki/File_URI_scheme
+   
+#ifdef WINDOWS_MINGW
+	 r = uri.substr(8);
+#else
+	 r = uri.substr(7);
+#endif
+
+   return r;
+
+}
--- utils/win-compat.hh
+++ utils/win-compat.hh
@@ -6,5 +6,6 @@ namespace coot {
    std::string get_fixed_font();
    bool is_regular_file(const std::string &file_name);
    bool is_dir_or_link( const std::string &file_name);
+   std::string uri_to_file_name(const std::string &file_name);
 
 }
