8f49e080062105b56c2ae145a0f145e5fddc1e8b
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2016-10-09 10:22:29 +0100

    Activate multi-solution widgets in ligand fitting dialog

--- src/c-interface-ligands-gui.cc
+++ src/c-interface-ligands-gui.cc
@@ -116,6 +116,8 @@ int fill_ligands_dialog(GtkWidget *find_ligand_dialog) {
 
    int ifound_map, ifound_coords, ifound_ligand; 
    short int diff_maps_only_flag = 0;
+   graphics_info_t g;
+
    ifound_map = fill_ligands_dialog_map_bits(find_ligand_dialog, diff_maps_only_flag);
    if (ifound_map == 0) {
       std::cout << "WARNING:: you must have a map to search for ligands!"
@@ -138,7 +140,7 @@ int fill_ligands_dialog(GtkWidget *find_ligand_dialog) {
 		<< std::endl;
       std::string s("WARNING:: you must have at least one\n          ligand to search for!\n");
       s += "         Ligands have less than ";
-      s += graphics_info_t::int_to_string(graphics_info_t::find_ligand_ligand_atom_limit);
+      s += coot::util::int_to_string(graphics_info_t::find_ligand_ligand_atom_limit);
       s += " atoms\n";
       GtkWidget *w = wrapped_nothing_bad_dialog(s);
       gtk_widget_show(w);
@@ -149,7 +151,7 @@ int fill_ligands_dialog(GtkWidget *find_ligand_dialog) {
    GtkWidget *togglebutton;
    togglebutton = lookup_widget(find_ligand_dialog,
 				"find_ligand_mask_waters_yes_radiobutton");
-   if (graphics_info_t::find_ligand_mask_waters_flag)
+   if (g.find_ligand_mask_waters_flag)
       gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(togglebutton), TRUE);
    else 
       gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(togglebutton), FALSE);
@@ -165,6 +167,32 @@ int fill_ligands_dialog(GtkWidget *find_ligand_dialog) {
 	 gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(search_here_toggle_button), TRUE);
    
    fill_ligands_sigma_level_entry(find_ligand_dialog);
+
+   // multi-solution check button
+   //
+   GtkWidget *multi_solution_check_button = lookup_widget(find_ligand_dialog,
+							  "find_ligand_multi_solution_checkbutton");
+   if (multi_solution_check_button) {
+
+      if (g.find_ligand_multiple_solutions_per_cluster_flag) {
+
+	 gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(multi_solution_check_button), TRUE);
+
+	 // g.find_ligand_score_by_correl_frac_limit
+	 // g.find_ligand_score_correl_frac_interesting_limit
+	 //
+	 GtkWidget *entry_1 = lookup_widget(find_ligand_dialog, "find_ligand_multi_solution_entry_1");
+	 GtkWidget *entry_2 = lookup_widget(find_ligand_dialog, "find_ligand_multi_solution_entry_2");
+	 if (entry_1) {
+	    gtk_entry_set_text(GTK_ENTRY(entry_1),
+			       coot::util::float_to_string(g.find_ligand_score_by_correl_frac_limit).c_str());
+	 }
+	 if (entry_2) {
+	    gtk_entry_set_text(GTK_ENTRY(entry_2),
+			       coot::util::float_to_string(g.find_ligand_score_correl_frac_interesting_limit).c_str());
+	 }
+      }
+   }
 
    // expert options
    fill_ligands_expert_options(find_ligand_dialog);
@@ -588,6 +616,41 @@ void execute_get_mols_ligand_search(GtkWidget *button) {
 	 } else {
 	    std::cout << ligand_str << " widget not found in "
 		      << "execute_get_mols_ligand_search" << std::endl;
+	 }
+      }
+   }
+
+   // multi-solution check button
+   //
+   GtkWidget *multi_solution_check_button = lookup_widget(button,
+							  "find_ligand_multi_solution_checkbutton");
+   if (GTK_TOGGLE_BUTTON(multi_solution_check_button)->active) {
+      g.find_ligand_multiple_solutions_per_cluster_flag = true;
+   }
+
+   GtkWidget *entry_1 = lookup_widget(button, "find_ligand_multi_solution_entry_1");
+   GtkWidget *entry_2 = lookup_widget(button, "find_ligand_multi_solution_entry_2");
+   if (entry_1) {
+      const gchar *e1t = gtk_entry_get_text(GTK_ENTRY(entry_1));
+      if (e1t) {
+	 try {
+	    float f1 = coot::util::string_to_float(e1t);
+	    g.find_ligand_score_by_correl_frac_limit = f1;
+	 }
+	 catch (const std::exception &e) {
+	    std::cout << "WARNING:: failed to convert to number: " << e.what() << std::endl;
+	 }
+      }
+   }
+   if (entry_2) {
+      const gchar *e2t = gtk_entry_get_text(GTK_ENTRY(entry_2));
+      if (e2t) {
+	 try {
+	    float f2 = coot::util::string_to_float(e2t);
+	    g.find_ligand_score_correl_frac_interesting_limit = f2;
+	 }
+	 catch (const std::exception &e) {
+	    std::cout << "WARNING:: failed to convert to number: " << e.what() << std::endl;
 	 }
       }
    }
