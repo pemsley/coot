464873089251495b6f7e8d768413c8c40fd52c1c
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2017-01-31 07:39:37 +0000

    Add missing rotamer files

---
+++ coords/rotamer-container.hh
@@ -0,0 +1,21 @@
+
+#ifndef ROTAMER_CONTAINER_HH
+#include "utils/coot-utils.hh"
+
+class rotamer_markup_container_t {
+
+public:
+   clipper::Coord_orth pos;
+   coot::colour_holder col;
+
+   rotamer_markup_container_t(const clipper::Coord_orth &pos_in,
+			      const coot::colour_holder &col_in) {
+      pos = pos_in;
+      col = col_in;
+   }
+
+   rotamer_markup_container_t() {}
+};
+
+#endif // ROTAMER_CONTAINER_HH
+
