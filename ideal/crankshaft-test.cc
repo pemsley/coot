7aef1a12fd321415be1e00fee0ad0976d88084fc
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2017-11-28 12:18:52 +0000

    More update to crankshaft optimization
    No solution the the cranshaft maxima problem.

--- ideal/crankshaft-test.cc
+++ ideal/crankshaft-test.cc
@@ -14,14 +14,37 @@ int main(int argc, char **argv) {
       atom_selection_container_t asc = get_atom_selection(pdb_file_name, 1, 0);
       if (asc.read_success) {
 	 coot::crankshaft cs(asc.mol);
-	 cs.test();
-	 coot::residue_spec_t rs("A", 51);
-	 std::vector<std::pair<float, float> > r = cs.spin_search(rs);
-	 if (r.size()) {
-	    std::cout << "Residue " << rs << std::endl;
-	    for (std::size_t i=0; i<r.size(); i++) {
-	       std::cout << i << "   " << r[i].first << " " << r[i].second << std::endl;
+	 // cs.test();
+	 zo::rama_table_set zorts;
+	 // coot::residue_spec_t rs("A", 51); // goodie
+	 // coot::residue_spec_t rs("A", 40); // baddie
+	 coot::residue_spec_t rs("A", 20); // baddie
+	 std::vector<std::pair<float, float> > r = cs.spin_search(rs, zorts);
+	 unsigned int n_samples = 30;
+	 if (argc > 2) {
+	    try {
+	       // what's the std:: version of this?
+	       n_samples = coot::util::string_to_int(argv[2]);
 	    }
+	    catch (const std::runtime_error &rte) {
+	       std::cout << "WARNING:: " << rte.what() << std::endl;
+	    }
+	 }
+
+	 if (false) {
+	    if (r.size()) {
+	       std::cout << "Residue " << rs << std::endl;
+	       for (std::size_t i=0; i<r.size(); i++) {
+		  std::cout << i << "   " << r[i].first << " " << r[i].second << std::endl;
+	       }
+	    }
+	 }
+	 if (true) {
+	    // the moves the atoms
+	    // cs.triple_spin_search(rs, zorts, true, n_samples);
+	    cs.find_maxima(rs, zorts, n_samples);
+	    // where did they move to?
+	    asc.mol->WritePDBASCII("moved.pdb");
 	 }
       } else {
 	 std::cout << "read failure " << pdb_file_name << std::endl;
