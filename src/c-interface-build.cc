8a962731a9b729d03adfb712367f868042a5f81c
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2017-12-30 15:58:33 +0000

    Fix the crash on add_OXT_to_residue()
    due to usage of out-of-date pointers from GetAtomTable().

--- src/c-interface-build.cc
+++ src/c-interface-build.cc
@@ -2705,11 +2705,15 @@ short int
 add_OXT_to_residue(int imol, int resno, const char *insertion_code, const char *chain_id) {
 
    short int istat = -1; 
-   if (imol < graphics_n_molecules()) { 
-      istat = graphics_info_t::molecules[imol].add_OXT_to_residue(resno, std::string(insertion_code),
-								  std::string(chain_id));
-      graphics_info_t::molecules[imol].update_symmetry();
-      graphics_draw();
+   if (imol < is_valid_model_molecule(imol)) {
+      if (insertion_code) {
+	 if (chain_id) {
+	    istat = graphics_info_t::molecules[imol].add_OXT_to_residue(resno, std::string(insertion_code),
+									std::string(chain_id));
+	    graphics_info_t::molecules[imol].update_symmetry();
+	    graphics_draw();
+	 }
+      }
    }
    std::string cmd = "add-OXT-to-residue";
    std::vector<coot::command_arg_t> args;
