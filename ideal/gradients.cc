221f247b66b3cecb8c74c61e26bab0245782c2b1
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2018-02-09 00:51:10 +0000

    Fix typos from previous merge

--- ideal/gradients.cc
+++ ideal/gradients.cc
@@ -302,7 +302,7 @@ void coot::my_df_bonds(const gsl_vector *v,
 	    } else {
 	       // debug
 	       if (0) {
-		  idx = 3*restraints)[i].atom_index_1;
+		  idx = 3*restraints->at(i).atom_index_1;
 		  std::cout << "BOND Fixed atom[0] "
 			    << restraints->get_atom((*restraints)[i].atom_index_1)->GetSeqNum() << " " 
 			    << restraints->get_atom((*restraints)[i].atom_index_1)->name << " " 
@@ -315,7 +315,7 @@ void coot::my_df_bonds(const gsl_vector *v,
 	    }
 
 	    if (!(*restraints)[i].fixed_atom_flags[1]) { 
-	       idx = 3*restraints[i].atom_index_2;
+	       idx = 3*restraints->at(i).atom_index_2;
 	       *gsl_vector_ptr(df, idx  ) += x_l_contrib;
 	       *gsl_vector_ptr(df, idx+1) += y_l_contrib;
 	       *gsl_vector_ptr(df, idx+2) += z_l_contrib;
@@ -323,7 +323,7 @@ void coot::my_df_bonds(const gsl_vector *v,
 	    } else {
 	       // debug
 	       if (false) {
-		  idx = 3*((*restraints)[i].atom_index_2 - 0);  
+		  idx = 3*(*restraints)[i].atom_index_2;
 		  std::cout << "BOND Fixed atom[1] "
 			    << restraints->get_atom((*restraints)[i].atom_index_2)->GetSeqNum() << " " 
 			    << restraints->get_atom((*restraints)[i].atom_index_2)->name << " " 
