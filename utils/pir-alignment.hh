ed98dc1cb8caba32100c5f5bfcddc43db2edc117
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2018-03-07 23:35:24 +0000

    Rewrite storage and application of pir alignements

--- utils/pir-alignment.hh
+++ utils/pir-alignment.hh
@@ -11,12 +11,10 @@ namespace coot {
    public:
       class matched_residue_t {
       public:
-	 matched_residue_t(char aligned_in, char target_in, int res_no_in) : res_no(res_no_in), target(target_in), aligned(aligned_in) { }
+	 matched_residue_t(char aligned_in, char target_in) : target(target_in), aligned(aligned_in) { }
 	 matched_residue_t() {};
-	 int res_no;
-	 std::string ins_code; // using this is perverse
 	 char target;
-	 char aligned; // aligned is aligned to target
+	 char aligned; // "aligned" is aligned to target/template
 	 friend std::ostream &operator<< (std::ostream &s, const matched_residue_t &m);
       };
    private:
@@ -26,12 +24,16 @@ namespace coot {
       void store(const std::vector<std::pair<int, std::string> > &seqs);
       int description_to_resno_start(const std::string &descr) const;
    public:
+      int resno_start;
       pir_alignment_t(const std::string &s);
       void read_file(const std::string &file_name);
       pir_alignment_t();
       // any number of alignments can be read - most typically there will be
       // just 1, I imagine
-      std::vector<std::map<int, matched_residue_t> > matches;
+      std::vector<std::vector<matched_residue_t> > matches;
+      unsigned int size() const { return matches.size(); }
+      unsigned int size(unsigned int idx) const { return matches[idx].size(); }
+      const std::vector<matched_residue_t> &get_matches(const int i) const { return matches[i]; }
    };
 
 }
