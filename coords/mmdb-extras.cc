24e2dc4c3c08fc0df4b436964915d4e84065f32c
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2018-03-10 19:49:40 +0000

    Fix atom index problem for link bonds by passing the atom index handle to the function

--- coords/mmdb-extras.cc
+++ coords/mmdb-extras.cc
@@ -147,10 +147,13 @@ make_asc(mmdb::Manager *mol, bool transfer_atom_index_flag) {
    asc.mol->GetSelIndex(asc.SelectionHandle, asc.atom_selection, asc.n_selected_atoms);
 
    int uddHnd = mol->RegisterUDInteger(mmdb::UDR_ATOM , "atom index");
+
+   if (false)
+      std::cout << "DEBUG:: in make_asc() atom index udd handle: " << uddHnd << std::endl;
+
    if (uddHnd < 0) {
       std::cout << " atom index registration failed.\n";
    } else {
-      // std::cout << "DEBUG:: in make_asc() atom index udd " << uddHnd << std::endl;
       asc.UDDAtomIndexHandle = uddHnd; 
       for (int i=0; i<asc.n_selected_atoms; i++)
 	 asc.atom_selection[i]->PutUDData(uddHnd,i);
