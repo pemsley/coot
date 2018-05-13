02b2b33bb63dbddff25535e9f3ede8ed3c2a1cb0
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2018-05-13 21:14:08 +0100

    Add link info to the remarks/header browser

--- src/remarks-browser-gtk-widgets.hh
+++ src/remarks-browser-gtk-widgets.hh
@@ -3,3 +3,5 @@
 void remarks_browser_fill_compound_info(mmdb::Manager *mol, GtkWidget *vbox);
 
 void  remarks_browser_fill_journal_info(mmdb::Manager *mol, GtkWidget *vbox);
+
+void  remarks_browser_fill_link_info(mmdb::Manager *mol, GtkWidget *vbox);
