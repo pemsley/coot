020b8e570782d40518cdad8e01b4056028e13e97
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2018-06-21 21:32:30 +0100

    Fixups for out-of-order function mol_to_asc_rdkit()
    Add new file lidia-core-functions.cc.

--- coot-utils/lidia-core-functions.hh
+++ coot-utils/lidia-core-functions.hh
@@ -1,3 +1,11 @@
+
+#ifndef LIDIA_CORE_FUNCTIONS_HH
+#define LIDIA_CORE_FUNCTIONS_HH
+
+#include "atom-selection-container.hh"
+// #include "lidia-core/lig-build.hh"
+#include "lidia-core/lbg-molfile.hh"
+
 namespace coot {
   // mdl mol file support
   atom_selection_container_t mdl_mol_to_asc(const lig_build::molfile_molecule_t &m);
@@ -6,4 +14,7 @@ namespace coot {
   atom_selection_container_t mol_to_asc_rdkit(const std::string &file_name);
 #endif // MAKE_ENHANCED_LIGAND_TOOLS
 }
+
+
+#endif // LIDIA_CORE_FUNCTIONS_HH
 
