c6e6c2e2d952c4c1cb181930a48470a5c3fcd02e
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2019-02-11 02:35:13 -0800

    Reweight atoms in refinement

--- ideal/simple-restraint.cc
+++ ideal/simple-restraint.cc
@@ -433,28 +433,31 @@ coot::restraints_container_t::init_shared_post(const std::vector<atom_spec_t> &f
    atom_z_occ_weight.resize(n_atoms);
    std::vector<std::pair<std::string, int> > atom_list = coot::util::atomic_number_atom_list();
    for (int i=0; i<n_atoms; i++) {
-      double z = coot::util::atomic_number(atom[i]->element, atom_list);
-      double weight = 1.0;
-      double occupancy = atom[i]->occupancy;
-      if (occupancy > 1.0) occupancy = 1.0;
-      if (cryo_em_mode) {
-	 // is-side-chain? would be a better test
-	 if (! is_main_chain_or_cb_p(atom[i]))
-	    {
-	       // std::cout << "downweighting atom " << coot::atom_spec_t(atom[i]) << std::endl;
-	       // weight = 0.1;
+      mmdb::Atom *at = atom[i];
+      if (! at->isTer()) {
+	 double z = coot::util::atomic_number(at->element, atom_list);
+	 double weight = 1.0;
+	 double occupancy = atom[i]->occupancy;
+	 if (occupancy > 1.0) occupancy = 1.0;
+	 if (cryo_em_mode) {
+	    // is-side-chain? would be a better test
+	    if (! is_main_chain_or_cb_p(atom[i]))
+	       {
+		  // std::cout << "downweighting atom " << coot::atom_spec_t(atom[i]) << std::endl;
+		  // weight = 0.1;
+	       }
+	    std::string at_name = at->name;
+	    if (at_name == " O  ") {
+	       // weight = 0.2;
 	    }
-	 std::string at_name = atom[i]->name;
-	 if (at_name == " O  ") {
-	    // weight = 0.2;
 	 }
-      }
 
-      if (z < 0.0) {
-	 std::cout << "Unknown element :" << atom[i]->element << ": " << std::endl;
-	 z = 6.0; // as for carbon
-      } 
-      atom_z_occ_weight[i] = weight * z * occupancy;
+	 if (z < 0.0) {
+	    std::cout << "Unknown element :" << at->element << ": " << std::endl;
+	    z = 6.0; // as for carbon
+	 } 
+	 atom_z_occ_weight[i] = weight * z * occupancy;
+      }
    }
    
    // the fixed atoms:   
