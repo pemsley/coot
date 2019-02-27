c868e82fa76222cbb952d8d1d4500d61f0308190
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2019-02-27 19:46:37 +0000

    Minor change to debug info on failure to construct rdkit molecule

--- src/flev.cc
+++ src/flev.cc
@@ -502,7 +502,8 @@ void fle_view_with_rdkit_internal(int imol, const char *chain_id, int res_no, co
 
 	       // assign atom names
 	       if (int(rdkm.getNumAtoms()) < res_ref->GetNumberOfAtoms()) {
-		  std::cout << "WARNING:: failure to construct rdkit molecule " << std::endl;
+		  std::cout << "WARNING:: failure to construct rdkit molecule " << rdkm.getNumAtoms() << " vs " << res_ref->GetNumberOfAtoms()
+	                    << std::endl;
 	       } else {
 		  mmdb::PPAtom residue_atoms = 0;
 		  int n_residue_atoms;
