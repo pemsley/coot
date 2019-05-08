c12f7efcade6a47764eef2ce9a8989731b8b5486
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2019-05-08 11:09:04 +0100

    gtk3: progress with support.c

--- src/support.c
+++ src/support.c
@@ -40,14 +40,14 @@ lookup_widget                          (GtkWidget       *widget,
 	if (GTK_IS_MENU (widget))
 	  parent = gtk_menu_get_attach_widget (GTK_MENU (widget));
 	else
-	  parent = widget->parent;
+	  parent = gtk_widget_get_parent(widget);
 	if (parent == NULL)
 	  break;
 	widget = parent;
       }
 
-    found_widget = (GtkWidget*) gtk_object_get_data (GTK_OBJECT (widget),
-						     widget_name);
+    found_widget = (GtkWidget*) g_object_get_data (G_OBJECT (widget), widget_name);
+
     if (!found_widget)
       g_warning ("Widget not found: %s", widget_name);
   } else { 
