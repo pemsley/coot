e587eba0a25af1a3c9125bb509c107af54a2bcc7
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2019-08-08 18:43:55 +0100

    Fix-up the typo in the header of coot-gui.scm
    sigh.

--- scheme/coot-gui.scm
+++ scheme/coot-gui.scm
@@ -1,7 +1,6 @@
 ;;;; Copyright 2007 by Paul Emsley
 ;;;; 
-;;;; This program
-   or modify
+;;;; This program is free software; you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
 ;;;; the Free Software Foundation; either version 3 of the License, or (at
 ;;;; your option) any later version.
