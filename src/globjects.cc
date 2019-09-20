253853c72332c9e52ed683a64ca28c140e68a80e
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2019-09-20 11:05:07 +0100

    Fix up the default x-ray map radius

--- src/globjects.cc
+++ src/globjects.cc
@@ -426,8 +426,8 @@ short int graphics_info_t::print_initial_chi_squareds_flag = 0;
 
 short int graphics_info_t::show_symmetry = 0;
 
-float    graphics_info_t::box_radius_xray = 125.4;
-float    graphics_info_t::box_radius_em   = 125;
+float    graphics_info_t::box_radius_xray =  25.0;
+float    graphics_info_t::box_radius_em   = 125.0;
 
 
 int      graphics_info_t::debug_atom_picking = 0;
