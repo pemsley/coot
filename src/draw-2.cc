d6636064043b56eb8a354d9d97aa620f5f8e8e49
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2020-04-24 11:23:48 +0100

    Rework the scrolling map again
    Should be better now (re: scrolling the visible if the scrollable-map is 
    not displayed).

--- src/draw-2.cc
+++ src/draw-2.cc
@@ -1195,19 +1195,26 @@ on_glarea_scroll(GtkWidget *widget, GdkEventScroll *event) {
       direction = -1;
 
    graphics_info_t g;
-   int imol_scroll = graphics_info_t::scroll_wheel_map;
+   int imol_scroll = g.scroll_wheel_map;
 
-   if (g.is_valid_map_molecule(imol_scroll)) {
+   if (! g.is_valid_map_molecule(imol_scroll)) {
 
-      // don't scroll the map if the map is not displayed. Scroll the
-      // map that *is* displayed
       std::vector<int> dm = g.displayed_map_imols();
       if (std::find(dm.begin(), dm.end(), imol_scroll) == dm.end()) {
-         // imol_map_for_scroll is not visible, choose another one
          if (dm.size() > 0)
             imol_scroll = dm[0];
       }
+   }
+
+   if (! g.molecules[imol_scroll].is_displayed_p()) {
+      // don't scroll the map if the map is not displayed. Scroll the
+      // map that *is* displayed
+      std::vector<int> dm = g.displayed_map_imols();
+      if (dm.size() > 0)
+         imol_scroll = dm[0];
+   }
  
+   if (g.is_valid_map_molecule(imol_scroll)) {
       // use direction
       if (direction == 1)
          graphics_info_t::molecules[imol_scroll].pending_contour_level_change_count--;
