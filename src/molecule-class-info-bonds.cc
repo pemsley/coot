d93bc42e8be5b8c89da1332b663536059491d1bf
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2020-05-19 14:25:51 +0100

    Fixup draw_missing_loops_flag usage for user_defined_colours_representation()

--- src/molecule-class-info-bonds.cc
+++ src/molecule-class-info-bonds.cc
@@ -91,7 +91,9 @@ molecule_class_info_t::user_defined_colours_representation(coot::protein_geometr
 
       Bond_lines_container bonds(geom_p);
       bonds.do_Ca_plus_ligands_bonds(atom_sel, imol_no, geom_p, 2.4, 4.7,
-                                     coot::COLOUR_BY_USER_DEFINED_COLOURS, false);
+                                     draw_missing_loops_flag,
+                                     coot::COLOUR_BY_USER_DEFINED_COLOURS,
+                                     false);
       bool add_residue_indices = true;
       bonds_box = bonds.make_graphical_bonds_no_thinning();
       bonds_box_type = coot::COLOUR_BY_USER_DEFINED_COLOURS_BONDS;
