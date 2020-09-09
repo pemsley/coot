395f5a02249694d6f9af20938a60e0f0fb7dd5f2
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2020-09-09 04:55:04 +0100

    Remove restraints debugging

--- ideal/ng.cc
+++ ideal/ng.cc
@@ -1778,7 +1778,7 @@ coot::restraints_container_t::add_header_metal_link_bond_ng(const coot::atom_spe
 	 break;
    }
 
-   if (true)
+   if (false)
       std::cout << "debug:: in add_header_metal_link_bond_ng() indices: " << index_1 << " " << index_2
 		<< std::endl;
 
