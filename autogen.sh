750f05f60bf3f6e5d811e956526b103cc10f7bed
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2020-12-15 21:21:08 +0000

    Remove Mac-related and old cruft from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -17,57 +17,30 @@ fi
 echo libtoolize --copy
 libtoolize --copy
 
-if test $sys = Darwin ; then
-   echo aclocal -I macros -I /sw/share/aclocal
-   aclocal -I macros -I /sw/share/aclocal
-else
 
-    # we can only add the directories if they exist, otherwise aclocal barfs (sigh)
+# we can only add the directories if they exist, otherwise aclocal barfs (sigh)
 
-    aclocal_extras=
-    dir_list="$HOME/autobuild/$OS-$HOST-pre-release-gtk2-python/share/aclocal $HOME/autobuild/$OS-refinement-pre-release-gtk2-python/share/aclocal"
+aclocal_extras=
+dir_list="$HOME/autobuild/$OS-$HOST-pre-release-gtk3/share/aclocal"
 
-
-    for dir in $dir_list
+for dir in $dir_list
     do
        if [ -e $dir ] ; then
 	  aclocal_extras="$aclocal_extras -I $dir"
        fi
     done
 
-    case "$sys" in
+case "$sys" in
     MINGW32_NT-* )
 	echo We have WIN
-	aclocal_extras="-I /usr/local/share/aclocal -I /mingw/share/aclocal";;
-    esac
-    echo aclocal -I macros $aclocal_extras
-    aclocal -I macros $aclocal_extras
-fi
-
-which git
+	aclocal_extras="-I /usr/local/share/aclocal -I /mingw/share/aclocal" ;;
+esac
+echo aclocal -I macros $aclocal_extras
+aclocal -I macros $aclocal_extras
 
 echo autoconf
 autoconf
 
-which git
-
 echo automake --add-missing --copy
 automake --add-missing --copy
-
-
-# configure needs to be newer that aclocal.m4, which on Paris
-# checkout, it seems not to be.  Perhaps they are generated too close
-# to distinguish.  So let's touch configure after automake, that
-# should provide sufficient time distinction.
-
-touch configure
-
-# then in the build directory, configure.status needs to be newer that
-# ../coot/configure or else it does a reconfigure.
-
-# No longer can/should we do this... (all hail git)
-# if [ ! -e src/svn-revision.cc ] ; then
-#    bash generate-svn-revision-cc.sh
-# fi
-
 
