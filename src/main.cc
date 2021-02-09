f51b65a109cff1bc62cc386fd0194897dd7c71f8
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2021-02-09 02:43:33 +0000

    Allow the use of COOT_MAIN_TITLE_EXTRA, which is appended to the title bar string
    in the compiler flags passed to configure.

--- src/main.cc
+++ src/main.cc
@@ -272,13 +272,18 @@ main (int argc, char *argv[]) {
       std::string main_title = "Coot " + version_string;
 #ifdef MAKE_ENHANCED_LIGAND_TOOLS	
       main_title += " EL";
-#endif 	
+#endif
+
+#ifdef COOT_MAIN_TITLE_EXTRA
+      main_title += COOT_MAIN_TITLE_EXTRA;
+#else
       // if this is a pre-release, stick in the revision number too
       if (version_string.find("-pre") != std::string::npos) {
 	 main_title += " (revision count ";
 	 main_title += coot::util::int_to_string(git_revision_count());
 	 main_title += ")";
       }
+#endif
      
 #ifdef WINDOWS_MINGW
       main_title = "Win" + main_title;
