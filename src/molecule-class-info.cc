deb7954be1d2e8f556719fd5a9bf26eebe3dd2f3
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2021-08-25 14:07:57 +0100

    Workstation (SSL 7.6 gcc 4.8.5) seems to crash on (new) close_yourself()
    So add a hack for now to prevent the deletion of stored data.

--- src/molecule-class-info.cc
+++ src/molecule-class-info.cc
@@ -5641,15 +5641,18 @@ molecule_class_info_t::close_yourself() {
       was_nxmap = true;
    }
 
-   if (original_fphis_filled)
-      delete original_fphis_p;
-
-   if (original_fobs_sigfobs_filled)
-      delete original_fobs_sigfobs_p;
-
-   if (original_r_free_flags_p) // no flag for filled?
-      delete original_r_free_flags_p;
-
+   bool delete_stored_data = false; // does this stop the crash on charybdis?
+   if (delete_stored_data) {
+      if (original_fphis_filled)
+         delete original_fphis_p;
+      
+      if (original_fobs_sigfobs_filled)
+         delete original_fobs_sigfobs_p;
+      
+      if (original_r_free_flags_p) // no flag for filled?
+         delete original_r_free_flags_p;
+   }
+      
    // delete from display manager combo box
    //
    graphics_info_t g;
