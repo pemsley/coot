bc48e2a2a902af3aa059d39ea7c4a44b6da7dae5
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2022-04-06 09:07:18 +0100

    Remove debugging of the tick function

--- src/draw-2.cc
+++ src/draw-2.cc
@@ -41,7 +41,7 @@ get_camera_up_direction(const glm::mat4 &mouse_quat_mat) {
 gboolean
 graphics_info_t::tick_function_is_active() {
 
-   if (true)
+   if (false)
       std::cout << "tick_function_is_active() " << do_tick_particles << " " << do_tick_spin << " " << do_tick_boids << " "
                 << do_tick_hydrogen_bonds_mesh << " " << do_tick_happy_face_residue_markers << " "
                 << do_tick_constant_draw << std::endl;
