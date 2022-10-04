f5f3eb61903f463bc70ef02bd8cd641a1e9f3ea6
hgonomeg <hgonomeg@gmail.com>
2022-10-04 13:09:02 +0200

    Introduce 'setup_gui_components' to the codebase

--- src/setup-gui-components.cc
+++ src/setup-gui-components.cc
@@ -1,1 +1,10 @@
 #include "setup-gui-components.hh"
+
+#include <gtk/gtk.h>
+#include "graphics-info.h"
+
+void setup_gui_components() {
+   g_info("Initializing UI components...");
+
+   g_info("Done initializing UI components.");
+}
---
+++ src/setup-gui-components.hh
@@ -0,0 +1,3 @@
+
+void setup_gui_components();
+
