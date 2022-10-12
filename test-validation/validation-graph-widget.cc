d83a198af0eebb761f5e2b2b99e33c987d465aa5
hgonomeg <hgonomeg@gmail.com>
2022-10-12 23:23:42 +0200

    Demonstrate successful subclassing using example code

--- test-validation/validation-graph-widget.cc
+++ test-validation/validation-graph-widget.cc
@@ -12,18 +12,66 @@ G_DEFINE_TYPE(CootValidationGraph, coot_validation_graph, GTK_TYPE_WIDGET)
 //     GObjectClass parent_class;
 // };
 
+
+/// This is a "demo" function copied from here: https://blog.gtk.org/2020/04/
+void
+demo_snapshot (GtkWidget *widget, GtkSnapshot *snapshot)
+{
+  GdkRGBA red, green, yellow, blue;
+  float w, h;
+
+  gdk_rgba_parse (&red, "red");
+  gdk_rgba_parse (&green, "green");
+  gdk_rgba_parse (&yellow, "yellow");
+  gdk_rgba_parse (&blue, "blue");
+
+  w = gtk_widget_get_width (widget) / 2.0;
+  h = gtk_widget_get_height (widget) / 2.0;
+
+  graphene_rect_t m_graphene_rect;
+
+  m_graphene_rect = GRAPHENE_RECT_INIT(0, 0, w, h);
+  gtk_snapshot_append_color (snapshot, &red,
+                             &m_graphene_rect);
+  m_graphene_rect = GRAPHENE_RECT_INIT(w, 0, w, h);
+  gtk_snapshot_append_color (snapshot, &green,
+                             &m_graphene_rect);
+  m_graphene_rect = GRAPHENE_RECT_INIT(0, h, w, h);
+  gtk_snapshot_append_color (snapshot, &yellow,
+                             &m_graphene_rect);
+  m_graphene_rect = GRAPHENE_RECT_INIT(w, h, w, h);
+  gtk_snapshot_append_color (snapshot, &blue,
+                             &m_graphene_rect);
+}
+
+void
+demo_measure (GtkWidget      *widget,
+              GtkOrientation  orientation,
+              int             for_size,
+              int            *minimum_size,
+              int            *natural_size,
+              int            *minimum_baseline,
+              int            *natural_baseline)
+{
+  *minimum_size = 100;
+  *natural_size = 200;
+}
+
 static void coot_validation_graph_init(CootValidationGraph* ) {
-    // not sure what to put here yet
+    // I think that this is the primary constructor
 }
 
 static void coot_validation_graph_class_init(CootValidationGraphClass* klass) {
-    // not sure what to put here yet
+    // I think that this is a GObject class constructor that sets up the GObject class at runtime.
+
+    GTK_WIDGET_CLASS(klass)->snapshot = demo_snapshot;
+    GTK_WIDGET_CLASS(klass)->measure = demo_measure;
 }
 
 CootValidationGraph* 
 coot_validation_graph_new()
 {
-  return COOT_COOT_VALIDATION_GRAPH(g_object_new (COOT_VALIDATION_GRAPH_TYPE, NULL));
+    return COOT_COOT_VALIDATION_GRAPH(g_object_new (COOT_VALIDATION_GRAPH_TYPE, NULL));
 }
 
 
