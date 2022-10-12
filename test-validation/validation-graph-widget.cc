2f49a0951ffca836448895e5c02202921ff47d7a
hgonomeg <hgonomeg@gmail.com>
2022-10-12 22:57:15 +0200

    Make attempts at subclassing GtkWidget

--- test-validation/validation-graph-widget.cc
+++ test-validation/validation-graph-widget.cc
@@ -1,1 +1,31 @@
 #include "validation-graph-widget.hh"
+
+G_BEGIN_DECLS
+struct _CootValidationGraph {
+    GtkWidget parent;
+};
+
+G_DEFINE_TYPE(CootValidationGraph, coot_validation_graph, GTK_TYPE_WIDGET)
+
+// not sure what this is for or whether it is going to be needed at all
+// struct _CootValidationGraphClass {
+//     GObjectClass parent_class;
+// };
+
+static void coot_validation_graph_init(CootValidationGraph* ) {
+    // not sure what to put here yet
+}
+
+static void coot_validation_graph_class_init(CootValidationGraphClass* klass) {
+    // not sure what to put here yet
+}
+
+CootValidationGraph* 
+coot_validation_graph_new()
+{
+  return COOT_COOT_VALIDATION_GRAPH(g_object_new (COOT_VALIDATION_GRAPH_TYPE, NULL));
+}
+
+
+
+G_END_DECLS
--- test-validation/validation-graph-widget.hh
+++ test-validation/validation-graph-widget.hh
@@ -1,4 +1,17 @@
 #ifndef VALIDATION_GRAPH_WIDGET_HH
 #define VALIDATION_GRAPH_WIDGET_HH
+#include <gtk/gtk.h>
+
+
+
+G_BEGIN_DECLS   
+
+#define COOT_VALIDATION_GRAPH_TYPE (coot_validation_graph_get_type ())
+G_DECLARE_FINAL_TYPE  (CootValidationGraph, coot_validation_graph, COOT, COOT_VALIDATION_GRAPH, GtkWidget)
+
+
+CootValidationGraph *coot_validation_graph_new();
+
+G_END_DECLS
 
 #endif // VALIDATION_GRAPH_WIDGET_HH
