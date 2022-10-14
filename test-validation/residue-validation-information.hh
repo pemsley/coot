b9623572b5b04a6bbf83b597b6a698fafe34116f
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2022-10-14 09:11:21 +0100

    Add a placeholder for the residue validation block colour

--- test-validation/residue-validation-information.hh
+++ test-validation/residue-validation-information.hh
@@ -10,10 +10,11 @@ namespace coot {
       residue_validation_information_t(const coot::residue_spec_t &rs,
                                        const coot::atom_spec_t &atom_spec_in,
                                        double distortion_in, const std::string &l) :
-         residue_spec(rs), atom_spec(atom_spec_in), distortion(distortion_in), label(l) {}
+         residue_spec(rs), atom_spec(atom_spec_in), distortion(distortion_in), label(l), block_colour("#904040") {}
       residue_spec_t residue_spec;
       atom_spec_t atom_spec;
       double distortion;
+      std::string block_colour;
       std::string label;
    };
 }
