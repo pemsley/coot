36e32101b9625b6a8fca22d8807a693a09cab1d1
hgonomeg <hgonomeg@gmail.com>
2022-10-17 10:09:13 +0200

    Move implementation from .hh to .cc

--- test-validation/validation-information.cc
+++ test-validation/validation-information.cc
@@ -1,1 +1,27 @@
 #include "validation-information.hh"
+
+coot::chain_validation_information_t::chain_validation_information_t(const std::string &chain_id_in) 
+:chain_id(chain_id_in) {
+
+}
+void coot::chain_validation_information_t::add_residue_valiation_informtion(const residue_validation_information_t &rvi) {
+    rviv.push_back(rvi);
+}
+
+unsigned int coot::validation_information_t::get_index_for_chain(const std::string &chain_id) {
+    for (unsigned int i=0; i<cviv.size(); i++) {
+    if (chain_id == cviv[i].chain_id)
+        return i;
+    }
+    chain_validation_information_t cvi(chain_id);
+    cviv.push_back(cvi);
+    return cviv.size() -1;
+}
+
+void coot::validation_information_t::add_residue_valiation_informtion(
+    const residue_validation_information_t &rvi, 
+    const std::string &chain_id) {
+
+    unsigned int idx = get_index_for_chain(chain_id);
+    cviv[idx].add_residue_valiation_informtion(rvi);
+}
--- test-validation/validation-information.hh
+++ test-validation/validation-information.hh
@@ -12,10 +12,8 @@ namespace coot {
       std::string name;
       std::string type;
       std::vector<residue_validation_information_t> rviv;
-      explicit chain_validation_information_t(const std::string &chain_id_in) : chain_id(chain_id_in) {}
-      void add_residue_valiation_informtion(const residue_validation_information_t &rvi) {
-         rviv.push_back(rvi);
-      }
+      explicit chain_validation_information_t(const std::string &chain_id_in);
+      void add_residue_valiation_informtion(const residue_validation_information_t &rvi);
    };
 
    class validation_information_t {
@@ -23,19 +21,8 @@ namespace coot {
       std::string name;
       std::string type;
       std::vector<chain_validation_information_t> cviv;
-      unsigned int get_index_for_chain(const std::string &chain_id) {
-         for (unsigned int i=0; i<cviv.size(); i++) {
-            if (chain_id == cviv[i].chain_id)
-               return i;
-         }
-         chain_validation_information_t cvi(chain_id);
-         cviv.push_back(cvi);
-         return cviv.size() -1;
-      }
-      void add_residue_valiation_informtion(const residue_validation_information_t &rvi, const std::string &chain_id) {
-         unsigned int idx = get_index_for_chain(chain_id);
-         cviv[idx].add_residue_valiation_informtion(rvi);
-      }
+      unsigned int get_index_for_chain(const std::string &chain_id);
+      void add_residue_valiation_informtion(const residue_validation_information_t &rvi, const std::string &chain_id);
    };
 
 }
