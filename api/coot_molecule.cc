35dccb8712d2e95743eebdc2d928706b9397e497
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2022-10-28 15:24:12 +0100

    Add a test have_close_peptide_bond() into ramachandran_validation()
    Don't Rama-connect residues that are far apart in space.

--- api/coot_molecule.cc
+++ api/coot_molecule.cc
@@ -1016,6 +1016,44 @@ int coot::molecule_t::flip_peptide(const coot::atom_spec_t &as_in, const std::st
 std::vector<std::pair<coot::Cartesian, coot::util::phi_psi_t> >
 coot::molecule_t::ramachandran_validation() const {
 
+   auto have_close_peptide_bond = [] (mmdb::Residue *residue_1, mmdb::Residue *residue_2) {
+
+      bool status = false;
+      mmdb::Atom **residue_atoms_1 = 0;
+      int n_residue_atoms_1 = 0;
+      // I should iterate over all alt confs
+      residue_1->GetAtomTable(residue_atoms_1, n_residue_atoms_1);
+      for (int iat=0; iat<n_residue_atoms_1; iat++) {
+         mmdb::Atom *at_1 = residue_atoms_1[iat];
+         if (! at_1->isTer()) {
+            std::string atom_name_1(at_1->GetAtomName());
+            if (atom_name_1 == " C  ") {
+               coot::Cartesian pt_c(at_1->x, at_1->y, at_1->z);
+               mmdb::Atom **residue_atoms_2 = 0;
+               int n_residue_atoms_2 = 0;
+               // I should iterate over all alt confs
+               residue_2->GetAtomTable(residue_atoms_2, n_residue_atoms_2);
+               for (int iat=0; iat<n_residue_atoms_2; iat++) {
+                  mmdb::Atom *at_2 = residue_atoms_2[iat];
+                  if (! at_2->isTer()) {
+                     std::string atom_name_2(at_2->GetAtomName());
+                     if (atom_name_2 == " N  ") {
+                        coot::Cartesian pt_n(at_1->x, at_1->y, at_1->z);
+                        double dd = coot::Cartesian::lengthsq(pt_c, pt_n);
+                        double d = std::sqrt(dd);
+                        if (d < 3.0) {
+                           status = true;
+                           break;
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      }
+      return status;
+   };
+
    auto get_HA_unit_vector = [] (mmdb::Residue *r) {
       bool status = false;
       coot::Cartesian dir;
@@ -1064,15 +1102,19 @@ coot::molecule_t::ramachandran_validation() const {
       mmdb::Residue *rt = phi_psi.residue_this;
       mmdb::Residue *rn = phi_psi.residue_next;
       if (rp && rt && rn) {
-         mmdb::Atom *at = rt->GetAtom(" CA "); // 20221006-PE alt-confs another day
-         if (at) {
-            coot::Cartesian pos(at->x, at->y, at->z);
-            std::pair<bool, coot::Cartesian> hav = get_HA_unit_vector(rt);
-            coot::Cartesian offset(0,0,rama_ball_pos_offset_scale);
-            if (hav.first) offset = hav.second * rama_ball_pos_offset_scale;
-            coot::util::phi_psi_t cupp(rp, rt, rn);
-            auto p = std::make_pair(pos + offset, cupp);
-            v.push_back(p);
+         if (have_close_peptide_bond(rp, rt)) {
+            if (have_close_peptide_bond(rt, rn)) {
+               mmdb::Atom *at = rt->GetAtom(" CA "); // 20221006-PE alt-confs another day
+               if (at) {
+                  coot::Cartesian pos(at->x, at->y, at->z);
+                  std::pair<bool, coot::Cartesian> hav = get_HA_unit_vector(rt);
+                  coot::Cartesian offset(0,0,rama_ball_pos_offset_scale);
+                  if (hav.first) offset = hav.second * rama_ball_pos_offset_scale;
+                  coot::util::phi_psi_t cupp(rp, rt, rn);
+                  auto p = std::make_pair(pos + offset, cupp);
+                  v.push_back(p);
+               }
+            }
          }
       }
    }
