17a582f33b2f1cb028ffd8eeb6b4d5a015b7c12a
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2022-11-18 18:04:22 +0000

    Add missing heaader phi-psi-prob.hh

---
+++ api/phi-psi-prob.hh
@@ -0,0 +1,24 @@
+#ifndef PHI_PSI_PROB_HH
+#define PHI_PSI_PROB_HH
+
+#include "coot-utils/coot-rama.hh"
+#include "coords/Cartesian.h"
+#include "coords/ramachandran-container.hh"
+
+namespace coot {
+   class phi_psi_prob_t {
+      public:
+         phi_psi_prob_t() {}
+      phi_psi_prob_t(const util::phi_psi_t &pp, const ramachandrans_container_t &rama_container);
+      util::phi_psi_t phi_psi;
+      coot::Cartesian position;
+      double probability;
+      bool is_allowed_flag;
+      bool is_allowed() const { return is_allowed_flag; }
+      std::string residue_name() const { return phi_psi.residue_name(); }
+   };
+}
+
+
+#endif // PHI_PSI_PROB_HH
+
