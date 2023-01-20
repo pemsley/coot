48b6536750f97ad1a2229359a4194a55abf0328f
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-01-20 02:23:19 +0000

    Now that g_triangle no longer has a colour index, simple_mesh_to_pythonic_mesh() needs some work
    Needs testing.

--- api/molecules_container_swig.cc
+++ api/molecules_container_swig.cc
@@ -11,13 +11,57 @@ molecules_container_t::simple_mesh_to_pythonic_mesh(const coot::simple_mesh_t &m
    // Blender has colour/material per face. Let's convert our mesh colours and add the
    // colour vect to the returned object.
 
+   auto make_colour_hash = [] (const glm::vec4 &c0, const glm::vec4 &c1, const glm::vec4 &c2) {
+      double rr = c0.r + 1290.0 * c1.r + 1290.0 * 1290.0 * c2.r;
+      double gg = c0.g + 1290.0 * c1.g + 1290.0 * 1290.0 * c2.g;
+      double bb = c0.b + 1290.0 * c1.b + 1290.0 * 1290.0 * c2.b;
+      double aa = c0.a + 1290.0 * c1.a + 1290.0 * 1290.0 * c2.a;
+      double ss = rr + 35.0 * gg + 35.0 * 35.0 * bb + 35.0 * 35.0 * 35.0 * aa;
+      long h = static_cast<long>(ss);
+      return h;
+   };
+
+   auto get_colour_index = [make_colour_hash] (std::map<unsigned long, unsigned int> &colour_index_map,
+                                               unsigned int &colour_index,
+                                               const glm::vec4 &c0, const glm::vec4 &c1, const glm::vec4 &c2) {
+
+      auto ch = make_colour_hash(c0, c1, c2);
+      std::map<unsigned long, unsigned int>::const_iterator it = colour_index_map.find(ch);
+      if (ch) {
+         return it->second;
+      } else {
+         colour_index_map[ch] = colour_index;
+         colour_index++;
+         return colour_index;
+      }
+   };
+
+   glm::vec4 c0(0.1, 0.1, 0.1, 0.1);
+   glm::vec4 c1(0.4, 0.5, 0.6, 1.0);
+   glm::vec4 c2(0.6, 0.5, 0.5, 1.0);
+   glm::vec4 c3(1.0, 1.0, 1.0, 1.0);
+   long t0 = make_colour_hash(c0, c0, c0);
+   long t1 = make_colour_hash(c1, c1, c1);
+   long t2 = make_colour_hash(c2, c2, c2);
+   long t3 = make_colour_hash(c3, c3, c3);
+
+   std::cout << "t0 " << t0 << std::endl;
+   std::cout << "t1 " << t1 << std::endl;
+   std::cout << "t2 " << t1 << std::endl;
+   std::cout << "t3 " << t1 << std::endl;
+
    PyObject *r_py = PyList_New(0);
 
    if (true) {
-      const std::vector<g_triangle> &tris = mesh.triangles;
+      const std::vector<g_triangle> &tris      = mesh.triangles;
+      const std::vector<coot::api::vnc_vertex> &vertices = mesh.vertices;
 
       std::cout << "simple_mesh_to_pythonic_mesh(): mesh vertices size " << mesh.vertices.size() << std::endl;
       std::cout << "simple_mesh_to_pythonic_mesh(): tris size " << tris.size() << std::endl;
+
+      std::map<unsigned long, unsigned int> colour_index_map;
+
+      unsigned int colour_index_running = 0;
 
       r_py = PyList_New(3);
       PyObject *vertices_py     = PyList_New(mesh.vertices.size());
@@ -32,7 +76,12 @@ molecules_container_t::simple_mesh_to_pythonic_mesh(const coot::simple_mesh_t &m
       }
       for (unsigned int i=0; i<tris.size(); i++) {
          PyObject *tri_py = PyList_New(4);
-         const int &colour_index = tris[i].colour_index;
+         // const int &colour_index = tris[i].colour_index;
+         unsigned int colour_index = get_colour_index(colour_index_map, // changes reference
+                                                      colour_index_running, // changes reference
+                                                      vertices[tris[i][0]].color,
+                                                      vertices[tris[i][1]].color,
+                                                      vertices[tris[i][2]].color);
          PyList_SetItem(tri_py, 0, PyLong_FromLong(tris[i][0]));
          PyList_SetItem(tri_py, 1, PyLong_FromLong(tris[i][1]));
          PyList_SetItem(tri_py, 2, PyLong_FromLong(tris[i][2]));
