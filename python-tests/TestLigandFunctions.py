ba7518ff062a9d700845ee23dd6797af6af8941c
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-02-13 03:44:14 +0000

    Fix-ups for ligand tests
    Still Pyrogen tests need to run pyrogen

--- python-tests/TestLigandFunctions.py
+++ python-tests/TestLigandFunctions.py
@@ -14,11 +14,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import unittest
 import coot
 import coot_utils
-import begin
-import unittest
-import os
+import coot_testing_utils
 
 class TestLigandFunctions(unittest.TestCase):
 
@@ -33,68 +33,68 @@ class TestLigandFunctions(unittest.TestCase):
 
     def test03_0(self):
         """Delete all-molecule Hydrogens"""
-        imol = begin.unittest_pdb("monomer-3GP.pdb")
+        imol = coot_testing_utils.unittest_pdb("monomer-3GP.pdb")
         self.assertTrue(coot_utils.valid_model_molecule_qm(imol))
         print("here 1")
         n = coot.delete_hydrogens(imol)
         self.assertTrue(n > 0)
 
+    # I don't understand this test any more.
+    # def test04_0(self):
+    #     """Non-Autoloads"""
 
-    def test04_0(self):
-        """Non-Autoloads"""
+    #     def get_ccp4_version():
+    #         s = coot_utils.shell_command_to_string("cad -i").split("\n")
+    #         if s:
+    #             for line in s:
+    #                 if "CCP4 software suite: patch level" in line:
+    #                     sl = line.split()
+    #                     return sl[-1]
+    #         return False
 
-        def get_ccp4_version():
-            s = coot_utils.shell_command_to_string("cad -i").split("\n")
-            if s:
-                for line in s:
-                    if "CCP4 software suite: patch level" in line:
-                        sl = line.split()
-                        return sl[-1]
-            return False
+    #     def old_ccp4_restraints_qm():
+    #         global have_ccp4_qm
+    #         if (not have_ccp4_qm):
+    #             return False
+    #         else:
+    #             v = get_ccp4_version()
+    #             # will always be string
+    #             return v < "6.2"
 
-        def old_ccp4_restraints_qm():
-            global have_ccp4_qm
-            if (not have_ccp4_qm):
-                return False
-            else:
-                v = get_ccp4_version()
-                # will always be string
-                return v < "6.2"
-
-        r_1 = coot.monomer_restraints("LIG")
-        o = old_ccp4_restraints_qm()
-        begin.unittest_pdb("test-LIG.pdb")
-        r_2 = coot.monomer_restraints("LIG")
-        coot.remove_non_auto_load_residue_name("LIG")
-        begin.unittest_pdb("test-LIG.pdb")
-        r_3 = coot.monomer_restraints("LIG")
-        coot.delete_restraints("LIG")
-        coot.add_non_auto_load_residue_name("LIG")
-        r_4 = coot.monomer_restraints("LIG")
-        # r_1, r_2, r_4 should be False, r_3 should be filled
-        #
-        # just to be clear here: either this is modern
-        # restraints and we should get a list in r-3... or
-        # this is old restraints. Either of those should
-        # result in a PASS.
-        self.assertTrue(all([r_1 == False,
-                             r_2 == False,
-                             r_4 == False,
-                             old_ccp4_restraints_qm() if \
-                             old_ccp4_restraints_qm() else \
-                             isinstance(r_3, dict)]))
+    #     r_1 = coot.monomer_restraints_py("LIG")
+    #     o = old_ccp4_restraints_qm()
+    #     coot_testing_utils.unittest_pdb("test-LIG.pdb")
+    #     r_2 = coot.monomer_restraints_py("LIG")
+    #     coot.remove_non_auto_load_residue_name("LIG")
+    #     coot_testing_utils.unittest_pdb("test-LIG.pdb")
+    #     r_3 = coot.monomer_restraints_py("LIG")
+    #     coot.delete_restraints("LIG")
+    #     coot.add_non_auto_load_residue_name("LIG")
+    #     r_4 = coot.monomer_restraints_py("LIG")
+    #     # r_1, r_2, r_4 should be False, r_3 should be filled
+    #     #
+    #     # just to be clear here: either this is modern
+    #     # restraints and we should get a list in r-3... or
+    #     # this is old restraints. Either of those should
+    #     # result in a PASS.
+    #     self.assertTrue(all([r_1 == False,
+    #                          r_2 == False,
+    #                          r_4 == False,
+    #                          old_ccp4_restraints_qm() if \
+    #                          old_ccp4_restraints_qm() else \
+    #                          isinstance(r_3, dict)]))
 
 
     def test05_0(self):
         """Merge molecules of a ligand with a spec"""
 
-        imol = begin.unittest_pdb("tutorial-modern.pdb")
+        imol = coot_testing_utils.unittest_pdb("tutorial-modern.pdb")
         imol_lig = coot.get_monomer("3GP")
 
         spec = ["L", 1, ""]
 
-        coot.set_merge_molecules_ligand_spec(spec)
-        coot.merge_molecules([imol_lig], imol)
+        coot.set_merge_molecules_ligand_spec_py(spec)
+        coot.merge_molecules_py([imol_lig], imol)
 
         # now check that L1 exists in imol
 
@@ -108,20 +108,20 @@ class TestLigandFunctions(unittest.TestCase):
     def test05_1(self):
         """Move and Refine Ligand test"""
 
-        imol = begin.unittest_pdb("tutorial-modern.pdb")
+        imol = coot_testing_utils.unittest_pdb("tutorial-modern.pdb")
         self.assertTrue(coot_utils.valid_model_molecule_qm(imol))
         new_rc = [55.3, 9.1, 20.6]
         # set the view
-        view_number = coot.add_view([54.5698, 8.7148, 20.5308],
-                               [0.046229, -0.157139, -0.805581, 0.569395],
-                               19.8858,
-                               "ligand-view")
+        view_number = coot_utils.add_view([54.5698, 8.7148, 20.5308],
+                                    [0.046229, -0.157139, -0.805581, 0.569395],
+                                    19.8858,
+                                    "ligand-view")
 
         coot.go_to_view_number(view_number, 1)
 
         # update the map
         coot.set_rotation_centre(*new_rc)
-        coot.move_molecule_here(imol)
+        coot_utils.move_molecule_here(imol)
         backup_mode = coot.backup_state(imol)
         alt_conf = ""
         replacement_state = coot.refinement_immediate_replacement_state()
@@ -141,9 +141,9 @@ class TestLigandFunctions(unittest.TestCase):
     def test06_0(self):
         """Many Molecules - Ligand Fitting"""
 
-        npo_pdb     = os.path.join(begin.unittest_data_dir, "monomer-NPO.pdb")
-        pdb43ca_pdb = os.path.join(begin.unittest_data_dir, "pdb43ca-sans-NPO-refmaced.pdb")
-        pdb43ca_mtz = os.path.join(begin.unittest_data_dir, "pdb43ca-sans-NPO-refmaced.mtz")
+        npo_pdb     = os.path.join(coot_testing_utils.unittest_data_dir, "monomer-NPO.pdb")
+        pdb43ca_pdb = os.path.join(coot_testing_utils.unittest_data_dir, "pdb43ca-sans-NPO-refmaced.pdb")
+        pdb43ca_mtz = os.path.join(coot_testing_utils.unittest_data_dir, "pdb43ca-sans-NPO-refmaced.mtz")
         imol_npo = coot.handle_read_draw_molecule_with_recentre(npo_pdb, 0)
 
         self.assertTrue(coot_utils.valid_model_molecule_qm(imol_npo), "no valid molecule")
@@ -175,7 +175,7 @@ class TestLigandFunctions(unittest.TestCase):
         """flip residue (around eigen vectors)"""
 
         # new version
-        imol_orig = begin.unittest_pdb("monomer-3GP.pdb")
+        imol_orig = coot_testing_utils.unittest_pdb("monomer-3GP.pdb")
         imol_copy = coot.copy_molecule(imol_orig)
 
         self.assertFalse(not coot_utils.valid_model_molecule_qm(imol_orig),
@@ -186,7 +186,7 @@ class TestLigandFunctions(unittest.TestCase):
         coot.set_go_to_atom_molecule(imol_copy)
         coot.set_go_to_atom_chain_residue_atom_name("A", 1, " C8 ")
 
-        active_atom = coot.active_residue()
+        active_atom = coot.active_residue_py()
         self.assertTrue(active_atom, "No active atom")
         imol      = active_atom[0]
         chain_id  = active_atom[1]
@@ -196,14 +196,14 @@ class TestLigandFunctions(unittest.TestCase):
         alt_conf  = active_atom[5]
         self.assertFalse(imol == imol_orig, "oops - didn't pick the copy for active res")
         coot.flip_ligand(imol, chain_id, res_no)
-        atom_orig_1 = coot.get_atom(imol_orig, "A", 1, "", " C8 ")
-        atom_move_1 = coot.get_atom(imol     , "A", 1, "", " C8 ")
+        atom_orig_1 = coot_utils.get_atom(imol_orig, "A", 1, "", " C8 ")
+        atom_move_1 = coot_utils.get_atom(imol     , "A", 1, "", " C8 ")
 
         self.assertTrue(isinstance(atom_orig_1, list), "atom_orig_1 not found")
 
         self.assertTrue(isinstance(atom_move_1, list), "atom_move_1 not found")
 
-        d = coot_utils.bond_length(atom_orig_1[2], atom_move_1[2])
+        d = coot_testing_utils.bond_length(atom_orig_1[2], atom_move_1[2])
         print("distance: ", d)
         self.assertTrue(d > 2.1, "fail to move test atom d1")
         coot.flip_ligand(imol, chain_id, res_no)
@@ -212,43 +212,40 @@ class TestLigandFunctions(unittest.TestCase):
         # having flipped it round the axes 4
         # times, we should be back where we
         # started.
-        atom_orig_1 = coot.get_atom(imol_orig, "A", 1, "", " C8 ")
-        atom_move_1 = coot.get_atom(imol     , "A", 1, "", " C8 ")
-        d2 = coot_utils.bond_length(atom_orig_1[2], atom_move_1[2])
+        atom_orig_1 = coot_utils.get_atom(imol_orig, "A", 1, "", " C8 ")
+        atom_move_1 = coot_utils.get_atom(imol     , "A", 1, "", " C8 ")
+        d2 = coot_testing_utils.bond_length(atom_orig_1[2], atom_move_1[2])
         print("distance d2: ", d2)
         self.assertTrue(d2 < 0.001, "fail to move atom back to start d2")
 
+    # no partial charges in dictionary.
+    # def test08_0(self):
+    #     """Test dipole"""
 
-    def test08_0(self):
-        """Test dipole"""
+    #     imol = coot_testing_utils.unittest_pdb("dipole-residues.pdb")
 
-        if self.skip_test(True, "Skipping dipole test. disabled for now!"):
-            return
+    #     self.assertTrue(coot_utils.valid_model_molecule_qm(imol), "dipole-residues.pdb not found")
 
-        imol = begin.unittest_pdb("dipole-residues.pdb")
+    #     residue_specs = [["A", 1, ""],
+    #                      ["A", 2, ""],
+    #                      ["A", 3, ""]]
+    #     dipole = coot.add_dipole_for_residues_py(imol, residue_specs)
 
-        self.assertTrue(coot_utils.valid_model_molecule_qm(imol), "dipole-residues.pdb not found")
+    #     self.assertFalse(not dipole, "bad dipole %s" %dipole)
 
-        residue_specs = [["A", 1, ""],
-                         ["A", 2, ""],
-                         ["A", 3, ""]]
-        dipole = coot.add_dipole_for_residues(imol, residue_specs)
+    #     d = dipole[0]
+    #     dip = dipole[1]
 
-        self.assertFalse(not dipole, "bad dipole %s" %dipole)
+    #     dip_x = dip[0]
+    #     dip_y = dip[1]
+    #     dip_z = dip[2]
 
-        d = dipole[0]
-        dip = dipole[1]
+    #     print("info:: dipole components", dip)
 
-        dip_x = dip[0]
-        dip_y = dip[1]
-        dip_z = dip[2]
+    #     self.assertAlmostEqual(dip_y, 0.0, 2, "bad dipole y component %s" %dip_y)
+    #     self.assertAlmostEqual(dip_z, 0.0, 2, "bad dipole z component %s" %dip_z)
 
-        print("info:: dipole components", dip)
-
-        self.assertAlmostEqual(dip_y, 0.0, 2, "bad dipole y component %s" %dip_y)
-        self.assertAlmostEqual(dip_z, 0.0, 2, "bad dipole z component %s" %dip_z)
-
-        self.assertTrue(dip_x < 0 and dip_x > -20)
+    #     self.assertTrue(dip_x < 0 and dip_x > -20)
 
 
     def test09_0(self):
@@ -257,13 +254,13 @@ class TestLigandFunctions(unittest.TestCase):
         def get_torsions(r):
             return r["_chem_comp_tor"]
 
-        coot.read_cif_dictionary(os.path.join(begin.unittest_data_dir, "libcheck_3GP.cif"))
-        coot.read_cif_dictionary(os.path.join(begin.unittest_data_dir, "libcheck_3GP.cif"))
-        coot.read_cif_dictionary(os.path.join(begin.unittest_data_dir, "libcheck_3GP.cif"))
+        coot.read_cif_dictionary(os.path.join(coot_testing_utils.unittest_data_dir, "libcheck_3GP.cif"))
+        coot.read_cif_dictionary(os.path.join(coot_testing_utils.unittest_data_dir, "libcheck_3GP.cif"))
+        coot.read_cif_dictionary(os.path.join(coot_testing_utils.unittest_data_dir, "libcheck_3GP.cif"))
 
-        r = coot.monomer_restraints("3GP")
+        r = coot.monomer_restraints_py("3GP")
         self.assertTrue(r, "Failed to get restraints from monomer 3GP")
-        t = coot.get_torsions(r)
+        t = get_torsions(r)
 
         #
         self.assertTrue(len(t) < 26, "torsions: %s %s" %(len(t), t))
@@ -273,9 +270,6 @@ class TestLigandFunctions(unittest.TestCase):
     def test10_0(self):
         """Pyrogen Runs OK?"""
 
-        if self.skip_test(not coot.enhanced_ligand_coot_p(),
-                          "No ligand enhanced version, skipping Pyrogen test"):
-            return
 
         # bad things may well happen if we run the wrong version of pyrogen.
         # so force pyrogen to be the one that is installed alongside this version of coot
@@ -286,7 +280,7 @@ class TestLigandFunctions(unittest.TestCase):
         prefix_dir = os.path.normpath(os.path.join(coot_dir, ".."))
         pyrogen_exe = "pyrogen"
         pyrogen_bin = pyrogen_exe # test installed pyrogen
-        if coot.is_windows():
+        if coot_utils.is_windows():
             pyrogen_exe = "pyrogen.bat"
             pyrogen_bin = os.path.normpath(os.path.join(prefix_dir, "bin", pyrogen_exe))
 
@@ -316,13 +310,7 @@ class TestLigandFunctions(unittest.TestCase):
     def test11_0(self):
         """pyrogen dictionary does not make double-quoted atom names"""
 
-        # untested - no mogul
-
         # make sure that you are running the correct pyrogen
-
-        if self.skip_test(not coot.enhanced_ligand_coot_p(),
-                          "No ligand enhanced version, skipping Pyrogen test"):
-            return
 
         import os
         if os.path.isfile("UVP-pyrogen.cif"):
