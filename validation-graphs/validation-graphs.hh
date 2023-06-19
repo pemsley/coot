0fae5741067665c0a3d1cedae6ccf7f4e3e95312
Jakub Smulski <hgonomeg@gmail.com>
2023-06-19 16:59:54 +0200

    Remove old validation graphs class

--- validation-graphs/validation-graphs.hh
+++ validation-graphs/validation-graphs.hh
@@ -22,26 +22,6 @@ namespace coot {
     /// `static_cast<validation_graph_type_repr_t>(enum_instance)`
     typedef std::underlying_type<validation_graph_type>::type validation_graph_type_repr_t;
     std::string validation_graph_type_to_human_name(validation_graph_type graph_type);
-
-    //todo: remove this class later
-    class validation_graphs_t {
-
-    public:
-        GtkWidget *dynarama_is_displayed;
-        GtkWidget *sequence_view_is_displayed;
-        GtkWidget *geometry_graph;
-        GtkWidget *b_factor_variance_graph;
-        ////B B GRAPH
-        GtkWidget *b_factor_graph;
-        ////E B GRAPH
-        GtkWidget *residue_density_fit_graph;
-        GtkWidget *omega_distortion_graph;
-        GtkWidget *rotamer_graph;
-        GtkWidget *ncs_diffs_graph;
-        validation_graphs_t();
-
-        void init();
-    };
 } // namespace coot
 
 #endif // VALIDATION_GRAPHS_HH
