7938fe76a7ab6ebf6af6c5353f52c6fcc1872a69
Jakub Smulski <hgonomeg@gmail.com>
2023-06-28 20:42:59 +0200

    Increase the width of the wavy bonds once again

--- new_ligand_builder/ligand_editor_canvas/model.cpp
+++ new_ligand_builder/ligand_editor_canvas/model.cpp
@@ -236,7 +236,7 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
             switch (bond.geometry) {
                 default:
                 case BondGeometry::Unspecified:{
-                    const float wave_arc_length_base = 0.18f;
+                    const float wave_arc_length_base = 0.25f;
                     const float wave_arc_radius = wave_arc_length_base * scale_factor / 2.f;
                     auto [full_vec_x, full_vec_y] = bond.get_vector();
                     float base_angle = std::atan(full_vec_y / full_vec_x);
