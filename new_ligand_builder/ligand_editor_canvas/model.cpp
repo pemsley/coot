794920d7bad743120f331f54c6ded92c1befe369
Jakub Smulski <hgonomeg@gmail.com>
2023-06-28 19:22:18 +0200

    Fix a weird wavy bond highlighting bug

--- new_ligand_builder/ligand_editor_canvas/model.cpp
+++ new_ligand_builder/ligand_editor_canvas/model.cpp
@@ -260,7 +260,7 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
                         //cairo_move_to(cr, current_x + next_x, current_y + next_y);
                         cairo_new_sub_path(cr);
                         cairo_arc(cr, current_x, current_y, wave_arc_radius, angle_one, angle_two);
-                        cairo_stroke_preserve(cr);
+                        cairo_stroke(cr);
                         current_x = next_x;
                         current_y = next_y;
                     }
