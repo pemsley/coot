99a8964cd54fe8e9bfebb837dcc06c316cdfff7b
Jakub Smulski <hgonomeg@gmail.com>
2023-07-06 18:07:04 +0200

    Simplify drawing subroutine after explicit hydrogens are skipped (again)

--- new_ligand_builder/ligand_editor_canvas/model.cpp
+++ new_ligand_builder/ligand_editor_canvas/model.cpp
@@ -572,11 +572,7 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
         };
 
         
-        if(atom.symbol == "H") {
-            process_highlight();
-            // Ignore Hydrogen.
-            g_warning_once("Explicit hydrogen present in drawing representation!");
-        } else if(atom.symbol == "C") {
+        if(atom.symbol == "C") {
             if(atom.appendix.has_value()) {
                 auto [markup,reversed] = process_appendix(atom.symbol,atom.appendix);
                 render_symbol_on_background(markup,atom.color,atom.highlighted,reversed);
