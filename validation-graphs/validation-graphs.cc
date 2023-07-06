cbdd6aa3cce19aae07f54dc8a0757f631384daba
Jakub Smulski <hgonomeg@gmail.com>
2023-07-06 15:32:23 +0200

    Remove remnants of old junk

--- validation-graphs/validation-graphs.cc
+++ validation-graphs/validation-graphs.cc
@@ -1,22 +1,5 @@
 #include "validation-graphs.hh"
 
-
-void coot::validation_graphs_t::init() {
-    dynarama_is_displayed = NULL;
-    sequence_view_is_displayed = NULL;
-    geometry_graph = NULL;
-    b_factor_variance_graph = NULL;
-    b_factor_graph = NULL;
-    residue_density_fit_graph = NULL;
-    omega_distortion_graph = NULL;
-    rotamer_graph = NULL;
-    ncs_diffs_graph = NULL;
-}
-
-coot::validation_graphs_t::validation_graphs_t() {
-    init(); 
-}
-
 std::string coot::validation_graph_type_to_human_name(coot::validation_graph_type graph_type) {
     switch (graph_type) {
         case validation_graph_type::density_fit: {
