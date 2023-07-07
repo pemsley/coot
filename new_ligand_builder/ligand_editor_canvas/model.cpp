f59857ff715d2c7ee66967fff81652e59d25fe59
Jakub Smulski <hgonomeg@gmail.com>
2023-07-07 14:41:36 +0200

    Fix exception and add render some visual feedback for testing

--- new_ligand_builder/ligand_editor_canvas/model.cpp
+++ new_ligand_builder/ligand_editor_canvas/model.cpp
@@ -367,9 +367,6 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
         // Returns on-screen bond coordinates
         // cropped not to overlap with atoms' symbols and appendices.
         auto cropped_bond_coords = [&]() -> std::pair<graphene_point_t,graphene_point_t> {
-            auto first_rect = atom_idx_to_canvas_rect.at(bond->first_atom_idx);
-            auto second_rect = atom_idx_to_canvas_rect.at(bond->second_atom_idx);
-
             graphene_point_t first_atom;
             first_atom.x = bond->first_atom_x * scale_factor + x_offset;
             first_atom.y = bond->first_atom_y * scale_factor + y_offset;
@@ -378,8 +375,8 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
             second_atom.x = bond->second_atom_x * scale_factor + x_offset;
             second_atom.y = bond->second_atom_y * scale_factor + y_offset;
 
-            float bond_vec_x = second_atom.x - first_atom.x;
-            float bond_vec_y = second_atom.y - first_atom.y;
+            graphene_point_t a = first_atom;
+            graphene_point_t b = second_atom;
 
             // We pass in the bond vector so that it always points "away" from the point
             auto crop_line_against_rect = [](const graphene_rect_t& rect, float bond_vec_x, float bond_vec_y, const graphene_point_t& point){
@@ -397,13 +394,33 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
                         ret.y = rect.origin.y;
                     }
                 } 
-
                 return ret;
             };
 
-            auto a = crop_line_against_rect(first_rect, bond_vec_x, bond_vec_y, first_atom);
-            auto b = crop_line_against_rect(second_rect, -bond_vec_x, -bond_vec_y, second_atom);
+            float bond_vec_x = second_atom.x - first_atom.x;
+            float bond_vec_y = second_atom.y - first_atom.y;
+
+            auto first_rect_iter = atom_idx_to_canvas_rect.find(bond->first_atom_idx);
+            if(first_rect_iter != atom_idx_to_canvas_rect.end()) {
+                a = crop_line_against_rect(first_rect_iter->second, bond_vec_x, bond_vec_y, first_atom);
+            }
+            auto second_rect_iter = atom_idx_to_canvas_rect.find(bond->second_atom_idx);
+            if(first_rect_iter != atom_idx_to_canvas_rect.end()) {
+                b = crop_line_against_rect(second_rect_iter->second, -bond_vec_x, -bond_vec_y, second_atom);
+            }
+            
             return std::make_pair(a,b);
+        };
+
+        auto draw_cropped_bond_for_testing = [&](){
+            auto [first,second] = cropped_bond_coords();
+            double w = cairo_get_line_width(cr);
+
+            cairo_set_line_width(cr,1.0);
+            cairo_move_to(cr, first.x + 2.f, first.y + 2.f);
+            cairo_line_to(cr, second.x + 2.f, second.y + 2.f);
+            cairo_stroke(cr);
+            cairo_set_line_width(cr, w);
         };
 
         auto draw_central_bond_line = [&](){
@@ -635,6 +652,7 @@ void CanvasMolecule::draw(GtkSnapshot* snapshot, PangoLayout* pango_layout, cons
                 default:
                 case BondType::Single:{
                     draw_central_bond_line();
+                    draw_cropped_bond_for_testing();
                     break;
                 }
             }
