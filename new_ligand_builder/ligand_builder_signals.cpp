75944cac094e8c4baa46872da424b378f50ed8d1
Jakub Smulski <hgonomeg@gmail.com>
2023-08-02 15:06:01 +0200

    Connect layla_on_apply

--- new_ligand_builder/ligand_builder_signals.cpp
+++ new_ligand_builder/ligand_builder_signals.cpp
@@ -1,8 +1,15 @@
 #include <gtk/gtk.h>
+#include "ligand_builder_state.hpp"
 
 extern "C" G_MODULE_EXPORT
 void
 layla_on_close(GtkButton* button, gpointer user_data) {
     // todo: this should probably do some checks before just closing
     gtk_window_close(GTK_WINDOW(user_data));
+}
+
+extern "C" G_MODULE_EXPORT
+void
+layla_on_apply(GtkButton* button, gpointer user_data) {
+    g_warning("TODO: Implement 'Apply'");
 }
