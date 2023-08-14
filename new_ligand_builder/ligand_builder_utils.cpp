dd2fe709e8d70c66cc7675006d3a9c56c71bdfc4
Jakub Smulski <hgonomeg@gmail.com>
2023-08-14 19:28:34 +0200

    Propose an implementation for the new remove_non_polar_hydrogens()

--- new_ligand_builder/ligand_builder_utils.cpp
+++ new_ligand_builder/ligand_builder_utils.cpp
@@ -1,6 +1,18 @@
 #include "ligand_builder_utils.hpp"
 
 void coot::ligand_editor::remove_non_polar_hydrogens(RDKit::RWMol* mol) {
-    
+    std::vector<RDKit::Atom*> atoms_to_be_removed;
+
     auto atoms = mol->atoms();
+    for(RDKit::Atom* atom: atoms) {
+        if(atom->getAtomicNum() == 1) {
+            if(atom->getFormalCharge() == 0) {
+                atoms_to_be_removed.push_back(atom);
+            }
+        }
+    }
+
+    for(RDKit::Atom* atom: atoms_to_be_removed) {
+        mol->removeAtom(atom);
+    }
 }
