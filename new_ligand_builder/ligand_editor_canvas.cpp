ca1c89224f320845587c46e2db147f3dc9399a4c
Jakub Smulski <hgonomeg@gmail.com>
2023-08-15 17:42:24 +0200

    Implement 'coot_ligand_editor_draw_on_cairo_surface()'

--- new_ligand_builder/ligand_editor_canvas.cpp
+++ new_ligand_builder/ligand_editor_canvas.cpp
@@ -3,6 +3,7 @@
 #include "ligand_editor_canvas/core.hpp"
 #include "ligand_editor_canvas/model.hpp"
 #include "ligand_editor_canvas/tools.hpp"
+#include "pango/pangocairo.h"
 #include <exception>
 #include <utility>
 #include <algorithm>
@@ -417,5 +418,7 @@ std::string coot_ligand_editor_get_smiles_for_molecule(CootLigandEditorCanvas* s
 }
 
 void coot_ligand_editor_draw_on_cairo_surface(CootLigandEditorCanvas* self, cairo_t* cr) noexcept {
-    g_error("todo: implement me");
+    PangoLayout* pango_layout = pango_cairo_create_layout(cr);
+    impl::Renderer ren(cr, pango_layout);
+    self->render(ren);
 }
