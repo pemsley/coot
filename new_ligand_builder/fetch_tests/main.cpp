28ffc5f91a9b92125fc7cf5ba9ebfd8e3c8fc6c5
Jakub Smulski <hgonomeg@gmail.com>
2023-09-04 16:52:55 +0200

    Try to get a primitive example of Fetch to work

---
+++ new_ligand_builder/fetch_tests/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../../src/cc-interface-network.hh"
+#include "../../src/coot-setup-python.hh"
+
+int main(int argc, char** argv) {
+    setup_python_basic(argc, argv);
+    setup_python_coot_module();
+    std::string input;
+    std::cout<<"Drug to fetch: \n";
+    std::cin>>input;
+    auto res = get_drug_via_wikipedia_and_drugbank_py(input);
+    std::cout<<"\nResult: \n"<<res<<'\n';
+}
