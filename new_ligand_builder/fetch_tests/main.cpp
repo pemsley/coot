f88e5c2f0e8ac11976429e3fcd7a01844b1feb74
Jakub Smulski <hgonomeg@gmail.com>
2023-09-06 12:38:40 +0200

    Experiment with repl

--- new_ligand_builder/fetch_tests/main.cpp
+++ new_ligand_builder/fetch_tests/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <string>
 #include "../../src/cc-interface-network.hh"
 #include "../../src/coot-setup-python.hh"
+
+
+void repl() {
+    std::cout<<"Coot pyrepl:\n";
+    while(true) {
+        std::string input;
+        std::getline(std::cin,input);
+        PyRun_SimpleString(input.c_str());
+    }
+}
 
 int main(int argc, char** argv) {
     setup_python_basic(argc, argv);
     setup_python_coot_module();
     PyRun_SimpleString("import coot_utils");
+    repl();
+    return 0;
+
     std::string input;
     std::cout<<"Drug to fetch: \n";
     std::cin>>input;
