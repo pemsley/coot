cb877d7b8552dfa5de01ef1148c2008cd9204053
Jakub Smulski <hgonomeg@gmail.com>
2023-09-09 18:36:51 +0200

    Find a way to get a return value from Python

--- new_ligand_builder/fetch_tests/main.cpp
+++ new_ligand_builder/fetch_tests/main.cpp
@@ -6,10 +6,37 @@
 
 void repl() {
     std::cout<<"Coot pyrepl:\n";
+    PyObject *am = PyImport_AddModule("__main__");
+    PyObject* d = PyModule_GetDict(am);
     while(true) {
         std::string input;
+        std::cout<<"\n> ";
+        std::flush(std::cout);
         std::getline(std::cin,input);
-        PyRun_SimpleString(input.c_str());
+        PyObject* source_code = Py_CompileString(input.c_str(), "adhoc", Py_eval_input);
+        if(!source_code) {
+            std::cout<<"Null source_code.\n";
+            PyErr_Print();
+            continue;
+        }
+        PyObject* func = PyFunction_New(source_code, d);
+        PyObject* result = PyObject_CallObject(func, PyTuple_New(0));
+        if(!result) {
+            std::cout<<"Null result.\n";
+            PyErr_Print();
+            Py_XDECREF(func);
+            Py_XDECREF(source_code);
+            continue;
+        }
+        const char* s = PyUnicode_AsUTF8(result);
+        if(!s) {
+            std::cout<<"Result is most likely not a string.\n";
+        } else {
+            std::cout<<"Result: "<<s<<'\n';
+        }
+        Py_XDECREF(result);
+        Py_XDECREF(func);
+        Py_XDECREF(source_code);
     }
 }
 
