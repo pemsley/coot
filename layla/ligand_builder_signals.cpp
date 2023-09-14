abe72e2258aa81de9e8b82ef133f08dd0cf7c1b3
Jakub Smulski <hgonomeg@gmail.com>
2023-09-14 15:04:04 +0200

    Add copyrights

--- layla/ligand_builder_signals.cpp
+++ layla/ligand_builder_signals.cpp
@@ -1,3 +1,24 @@
+/* layla/ligand_builder_signals.cpp
+ * 
+ * Copyright 2023 by Global Phasing Ltd.
+ * Author: Jakub Smulski
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
+
 #include <gtk/gtk.h>
 #include "ligand_builder_state.hpp"
 #include "ligand_builder_generators.hpp"
