4fee94409cfc01f050d6b36aac509f07a9b11ef9
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-09-15 22:15:33 +0100

    Add layla to the build system
    It is not integrated into the coot binary.
    There are problems with compilation of source files in the
    ligand_editor_canvas sub-directory.

--- layla/main.cpp
+++ layla/main.cpp
@@ -19,47 +19,66 @@
  * 02110-1301, USA
  */
 
+#include "Python.h"
 #include <gtk/gtk.h>
 #include <thread>
+#include "utils/coot-utils.hh"
 #include "ligand_builder_state.hpp"
 #include "ligand_builder_generators.hpp"
 #include "ligand_builder_ui.hpp"
-#include "../src/coot-setup-python.hh"
+#include "python-utils.hpp"
+
+
+// #include "../src/coot-setup-python.hh"
 
 int main(int argc, char** argv) {
     using namespace coot::ligand_editor;
 
     std::thread python_init_thread([argc,argv](){
-        setup_python_basic(argc, argv);
-        setup_python_coot_module();
-        PyRun_SimpleString("import coot_utils");
+        layla_setup_python_basic(argc, argv);
+        // layla_setup_python_module("coot");
+        // layla_setup_python_module("coot_utils");
     });
 
     python_init_thread.detach();
 
     gtk_init();
     
-    GtkApplication* app = gtk_application_new("org.pemsley.Layla",G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication* app = gtk_application_new("org.pemsley.Layla", G_APPLICATION_DEFAULT_FLAGS);
     GError *error = NULL;
     g_application_register(G_APPLICATION(app), NULL, &error);
 
-    g_signal_connect(app,"activate",G_CALLBACK(+[](GtkApplication* app, gpointer user_data){
+    g_signal_connect(app, "activate", G_CALLBACK(+[](GtkApplication* app, gpointer user_data) {
+
         // todo: Make this not use a relative path:
-        GtkBuilder* builder = gtk_builder_new_from_file("layla.ui");
+        std::string dir = coot::package_data_dir();
+        // all ui files should live here:
+        std::string dir_ui = coot::util::append_dir_dir(dir, "ui");
+        std::string ui_file_name = "layla.ui";
+        std::string ui_file_full = coot::util::append_dir_file(dir_ui, ui_file_name);
+        if (coot::file_exists(ui_file_name))
+            ui_file_full = ui_file_name;
+        GError* error = NULL;
+        GtkBuilder* builder = gtk_builder_new();
+        gboolean status = gtk_builder_add_from_file(builder, ui_file_full.c_str(), &error);
+        if (status == FALSE) {
+            std::cout << "ERROR:: Failure to read or parse " << ui_file_full << std::endl;
+            std::cout << error->message << std::endl;
+            exit(0);
+        }
         
         auto* win = coot::ligand_editor::setup_main_window(app,builder);
 
         // for now
         auto* icon_theme = gtk_icon_theme_get_for_display(gtk_widget_get_display(GTK_WIDGET(win)));
         gtk_icon_theme_add_search_path(icon_theme, "icons");
-        
+
         coot::ligand_editor::global_layla_gtk_builder = builder;
         coot::ligand_editor::global_generator_request_task_cancellable = nullptr;
-        
+
         gtk_window_present(GTK_WINDOW(win));
         gtk_application_add_window(app,GTK_WINDOW(win));
-    }),NULL);
-
+    }), NULL);
 
     auto ret = g_application_run(G_APPLICATION(app),0,0);
     g_info("Exiting...");
