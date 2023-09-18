85d860012db1cc753ffa17b26120d9384140977f
Jakub Smulski <hgonomeg@gmail.com>
2023-09-18 20:51:35 +0200

    Move "Python utils" where they belong and remove a useless function

--- layla/python-utils.hpp
+++ layla/python-utils.hpp
@@ -1,4 +1,8 @@
+#include <string>
+#include "Python.h"
 
 void layla_setup_python_basic(int argc, char **argv);
 void layla_setup_python_coot_module();
 void layla_setup_python_module(const std::string &module_name);
+PyObject *layla_safe_python_command_with_return(const std::string &python_cmd);
+std::string get_drug_via_wikipedia_and_drugbank_py(const std::string &drugname);
