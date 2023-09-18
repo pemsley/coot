b58bdad504bb6beb9279dc61517ec6703364b25d
Jakub Smulski <hgonomeg@gmail.com>
2023-09-18 20:54:04 +0200

    Add missing copyright and missing ifndef/define/endif

--- layla/python-utils.hpp
+++ layla/python-utils.hpp
@@ -1,3 +1,26 @@
+/* layla/python-utils.hpp
+ * 
+ * Copyright 2023 by Global Phasing Ltd.
+ * Author: Jakub Smulski
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
+
+#ifndef LAYLA_PYTHON_UTILS_HPP
+#define LAYLA_PYTHON_UTILS_HPP
 #include <string>
 #include "Python.h"
 
@@ -6,3 +29,5 @@ void layla_setup_python_coot_module();
 void layla_setup_python_module(const std::string &module_name);
 PyObject *layla_safe_python_command_with_return(const std::string &python_cmd);
 std::string get_drug_via_wikipedia_and_drugbank_py(const std::string &drugname);
+
+#endif // LAYLA_PYTHON_UTILS_HPP
