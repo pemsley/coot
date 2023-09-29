c43c28befbce18ee7deeb82f53a258762e9b02d9
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-09-29 16:14:09 +0100

    Add more includes for test-molecules-container.hh

--- api/test-molecules-container.hh
+++ api/test-molecules-container.hh
@@ -2,9 +2,14 @@
 #define TEST_MOLECULES_CONTAINER_HH
 
 #include <string>
+#include <mmdb2/mmdb_manager.h>
+#include "coords/Cartesian.h"
 
 void starting_test(const char *func);
 std::string reference_data(const std::string &file);
+namespace coot {
+   Cartesian atom_to_cartesian(mmdb::Atom *at);
+}
 
 
 #endif // TEST_MOLECULES_CONTAINER_HH
