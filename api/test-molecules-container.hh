b1a713797991d6ca13b56648676af1012e32d347
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-09-29 16:11:09 +0100

    Add test-molecules-container.hh header

---
+++ api/test-molecules-container.hh
@@ -0,0 +1,10 @@
+#ifndef TEST_MOLECULES_CONTAINER_HH
+#define TEST_MOLECULES_CONTAINER_HH
+
+#include <string>
+
+void starting_test(const char *func);
+std::string reference_data(const std::string &file);
+
+
+#endif // TEST_MOLECULES_CONTAINER_HH
