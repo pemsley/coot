01a6231bd55e5c1d1b23169c940f76bb8648ee88
Jakub Smulski <hgonomeg@gmail.com>
2023-10-16 13:26:48 +0200

    Continue work on the build script

--- lhasa/initial_build.sh
+++ lhasa/initial_build.sh
@@ -46,24 +46,24 @@ emar q ${INSTALL_DIR}/lib/libboost_wserialization.a ${INSTALL_DIR}/lib/libboost_
 #RDKit
 mkdir -p ${BUILD_DIR}/rdkit_build
 cd ${BUILD_DIR}/rdkit_build
-emcmake cmake -Dboost_iostreams_DIR=${INSTALL_DIR}/lib/cmake/boost_iostreams-1.83.0/ \
-              -Dboost_system_DIR=${INSTALL_DIR}/lib/cmake/boost_system-1.83.0/       \
-              -Dboost_headers_DIR=${INSTALL_DIR}/lib/cmake/boost_headers-1.83.0/     \ 
-              -DBoost_DIR=${INSTALL_DIR}/lib/cmake/Boost-1.83.0/                     \
-              -DRDK_BUILD_PYTHON_WRAPPERS=OFF                                        \
-              -DRDK_INSTALL_STATIC_LIBS=ON                                           \
-              -DRDK_INSTALL_INTREE=OFF                                               \
-              -DRDK_BUILD_SLN_SUPPORT=OFF                                            \
-              -DRDK_TEST_MMFF_COMPLIANCE=OFF                                         \
-              -DRDK_BUILD_CPP_TESTS=OFF                                              \
-              -DRDK_USE_BOOST_SERIALIZATION=ON                                       \
-              -DRDK_BUILD_THREADSAFE_SSS=OFF                                         \
-              -DBoost_INCLUDE_DIR=${INSTALL_DIR}/include                             \
-              -DBoost_USE_STATIC_LIBS=ON                                             \
-              -DBoost_USE_STATIC_RUNTIME=ON                                          \
-              -DBoost_DEBUG=TRUE                                                     \
+emcmake cmake -Dboost_iostreams_DIR="${INSTALL_DIR}/lib/cmake/boost_iostreams-1.83.0"  \
+              -Dboost_system_DIR="${INSTALL_DIR}/lib/cmake/boost_system-1.83.0"        \
+              -Dboost_headers_DIR="${INSTALL_DIR}/lib/cmake/boost_headers-1.83.0"      \ 
+              -DBoost_DIR="${INSTALL_DIR}/lib/cmake/Boost-1.83.0"                      \
+              -DRDK_BUILD_PYTHON_WRAPPERS=OFF                                          \
+              -DRDK_INSTALL_STATIC_LIBS=ON                                             \
+              -DRDK_INSTALL_INTREE=OFF                                                 \
+              -DRDK_BUILD_SLN_SUPPORT=OFF                                              \
+              -DRDK_TEST_MMFF_COMPLIANCE=OFF                                           \
+              -DRDK_BUILD_CPP_TESTS=OFF                                                \
+              -DRDK_USE_BOOST_SERIALIZATION=ON                                         \
+              -DRDK_BUILD_THREADSAFE_SSS=OFF                                           \
+              -DBoost_INCLUDE_DIR="${INSTALL_DIR}/include"                             \
+              -DBoost_USE_STATIC_LIBS=ON                                               \
+              -DBoost_USE_STATIC_RUNTIME=ON                                            \
+              -DBoost_DEBUG=TRUE                                                       \
               -DCMAKE_CXX_FLAGS="-s USE_PTHREADS=1 -pthread -Wno-enum-constexpr-conversion -D_HAS_AUTO_PTR_ETC=0" \
-              -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SOURCE_DIR}/deps/rdkit         \
+              -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" "${SOURCE_DIR}/deps/rdkit"       \
               -DRDK_OPTIMIZE_POPCNT=OFF
 emmake make -j ${NUMPROCS}
 emmake make install
