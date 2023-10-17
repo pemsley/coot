ec941d8ee552ebf0741bcda504cd42444825f0a6
Jakub Smulski <hgonomeg@gmail.com>
2023-10-17 23:41:10 +0200

    Extend build script and lhrs setup

--- lhasa/build_lhasa.sh
+++ lhasa/build_lhasa.sh
@@ -5,3 +5,8 @@ emcmake cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -S . -B lhbuild &&\
 cd lhbuild/;\
 emmake make LDFLAGS=-all-static -j16;\
 cd ..
+emcc -lembind -o liblhasa.js lhbuild/liblhasa.a
+cp liblhasa.{js,wasm} lhrs/
+cd lhrs/
+cargo make build
+cd ..
