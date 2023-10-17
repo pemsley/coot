77f9d8096bf2d419f2db38fb019a7e4adc0ebaca
Jakub Smulski <hgonomeg@gmail.com>
2023-10-17 16:09:20 +0200

    Implement replacement for g_log

--- lhasa/glog_replacement.cpp
+++ lhasa/glog_replacement.cpp
@@ -1,1 +1,47 @@
 #include "glog_replacement.hpp"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string>
+#include <stdexcept>
+
+void lhasa::impl::glog_replacement(lhasa::impl::LogLevel level, const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    std::string m_format;
+    switch(level) {
+        case LogLevel::Debug:{
+            m_format += "[Debug] - ";
+            break;
+        }
+        case LogLevel::Info:{
+            m_format += "[Info] - ";
+            break;
+        }
+        case LogLevel::Message:{
+            m_format += "[Message] - ";
+            break;
+        }
+        case LogLevel::Warning:{
+            m_format += "[Warning] - ";
+            break;
+        }
+        case LogLevel::Critical:{
+            m_format += "[Critical] - ";
+            break;
+        }
+        default:
+        case LogLevel::Error:{
+            m_format += "[Error] - ";
+            break;
+        }
+    }
+    m_format.append(format);
+    m_format.append("\n");
+    vprintf(m_format.c_str(), args);
+    va_end(args);
+
+    if(level == LogLevel::Error){
+        // todo: this might need improvement
+        throw std::runtime_error("g_error() called. Fatal error.");
+    }
+}
--- lhasa/glog_replacement.hpp
+++ lhasa/glog_replacement.hpp
@@ -1,6 +1,24 @@
 #ifndef LHASA_GLOG_REPLACEMENT_HPP
 #define LHASA_GLOG_REPLACEMENT_HPP
 
+namespace lhasa::impl {
+    enum class LogLevel {
+        Debug,
+        Info,
+        Message,
+        Warning,
+        Critical,
+        Error
+        // This is all we need for now.
+    };
+    void glog_replacement(LogLevel level, const char* format, ...) __attribute__((__format__ (__printf__, 2, 3)));
+};
 
+#define g_error(...)    lhasa::impl::glog_replacement(lhasa::impl::LogLevel::Error,    __VA_ARGS__)          
+#define g_message(...)  lhasa::impl::glog_replacement(lhasa::impl::LogLevel::Message,  __VA_ARGS__)
+#define g_critical(...) lhasa::impl::glog_replacement(lhasa::impl::LogLevel::Critical, __VA_ARGS__)
+#define g_warning(...)  lhasa::impl::glog_replacement(lhasa::impl::LogLevel::Warning,  __VA_ARGS__)
+#define g_info(...)     lhasa::impl::glog_replacement(lhasa::impl::LogLevel::Info,     __VA_ARGS__)
+#define g_debug(...)    lhasa::impl::glog_replacement(lhasa::impl::LogLevel::Debug,    __VA_ARGS__)
 
 #endif // LHASA_GLOG_REPLACEMENT_HPP
