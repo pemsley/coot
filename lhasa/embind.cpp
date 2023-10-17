5b59246c051055631ebd1b44d91371de7e0c07b0
Jakub Smulski <hgonomeg@gmail.com>
2023-10-17 23:21:58 +0200

    Add embind.{cpp,hpp}

---
+++ lhasa/embind.cpp
@@ -0,0 +1,8 @@
+#include "embind.hpp"
+
+// EMSCRIPTEN_BINDINGS(my_module) {
+//   function("lerp", &lerp);
+//   class_<LhasaDemo>("LhasaDemo")
+//     .constructor<>()
+//     .function("demo", &LhasaDemo::demo);
+// }
---
+++ lhasa/embind.hpp
@@ -0,0 +1,6 @@
+#ifndef LHASA_EMBIND_HPP
+#define LHASA_EMBIND_HPP
+#include <emscripten/bind.h>
+using namespace emscripten;
+
+#endif // LHASA_EMBIND_HPP
