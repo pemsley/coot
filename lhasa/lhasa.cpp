bbe047500545ac4465fc6c0a1371d3de83d10778
Jakub Smulski <hgonomeg@gmail.com>
2023-10-25 21:08:08 +0200

    Add two utility functions for Lhasa

--- lhasa/lhasa.cpp
+++ lhasa/lhasa.cpp
@@ -1,1 +1,13 @@
 #include "lhasa.hpp"
+#include <rdkit/GraphMol/SmilesParse/SmilesParse.h>
+#include <rdkit/GraphMol/SmilesParse/SmilesWrite.h>
+
+std::unique_ptr<RDKit::RWMol> lhasa::rdkit_mol_from_smiles(std::string smiles) {
+    std::unique_ptr<RDKit::RWMol> ret(RDKit::SmilesToMol(smiles));
+    return ret;
+}
+
+std::string lhasa::rdkit_mol_to_smiles(RDKit::ROMol& mol) {
+    auto ret = RDKit::MolToSmiles(mol);
+    return ret;
+}
--- lhasa/lhasa.hpp
+++ lhasa/lhasa.hpp
@@ -1,4 +1,15 @@
 #ifndef LHASA_HPP
 #define LHASA_HPP
+#include <rdkit/GraphMol/RWMol.h>
+#include <string>
+#include <memory>
+
+namespace lhasa {
+
+std::unique_ptr<RDKit::RWMol> rdkit_mol_from_smiles(std::string smiles);
+std::string rdkit_mol_to_smiles(RDKit::ROMol& mol);
+
+}
+
 
 #endif
