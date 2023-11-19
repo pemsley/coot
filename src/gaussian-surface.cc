fdaa37b0d31174356671b79e44454647ee4feb67
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-11-19 20:34:33 +0000

    Change the Gaussian surface to use NCS chains
    (not ghost chains)

--- src/gaussian-surface.cc
+++ src/gaussian-surface.cc
@@ -8,11 +8,11 @@
 
 int gaussian_surface(int imol) {
 
-   auto make_a_surface = [] (int imol, mmdb::Manager *mol,
-                             unsigned int i_ch, const std::string &chain_id,
-                             const std::vector<coot::ghost_molecule_display_t> &gi,
-                             bool colour_by_ncs_ghost,
-                             const std::map<std::string, int> &chain_id_map) {
+   auto make_an_ncs_ghost_surface = [] (int imol, mmdb::Manager *mol,
+                                        unsigned int i_ch, const std::string &chain_id,
+                                        const std::vector<coot::ghost_molecule_display_t> &gi,
+                                        bool colour_by_ncs_ghost,
+                                        const std::map<std::string, int> &chain_id_map) {
 
       graphics_info_t g;
       coot::colour_holder ch(0.66, 0.44, 0.44);
@@ -62,36 +62,66 @@ int gaussian_surface(int imol) {
    };
 
 
+   auto make_an_ncs_chain_surface = [] (int imol, mmdb::Manager *mol,
+                                        mmdb::Chain  *chain_p,
+                                        const std::vector<std::vector<mmdb::Chain *> > &ncs_chains) {
+
+      coot::colour_holder ch(0.66, 0.44, 0.44);
+      int chain_set_idx = -1;
+      for (unsigned int i=0; i<ncs_chains.size(); i++) {
+         const auto &vc = ncs_chains[i];
+         for (const auto &c : vc) {
+            if (c == chain_p) {
+               chain_set_idx = i;
+               break;
+            }
+         }
+      }
+      ch.rotate_by(0.22 * chain_set_idx);
+
+      glm::vec4 col = colour_holder_to_glm(ch);
+      std::string chain_id = chain_p->GetChainID();
+      coot::gaussian_surface_t gauss_surf(mol, chain_id);
+      coot::simple_mesh_t smesh = gauss_surf.get_surface();
+      std::vector<s_generic_vertex> vertices(smesh.vertices.size());
+      for (unsigned int i = 0; i < smesh.vertices.size(); i++) {
+         vertices[i] = s_generic_vertex(smesh.vertices[i].pos,
+                                        smesh.vertices[i].normal,
+                                        smesh.vertices[i].color);
+         vertices[i].color = col;
+      }
+      graphics_info_t g;
+      g.attach_buffers();
+      std::string object_name("Gaussian Surface #");
+      object_name += std::to_string(imol);
+      object_name += std::string(" Chain ");
+      object_name += chain_id;
+      int obj_mesh = new_generic_object_number(object_name);
+      meshed_generic_display_object &obj = g.generic_display_objects[obj_mesh];
+      obj.imol = imol;
+      obj.mesh.name = object_name;
+      obj.mesh.set_draw_mesh_state(true);
+      obj.mesh.import(vertices, smesh.triangles);
+      obj.mesh.set_material_specularity(0.7, 128);
+      obj.mesh.setup_buffers();
+   };
+
    int status = 0;
    graphics_info_t g;
    if (g.is_valid_model_molecule(imol)) {
-
-      std::vector<coot::ghost_molecule_display_t> gi = g.molecules[imol].NCS_ghosts();
-
+      int imodel = 1;
       mmdb::Manager *mol = g.molecules[imol].atom_sel.mol;
-      std::vector<std::string> chain_ids = g.molecules[imol].get_chain_ids();
-      std::map<std::string, int> chain_id_map;
-      for (unsigned int i_ch=0; i_ch<chain_ids.size(); i_ch++) {
-         auto chain_id = chain_ids[i_ch];
-         chain_id_map[chain_id] = i_ch;
-      }
-
-      if (false) {
-         std::cout << ":::::::::::::::::::::::: ghosts " << gi.size() << std::endl;
-         for (unsigned int ighost=0; ighost<gi.size(); ighost++) {
-            const auto &ghost = gi[ighost];
-            std::cout << "   " << ighost << " " << ghost.name << " " << ghost.chain_id << " " << ghost.target_chain_id
-                      << "\n" << ghost.rtop.format() << std::endl;
+      mmdb::Model *model_p = mol->GetModel(imodel);
+      if (model_p) {
+         std::vector<std::vector<mmdb::Chain *> > ncs_chains = coot::ncs_related_chains(mol, imodel);
+         int n_chains = model_p->GetNumberOfChains();
+         for (int ichain=0; ichain<n_chains; ichain++) {
+            mmdb::Chain *chain_p = model_p->GetChain(ichain);
+            std::cout << "surfce for chain " << chain_p->GetChainID() << std::endl;
+            make_an_ncs_chain_surface(imol, mol, chain_p, ncs_chains);
          }
       }
-
-      for (unsigned int i_ch=0; i_ch<chain_ids.size(); i_ch++) {
-         auto chain_id = chain_ids[i_ch];
-         // gi is used if colour_by_ncs_ghost is true
-         bool colour_by_ncs_ghost = false;
-         make_a_surface(imol, mol, i_ch, chain_id, gi, colour_by_ncs_ghost, chain_id_map);
-         status = 1;
-      }
+      g.graphics_draw();
    }
    return status;
 }
