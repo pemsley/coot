e8d3a7be7bcec06bdd927861682e8c4698330723
Jakub Smulski <hgonomeg@gmail.com>
2023-12-01 19:11:10 +0100

    Remove some of the templated code and start porting Lhasa

---
+++ lhasa/lhreact/src/Lhasa.tsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+// import './_App.css'
+import './index.css';
+import * as d3 from "d3";
+
+
+export function LhasaComponent() {
+  // const [count, setCount] = useState(0)
+  return (
+    <>
+      <div className="lhasa_editor">
+        Hello world
+      </div>
+    </>
+  )
+}
+
+export function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <LhasaComponent />
+    </>
+  )
+}
+
+// export default App
