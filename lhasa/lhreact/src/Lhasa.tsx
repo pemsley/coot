ba62a75cf1cffaba151f6a7c064d7adfd3cea08b
Jakub Smulski <hgonomeg@gmail.com>
2023-12-05 15:16:48 +0100

    Make some tiny progress porting Lhasa to React

--- lhasa/lhreact/src/Lhasa.tsx
+++ lhasa/lhreact/src/Lhasa.tsx
@@ -8,7 +8,7 @@ import { Canvas, Color } from './lhasa';
 class ToolButtonProps {
   onclick: MouseEventHandler<HTMLDivElement> | undefined;
   caption: string | undefined;
-  icon: string | undefined
+  icon: string | undefined | null
 }
 
 function ToolButton(props:ToolButtonProps) {
@@ -25,9 +25,290 @@ function ToolButton(props:ToolButtonProps) {
   )
 }
 
+function on_render(lh: Canvas) {
+  console.debug("on_render() called.");
+
+  const css_color_from_lhasa_color = (lhasa_color: Color) => {
+    return 'rgba(' + lhasa_color.r * 255 + ','+ lhasa_color.g * 255 + ',' + lhasa_color.b * 255 + ',' + lhasa_color.a + ')';
+  }
+  const lhasa_text_to_d3js = (msvg, text) => {
+    const style_to_attrstring = (style) => {
+      let style_string = "";
+      if(style.specifies_color) {
+        style_string += "fill:";
+        style_string += css_color_from_lhasa_color(style.color);
+        style_string += ";";
+      }
+      // font-size:75%;baseline-shift:sub / super
+      switch(style.positioning) {
+        case Lhasa.TextPositioning.Normal:
+          if(style.size != "") {
+            style_string += "font-size:";
+            style_string += style.size;
+            style_string += ";";
+          }
+          break;
+        case Lhasa.TextPositioning.Sub:
+          style_string += "font-size:75%;baseline-shift:sub;";
+          break;
+        case Lhasa.TextPositioning.Super:
+          style_string += "font-size:75%;baseline-shift:super;";
+          break;
+      }
+      if(style.weight != "") {
+        style_string += "font-weight:";
+        style_string += style.weight;
+        style_string += ";";
+      }
+      return style_string;
+    };
+    const append_spans_to_node = (spans, text_node) => {
+      for(let i = 0; i < spans.size(); i++) {
+        const span = spans.get(i);
+        let child = text_node
+          // .enter()
+          .append("tspan");
+        if(span.specifies_style) {
+          child.attr("style", style_to_attrstring(span.style));
+        }
+        if(span.has_subspans()) {
+          append_spans_to_node(span.as_subspans(), child);
+        } else {
+          const caption = span.as_caption();
+          child.text(caption);
+        }
+      }
+      // return ret;
+    }
+    const ret = msvg.append("text")
+      .attr("x", text.origin.x)
+      .attr("y", text.origin.y);
+    
+    const style_string = style_to_attrstring(text.style);
+    if(style_string != "") {
+      ret.attr("style", style_string);
+    } else {
+      // console.warn("Empty style string!");
+    }
+    if(text.spans.size() == 0) {
+      console.warn("Text contains no spans!");
+    }
+    append_spans_to_node(text.spans, ret);
+    return ret;
+  };
+  const text_measure_function = (text) => {
+    // let size_info = new Lhasa.TextSize;
+    let size_info = {
+      'width': 0,
+      'height': 0
+    };
+    try{
+      // console.warn('todo: Fix in React');
+      return size_info;
+      const parent = document.getElementById("editor_canvas_container");
+      const msvg = d3.create("svg")
+        .attr("width", 100)
+        .attr("height", 100)
+        .attr("id", "measurement_temporary");
+      const text_elem = lhasa_text_to_d3js(msvg, text);
+      parent.append(msvg.node());
+      const node = text_elem.node();
+      // This has awful performance but I don't really have a choice
+      const bbox = node.getBBox();
+      size_info.width = Math.ceil(bbox.width);
+      size_info.height = Math.ceil(bbox.height);
+      msvg.remove();
+    } catch(err) {
+      console.error('Error occured in text measurement: ', err);
+    } finally {
+      return size_info;
+    }
+    
+  };
+  const ren = new Lhasa.Renderer(text_measure_function);
+  lh.render(ren);
+  const commands = ren.get_commands();
+  const svg = d3.create("svg")
+    .attr("id", "lhasa_drawing")
+    .attr("width", lh.measure(Lhasa.MeasurementDirection.HORIZONTAL).requested_size)
+    .attr("height", lh.measure(Lhasa.MeasurementDirection.VERTICAL).requested_size);
+
+  const render_commands = (cmds) => {
+    for(var i = 0; i < cmds.size(); i++) {
+      const command = cmds.get(i);
+      if(command.is_line()) {
+        const line = command.as_line();
+        const color = line.style.color;
+        svg.append("line")
+          .attr("x1", line.start.x)
+          .attr("y1", line.start.y)
+          .attr("x2", line.end.x)
+          .attr("y2", line.end.y)
+          .attr("stroke", css_color_from_lhasa_color(color))
+          .attr("stroke-width", line.style.line_width);
+
+      } else if(command.is_arc()) {
+        const arc = command.as_arc();
+        // Thanks to: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+        function polarToCartesian(centerX, centerY, radius, angleInRadians) {
+          //var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+          return {
+            x: centerX + (radius * Math.cos(angleInRadians)),
+            y: centerY + (radius * Math.sin(angleInRadians))
+          };
+        }
+
+        function describeArc(x, y, radius, startAngle, endAngle){
+
+            var start = polarToCartesian(x, y, radius, endAngle);
+            var end = polarToCartesian(x, y, radius, startAngle);
+
+            var largeArcFlag = endAngle - startAngle <= Math.PI ? "0" : "1";
+            //var largeArcFlag = "1";
+
+            var d = [
+                "M", start.x, start.y, 
+                "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+            ].join(" ");
+            // console.log("d", d);
+
+            return d;       
+        }
+        // svg.append("path")
+        //   // todo
+        //   .attr("stroke-width", 1)
+        //   // todo
+        //   .attr("fill", "none")
+        //   // todo
+        //   .attr("stroke", "#000000")
+        //   .attr("d", describeArc(arc.origin.x, arc.origin.y, arc.radius, arc.angle_one - 0.001, arc.angle_two));
+        console.log("todo: render arcs properly");
+
+      } else if(command.is_path()) {
+        const path = command.as_path();
+        render_commands(path.commands);
+        console.log("todo: implement fills for paths.");
+
+      } else if(command.is_text()) {
+        const text = command.as_text();
+        lhasa_text_to_d3js(svg, text);
+      }
+    }
+  };
+  render_commands(commands);
+  commands.delete();
+  ren.delete();
+  
+  const node = svg.node();
+  //console.log(node);
+  return node;
+  // const elem = document.getElementById("editor_canvas_container");
+  // while (elem.firstChild) {
+  //   elem.removeChild(elem.lastChild);
+  // }
+  // elem.append(node);
+};
+
 export function LhasaComponent() {
+  const [st, setSt] = useState(() => {
+    return {
+      svg_node: null,
+      smiles: ''
+    };
+  });
   const [lh, setLh] = useState(() => {
-    return new Lhasa.Canvas();
+    const lh = new Lhasa.Canvas();
+    lh.connect("queue_redraw", () => {
+      const node = on_render(lh);
+      st.svg_node = node;
+      // console.log(st.svg_node);
+      setSt(st);
+    });
+
+    const on_status_updated = function (status_text) {
+      // For now
+      console.log("Status: " + status_text);
+      // const elem = document.getElementById("status_display");
+      // while (elem.firstChild) {
+      //   elem.removeChild(elem.lastChild);
+      // }
+      // elem.textContent = status_text;
+    };
+    lh.connect("status_updated", on_status_updated);
+    lh.connect("smiles_changed", function () {
+      const smiles_raw = lh.get_smiles();
+
+      const smiles_array = smiles_raw.split("\n");
+      //console.log("array: ", smiles_array);
+      //const smiles_array = smiles_raw.split("/\r?\n/");
+      // for(let i = 0; i < smiles_array.length; i++) {
+      //   const m_div = document.createElement("div");
+      //   m_div.innerHTML = smiles_array[i];
+      //   //m_div.textContent = smiles_array[i];
+      //   elem.append(m_div);
+      // }
+    });
+    lh.connect("molecule_deleted", function (mol_id) {
+      console.log("Molecule with id " + mol_id + " has been deleted.");
+    });
+    // lh.connect("scale_changed", function (new_scale) {
+    //   const elem = document.getElementById("scale_display");
+    //   while (elem.firstChild) {
+    //     elem.removeChild(elem.lastChild);
+    //   }
+    //   elem.textContent = '' + new_scale;
+    // });
+    // const smiles_import_button = document.getElementById("smiles_import_button");
+    // smiles_import_button.addEventListener("click", function (el) {
+    //   const smiles_input = document.getElementById("smiles_input");
+    //   Module.append_from_smiles(lh, smiles_input.value);
+    // });
+
+    // on_x_element_button = (button) => {
+    //   const panel = document.getElementById("x_element_panel");
+    //   console.log(panel.style.display);
+    //   if(panel.style.display == 'none' || panel.style.display == '') {
+    //     panel.style.display = 'flex';
+    //   } else {
+    //     panel.style.display = 'none';
+    //   }
+    // };
+
+    // on_x_element_submit_button = (button) => {
+    //   const symbol_input = document.getElementById("x_element_symbol_input");
+    //   const x_button = document.getElementById("x_element_button");
+    //   const err_display = document.getElementById("error_display");
+    //   try {
+    //     const el_ins = Module.element_insertion_from_symbol(symbol_input.value);
+    //     switch_tool(x_button, el_ins);
+    //     const panel = document.getElementById("x_element_panel");
+    //     panel.style.display = 'none';
+    //     err_display.style.display = 'none';
+    //   }catch(err) {
+    //     console.warn("Could not set custom element: ", err);
+    //     err_display.textContent = "Could not load ElementInsertion tool. Is your symbol valid?";
+    //     err_display.style.display = 'flex';
+    //   }
+    // };
+
+    // switch_display_mode = (value) => {
+    //   switch(value) {
+    //     default:
+    //     case 'standard':
+    //       lh.set_display_mode(Module.LhasaDisplayMode.Standard);
+    //       break;
+    //     case 'atom_indices':
+    //       lh.set_display_mode(Module.LhasaDisplayMode.AtomIndices);
+    //       break;
+    //   }
+    // };
+
+    console.log('Adding demo molecule.');
+    Lhasa.append_from_smiles(lh, "O=C(C)Oc1ccccc1C(=O)O");
+    const default_scale = 1.0;
+    lh.set_scale(default_scale);
+    return lh;
   });
   const chLh = (func: () => void) => {
     func();
@@ -36,188 +317,10 @@ export function LhasaComponent() {
 
   function switch_tool(tool : any) {
     // todo: pressed buttons
-    console.log('tool switch!');
     chLh(() => lh.set_active_tool(Lhasa.make_active_tool(tool)));
   };
   
-  function on_render() {
-    console.debug("on_render() called.");
-    const css_color_from_lhasa_color = (lhasa_color: Color) => {
-      return 'rgba(' + lhasa_color.r * 255 + ','+ lhasa_color.g * 255 + ',' + lhasa_color.b * 255 + ',' + lhasa_color.a + ')';
-    }
-    const lhasa_text_to_d3js = (msvg, text) => {
-      const style_to_attrstring = (style) => {
-        let style_string = "";
-        if(style.specifies_color) {
-          style_string += "fill:";
-          style_string += css_color_from_lhasa_color(style.color);
-          style_string += ";";
-        }
-        // font-size:75%;baseline-shift:sub / super
-        switch(style.positioning) {
-          case Lhasa.TextPositioning.Normal:
-            if(style.size != "") {
-              style_string += "font-size:";
-              style_string += style.size;
-              style_string += ";";
-            }
-            break;
-          case Lhasa.TextPositioning.Sub:
-            style_string += "font-size:75%;baseline-shift:sub;";
-            break;
-          case Lhasa.TextPositioning.Super:
-            style_string += "font-size:75%;baseline-shift:super;";
-            break;
-        }
-        if(style.weight != "") {
-          style_string += "font-weight:";
-          style_string += style.weight;
-          style_string += ";";
-        }
-        return style_string;
-      };
-      const append_spans_to_node = (spans, text_node) => {
-        for(let i = 0; i < spans.size(); i++) {
-          const span = spans.get(i);
-          let child = text_node
-            // .enter()
-            .append("tspan");
-          if(span.specifies_style) {
-            child.attr("style", style_to_attrstring(span.style));
-          }
-          if(span.has_subspans()) {
-            append_spans_to_node(span.as_subspans(), child);
-          } else {
-            const caption = span.as_caption();
-            child.text(caption);
-          }
-        }
-        // return ret;
-      }
-      const ret = msvg.append("text")
-        .attr("x", text.origin.x)
-        .attr("y", text.origin.y);
-      
-      const style_string = style_to_attrstring(text.style);
-      if(style_string != "") {
-        ret.attr("style", style_string);
-      } else {
-        // console.warn("Empty style string!");
-      }
-      if(text.spans.size() == 0) {
-        console.warn("Text contains no spans!");
-      }
-      append_spans_to_node(text.spans, ret);
-      return ret;
-    };
-    const text_measure_function = (text) => {
-      // let size_info = new Lhasa.TextSize;
-      let size_info = {
-        'width': 0,
-        'height': 0
-      };
-      try{
-        const parent = document.getElementById("editor_canvas_container");
-        const msvg = d3.create("svg")
-          .attr("width", 100)
-          .attr("height", 100)
-          .attr("id", "measurement_temporary");
-        const text_elem = lhasa_text_to_d3js(msvg, text);
-        parent.append(msvg.node());
-        const node = text_elem.node();
-        // This has awful performance but I don't really have a choice
-        const bbox = node.getBBox();
-        size_info.width = Math.ceil(bbox.width);
-        size_info.height = Math.ceil(bbox.height);
-        msvg.remove();
-      } catch(err) {
-        console.error('Error occured in text measurement: ', err);
-      } finally {
-        return size_info;
-      }
-      
-    };
-    const ren = new Lhasa.Renderer(text_measure_function);
-    lh.render(ren);
-    const commands = ren.get_commands();
-    const svg = d3.create("svg")
-      .attr("id", "lhasa_drawing")
-      .attr("width", lh.measure(Lhasa.MeasurementDirection.HORIZONTAL).requested_size)
-      .attr("height", lh.measure(Lhasa.MeasurementDirection.VERTICAL).requested_size);
 
-    const render_commands = (cmds) => {
-      for(var i = 0; i < cmds.size(); i++) {
-        const command = cmds.get(i);
-        if(command.is_line()) {
-          const line = command.as_line();
-          const color = line.style.color;
-          svg.append("line")
-            .attr("x1", line.start.x)
-            .attr("y1", line.start.y)
-            .attr("x2", line.end.x)
-            .attr("y2", line.end.y)
-            .attr("stroke", css_color_from_lhasa_color(color))
-            .attr("stroke-width", line.style.line_width);
-
-        } else if(command.is_arc()) {
-          const arc = command.as_arc();
-          // Thanks to: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
-          function polarToCartesian(centerX, centerY, radius, angleInRadians) {
-            //var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-
-            return {
-              x: centerX + (radius * Math.cos(angleInRadians)),
-              y: centerY + (radius * Math.sin(angleInRadians))
-            };
-          }
-
-          function describeArc(x, y, radius, startAngle, endAngle){
-
-              var start = polarToCartesian(x, y, radius, endAngle);
-              var end = polarToCartesian(x, y, radius, startAngle);
-
-              var largeArcFlag = endAngle - startAngle <= Math.PI ? "0" : "1";
-              //var largeArcFlag = "1";
-
-              var d = [
-                  "M", start.x, start.y, 
-                  "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-              ].join(" ");
-              // console.log("d", d);
-
-              return d;       
-          }
-          // svg.append("path")
-          //   // todo
-          //   .attr("stroke-width", 1)
-          //   // todo
-          //   .attr("fill", "none")
-          //   // todo
-          //   .attr("stroke", "#000000")
-          //   .attr("d", describeArc(arc.origin.x, arc.origin.y, arc.radius, arc.angle_one - 0.001, arc.angle_two));
-          console.log("todo: render arcs properly");
-
-        } else if(command.is_path()) {
-          const path = command.as_path();
-          render_commands(path.commands);
-          console.log("todo: implement fills for paths.");
-
-        } else if(command.is_text()) {
-          const text = command.as_text();
-          lhasa_text_to_d3js(svg, text);
-        }
-      }
-    };
-    render_commands(commands);
-    commands.delete();
-    ren.delete();
-
-    const elem = document.getElementById("editor_canvas_container");
-    while (elem.firstChild) {
-      elem.removeChild(elem.lastChild);
-    }
-    elem.append(svg.node());
-  };
   return (
     <>
       <div id="lhasa_editor">
@@ -281,8 +384,40 @@ export function LhasaComponent() {
             <ToolButton caption="I" onclick={() => switch_tool(new Lhasa.ElementInsertion(Lhasa.Element.I))} />
             <ToolButton caption="X" id="x_element_button" />
           </div>
-          <div id="editor_canvas_container">
-            <div id="pre_render_message">Lhasa not loaded.</div>
+          <div 
+            id="editor_canvas_container"
+            // todo: fix
+            onContextMenu={() => {return false}}
+            onMouseMove={(event) => {
+              // console.log('Mousemove');
+              lh.on_hover(event.nativeEvent.offsetX, event.nativeEvent.offsetY, event.altKey);
+            }}
+            onMouseDown={(event) => {
+              if(event.button == 0) {
+                //console.log('lclick');
+                lh.on_left_click(event.nativeEvent.offsetX, event.nativeEvent.offsetY, event.altKey, event.ctrlKey, event.shiftKey);
+              } else if(event.button == 2) {
+                //console.log('rclick');
+                lh.on_right_click(event.nativeEvent.offsetX, event.nativeEvent.offsetY, event.altKey, event.ctrlKey, event.shiftKey);
+              }
+            }}
+            onMouseUp={(event) => {
+              if(event.button == 0) {
+                //console.log('lreleased');
+                lh.on_left_click_released(event.nativeEvent.offsetX, event.nativeEvent.offsetY, event.altKey, event.ctrlKey, event.shiftKey);
+              } else if(event.button == 2) {
+                //console.log('rreleased');
+                lh.on_right_click_released(event.nativeEvent.offsetX, event.nativeEvent.offsetY, event.altKey, event.ctrlKey, event.shiftKey);
+              }
+            }}
+            onWheel={(event) => {
+              lh.on_scroll(event.deltaX, event.deltaY, event.ctrlKey);
+            }}
+
+            dangerouslySetInnerHTML={st.svg_node != null ? {__html:st.svg_node.outerHTML} : undefined}
+
+          >
+            {/* {st.svg_node == null && <div id="pre_render_message">Lhasa not rendered.</div>} */}
           </div>
         </div>
         <div id="status_display_panel" className="panel">
@@ -317,7 +452,7 @@ export function LhasaComponent() {
           <div id="smiles_display_outer" className="panel">
             <b>SMILES</b>
             <div id="smiles_display">
-              {/* <!-- Smiles goes here --> */}
+              {st.smiles}
             </div>
           </div>
         </div>
