f2b421ce077012afceaa41680828bbc7b06372af
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-12-06 02:55:21 +0000

    Add a counter for the number of sound files open
    because I was trying to debug why I got the ALSA error message.

--- src/sound.cc
+++ src/sound.cc
@@ -12,6 +12,10 @@
 #include "utils/coot-utils.hh"
 #include "graphics-info.h" // to check for graphics_info_t::use_sounds
 
+#include <atomic>
+
+std::atomic<unsigned int> n_sound_files_playing(0);
+
 void
 play_sound_file(const std::string &file_name) {
 
@@ -30,68 +34,77 @@ play_sound_file(const std::string &file_name) {
          if (open_status < 0) {
             std::cout << "Failed to VorbisOpen " << file_name << std::endl;
          } else {
-            if(!ov_seekable(&ovf)) {
-               std::cout << "Failed to Vorbis Seek " << file_name << std::endl;
-            } else {
+            if (true) {
+               n_sound_files_playing++;
+               std::string ss = "n_sound_files_playing: " + std::to_string(n_sound_files_playing) + "\n";
+               std::cout << ss << std::endl;
+               if(!ov_seekable(&ovf)) {
+                  std::cout << "Failed to Vorbis Seek " << file_name << std::endl;
+               } else {
 
-               snd_pcm_sframes_t frames;
-               snd_pcm_t *handle;
-               const char *device = "default";
+                  snd_pcm_sframes_t frames;
+                  snd_pcm_t *handle;
+                  const char *device = "default";
 
-               int err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0);
-               if (err < 0) {
-                  printf("Playback open error: %s\n", snd_strerror(err));
-                  return;
-               }
-               err = snd_pcm_set_params(handle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 2, 44100, 1, 500000);  /* 0.5sec */
-               if (err < 0) {
-                  printf("Playback open error: %s\n", snd_strerror(err));
-                  return;
-               }
+                  int err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0);
+                  if (err < 0) {
+                     printf("Playback open error: %s\n", snd_strerror(err));
+                     return;
+                  }
+                  err = snd_pcm_set_params(handle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 2, 44100, 1, 500000);  /* 0.5sec */
+                  if (err < 0) {
+                     printf("Playback open error: %s\n", snd_strerror(err));
+                     return;
+                  }
 
-               long tt = ov_time_total(&ovf, -1);
-               std::cout << "OggVorbis total time for " << file_name << " " << tt << std::endl;
+                  long tt = ov_time_total(&ovf, -1);
+                  std::cout << "OggVorbis total time for " << file_name << " " << tt << std::endl;
 
-               for(int i=0; i<ov_streams(&ovf); i++){
-                  vorbis_info *vi=ov_info(&ovf,i);
-                  printf("    logical bitstream section %d information:\n", i+1);
-                  printf("        %ldHz %d channels bitrate %ldkbps serial number=%ld\n",
-                         vi->rate,vi->channels,ov_bitrate(&ovf,i)/1000,
-                         ov_serialnumber(&ovf,i));
-                  printf("        compressed length: %ld bytes ",(long)(ov_raw_total(&ovf,i)));
-                  printf(" play time: %lds\n",(long)ov_time_total(&ovf,i));
+                  for(int i=0; i<ov_streams(&ovf); i++){
+                     vorbis_info *vi=ov_info(&ovf,i);
+                     printf("    logical bitstream section %d information:\n", i+1);
+                     printf("        %ldHz %d channels bitrate %ldkbps serial number=%ld\n",
+                            vi->rate,vi->channels,ov_bitrate(&ovf,i)/1000,
+                            ov_serialnumber(&ovf,i));
+                     printf("        compressed length: %ld bytes ",(long)(ov_raw_total(&ovf,i)));
+                     printf(" play time: %lds\n",(long)ov_time_total(&ovf,i));
 
-                  int eof = 0;
-                  char pcmout[4096];
-                  int current_section;
-                  while (!eof) {
-                     // std::cout << "################ really playing sound" << std::endl;
-                     long ret= ov_read(&ovf, pcmout, sizeof(pcmout), 0, 2, 1, &current_section);
-                     if (ret == 0) {
-                        /* EOF */
-                        eof=1;
-                     } else if (ret < 0) {
-                        std::cout << "test_sound(): error in the stream" << std::endl;
-                     } else {
+                     int eof = 0;
+                     char pcmout[4096];
+                     int current_section;
+                     while (!eof) {
+                        // std::cout << "################ really playing sound" << std::endl;
+                        long ret= ov_read(&ovf, pcmout, sizeof(pcmout), 0, 2, 1, &current_section);
+                        if (ret == 0) {
+                           /* EOF */
+                           eof=1;
+                        } else if (ret < 0) {
+                           std::cout << "test_sound(): error in the stream" << std::endl;
+                        } else {
 
-                        frames = snd_pcm_writei(handle, pcmout, ret/4);
-                        snd_pcm_wait(handle, 20000); // max wait time in ms.
-                        if (frames < 0)
-                           frames = snd_pcm_recover(handle, frames, 0);
-                        if (frames < 0) {
-                           printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
-                           break;
+                           frames = snd_pcm_writei(handle, pcmout, ret/4);
+                           snd_pcm_wait(handle, 20000); // max wait time in ms.
+                           if (frames < 0)
+                              frames = snd_pcm_recover(handle, frames, 0);
+                           if (frames < 0) {
+                              printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
+                              break;
+                           }
+                           // test for dropped frames here, comparing ret and frames.
+
+                           // fwrite(pcmout,1,ret,stdout);
                         }
-                        // test for dropped frames here, comparing ret and frames.
-
-                        // fwrite(pcmout,1,ret,stdout);
                      }
                   }
                }
             }
+            std::string ss = "reducing n_sound_files_playing\n";
+            std::cout << ss << std::endl;
+            n_sound_files_playing--;
          }
          fclose(f);
          // close oggvorbisfile?
+         // ov_clear(&ovf);
       }
    };
 
@@ -119,6 +132,8 @@ void play_sound(const std::string &type) {
       if (type == "TINK")    play_sound_file("538549_3725923-lq-Sjonas-Select-2.ogg");
       if (type == "STARS")   play_sound_file("538553_3725923-lq-Sjonas88-Stars.ogg");
       if (type == "OOPS")    play_sound_file("538550_3725923-lq-Sjonas88-Deep-tone.ogg");
+      if (type == "diego-gone-pop") play_sound_file("pop-dodrio-554022_1433422-lq.ogg");
+      if (type == "diego-arrives") play_sound_file("cdonahueucsd-337132_5955158-lq.ogg");
    }
 
 }
