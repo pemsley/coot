7a6ede8260bdd7e3b90128bcc762ad1e8a88ba34
Jakub Smulski <hgonomeg@gmail.com>
2023-12-17 13:59:19 +0100

    Move lhreact sources into LhasaReact repository, update RDKit version, remove lhjs

--- lhasa/build_lhjs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-./build_lhasa.sh
-cd lhjs/
-cp ../lhbuild/lhasa.{worker.js,js,wasm} ./
-# nothing for now
-cd ..
--- lhasa/build_lhreact.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-./build_lhasa.sh
-cd lhreact/
-cp ../lhbuild/lhasa.{worker.js,js,wasm} ./public/
-cp ../lhbuild/lhasa.d.ts ./src/
-# nothing for now
-# todo: distribute
-cd ..
