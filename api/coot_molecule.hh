13e365cd5498c98ce3b9f88da918e250b9b7885a
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2023-12-17 19:35:18 +0000

    Use ax_boost_thread to set HAVE_BOOST_BASED_THREAD_POOL_LIBRARY

--- api/coot_molecule.hh
+++ api/coot_molecule.hh
@@ -48,7 +48,11 @@
 // 2023-07-04-PE This is a hack. This should be configured - and the
 // various functions that depend on this being true should be
 // reworked so that they run without a thread pool.
-// #define HAVE_BOOST_BASED_THREAD_POOL_LIBRARY
+
+#ifdef HAVE_BOOST_THREAD // part of DEFS in Makefile
+#define HAVE_BOOST_BASED_THREAD_POOL_LIBRARY
+#endif
+
 
 
 namespace coot {
