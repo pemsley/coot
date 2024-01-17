b2a9a0ed00b8ea8470c772484f702f13812db0ca
LucreziaCatapano <lucrezia@mrc-lmb.cam.ac.uk>
2024-01-17 17:33:03 +0000

    Add usage of details for the residue spec in test_chapi2.py

--- python-tests/test_chapi2.py
+++ python-tests/test_chapi2.py
@@ -3,4 +3,4 @@ mc = chapi.molecules_container_t(True)
 imol = mc.read_pdb('data/tutorial-modern.pdb')
 residue_list = mc.get_residues_near_residue(imol, '//A/20', 6)
 for r in residue_list:
-    print(r)
+    print(r.chain_id, r.res_no)
