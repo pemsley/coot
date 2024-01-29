f26082ab262fe40b603740539f95e12085be1ed5
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-01-29 00:07:52 +0000

    Add get_residue_name() to test_chapi2.py

--- python-tests/test_chapi2.py
+++ python-tests/test_chapi2.py
@@ -3,4 +3,5 @@ mc = chapi.molecules_container_t(True)
 imol = mc.read_pdb('data/tutorial-modern.pdb')
 residue_list = mc.get_residues_near_residue(imol, '//A/20', 6)
 for r in residue_list:
-    print(r.chain_id, r.res_no)
+    rn = mc.get_residue_name(imol, r.chain_id, r.res_no, r.ins_code)
+    print(r.chain_id, r.res_no, rn)
