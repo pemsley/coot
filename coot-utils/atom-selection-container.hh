d6a6297adfd4d961b4cc6dd5efc8d626b80fc112
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-03-15 01:37:47 +0000

    Add some protection for atom_selection_container functions

--- coot-utils/atom-selection-container.hh
+++ coot-utils/atom-selection-container.hh
@@ -118,10 +118,12 @@ public:
    void clear_up() {
       if (read_success)
          if (SelectionHandle)
-            mol->DeleteSelection(SelectionHandle);
+            if (mol)
+               mol->DeleteSelection(SelectionHandle);
       delete mol;
       atom_selection = 0;
       mol = 0;
+      read_success = 0;
    }
 
    //! Delete atom selection - do this before modifying the internal mol
@@ -129,6 +131,7 @@ public:
       mol->DeleteSelection(SelectionHandle);
       n_selected_atoms = 0;
       atom_selection = 0;
+      // SelectionHandle = -1;  crash on first test in test-moleculess-container
    }
 
    // sets UDDOldAtomIndexHandle
