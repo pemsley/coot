bb2a3d944ddcd4529dd34021ccb426454a4bb280
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-03-30 10:23:06 +0000

    Revert "Change the copyright notice on MoleculesToTriangles sources"
    This reverts commit 85f708887a45fcbf0d455eaa3f3ed1edb2504952.

--- MoleculesToTriangles/CXXClasses/MolecularRepresentation.h
+++ MoleculesToTriangles/CXXClasses/MolecularRepresentation.h
@@ -1,23 +1,10 @@
 /*
- * MoleculesToTriangles/CXXClasses/MolecularRepresentation.h
+ *  MolecularRepresentation.h
+ *  Aesop
  *
- * Copyright 2009 by Martin Noble, University of Oxford
- * Author: Martin Noble
+ *  Created by Martin Noble on 17/02/2009.
+ *  Copyright 2009 Dept. of Biochemistry, Oxford University. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA
  */
 #ifndef MolecularRepresentation_h
 #define MolecularRepresentation_h
--- MoleculesToTriangles/CXXSurface/CXXCircle.cpp
+++ MoleculesToTriangles/CXXSurface/CXXCircle.cpp
@@ -1,23 +1,10 @@
 /*
- * MoleculesToTriangles/CXXSurface/CXXCircle.cpp
+ *  CXXCircle.cpp
+ *  CXXSurface
  *
- * Copyright 2009 by Martin Noble, University of Oxford
- * Author: Martin Noble
+ *  Created by martin on Sat Feb 28 2004.
+ *  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA
  */
 #define _USE_MATH_DEFINES
 #include <math.h>
