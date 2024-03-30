49cd20a814681e70b3f09c05c590b1a55bc78e87
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-03-30 04:31:52 +0000

    Replace the license notification on cootaneer, cootilus and protein_db

--- cootaneer/buccaneer-lib.cpp
+++ cootaneer/buccaneer-lib.cpp
@@ -1,5 +1,26 @@
-/*! \file buccaneer-lib.cpp buccaneer library */
-/* (C) 2002-2006 Kevin Cowtan & University of York all rights reserved */
+/*
+ * cootaneer/buccaneer-lib.cpp
+ *
+ * Copyright 2002-2006 by Kevin Cowtan
+ * Author: Kevin Cowtan
+ *
+ * This file is part of Coot
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ */
 
 #include "buccaneer-lib.h"
 
