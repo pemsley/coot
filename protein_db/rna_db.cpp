aa9aaff01c6c0d86981ac13eb652a0a1d20c718e
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-03-30 04:11:32 +0000

    Revert "Change the copyright notice/license on code written by Kevin Cowtan"
    This reverts commit 3ad4b0dee05f34dde3ccbd743652719ce09fdd3b.

--- protein_db/rna_db.cpp
+++ protein_db/rna_db.cpp
@@ -1,24 +1,6 @@
-/*
- * protein_db/rna_db.cpp
- *
- * Copyright 2008-2009 by Kevin Cowtan & University York
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA
- */
 
+/*! RNADB Top 500 main chain database */
+/* (C) 2008-2009 Kevin Cowtan & University of York all rights reserved */
 
 
 #include "rna_db.h"
