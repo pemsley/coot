4eef26a80b1ccf2a89a708402f465af107f8b3b9
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-04-24 17:44:19 +0100

    Rework the tests for ligands with the same name
    the fragment cif is not yet in the test dataset.

--- api/lucrezia-tests.hh
+++ api/lucrezia-tests.hh
@@ -1,5 +1,12 @@
 
+#ifndef COOT_API_LUCREZIA_TESTS_HH
+#define COOT_API_LUCREZIA_TESTS_HH
+
 #include "molecules-container.hh"
 #include "test-molecules-container.hh"
 
 int test_multiligands_lig_bonding(molecules_container_t &mc);
+int test_import_ligands_with_same_name(molecules_container_t &mc);
+
+#endif // COOT_API_LUCREZIA_TESTS_HH
+
