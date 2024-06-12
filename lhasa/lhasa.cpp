d85152c7d61736c008dc07647a2925fddcb1b88f
Jakub Smulski <hgonomeg@gmail.com>
2024-06-12 18:14:49 +0200

    Begin fixing Lhasa standalone build

--- lhasa/lhasa.cpp
+++ lhasa/lhasa.cpp
@@ -23,20 +23,8 @@
 #include <rdkit/GraphMol/SmilesParse/SmilesWrite.h>
 #include <rdkit/GraphMol/MolPickler.h>
 #include "glog_replacement.hpp"
+#include "../coot-utils/base64-encode-decode.hh"
 
-#ifdef LHASA_STANDALONE
-#define TINYGLTF_IMPLEMENTATION
-#include "../coot-utils/tiny_gltf.h"
-#else
-// This must not be here because it will cause a linker error (in Moorhen)
-// #define TINYGLTF_IMPLEMENTATION
-// #include "../coot-utils/tiny_gltf.h"
-// ..and so we have to do a forward declaration
-namespace tinygltf {
-    std::string base64_encode(unsigned char const *bytes_to_encode, unsigned int in_len);
-    std::string base64_decode(std::string const &encoded_string);
-}
-#endif
 
 std::unique_ptr<RDKit::RWMol> lhasa::rdkit_mol_from_smiles(std::string smiles) {
     std::unique_ptr<RDKit::RWMol> ret(RDKit::SmilesToMol(smiles));
@@ -60,7 +48,7 @@ void lhasa::append_from_smiles(CootLigandEditorCanvas& canvas, std::string smile
 }
 
 void lhasa::append_from_pickle_base64(CootLigandEditorCanvas& canvas, std::string base64_pickle_string) {
-    std::string pickle_string = tinygltf::base64_decode(base64_pickle_string);
+    std::string pickle_string = moorhen_base64::base64_decode(base64_pickle_string);
     auto appendee = rdkit_mol_from_pickle(pickle_string);
     auto smiles = rdkit_mol_to_smiles(*appendee.get());
     g_info("Smiles from pickle: %s -> %s", base64_pickle_string.c_str(), smiles.c_str());
