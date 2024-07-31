4b55681e8cc33d0065f6de50e90fba4798094f39
bernhardcl <b.lohkamp@gmail.com>
2024-07-31 09:38:07 +0200

    Fix acedrg run on Windows
    Unset python env variables so that acedrg can find the one from ccp4.

---
+++ compare_versions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+tmp_v1=`grep AC_INIT configure.ac`
+tmp_v2=`grep '<property name="version">' ui/coot-gtk4.ui`
+ver1=$(echo "$tmp_v1" | tr -cd '[:digit:].')
+ver2=$(echo "$tmp_v2" | tr -cd '[:digit:].')
+echo version configure $ver1
+echo version ui file   $ver2
+if [ $ver1 == $ver2 ] ; then
+	echo All good
+else
+	echo damn, versions dont match
+fi
