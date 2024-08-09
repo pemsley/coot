f2f4be40482576615cd921d66be63904087ab255
Jakub Smulski <hgonomeg@gmail.com>
2024-08-09 21:06:34 +0200

    Delete old SMILES display code

--- layla/ui.cpp
+++ layla/ui.cpp
@@ -146,25 +146,19 @@ GtkApplicationWindow* coot::layla::setup_main_window(GtkApplication* app, GtkBui
     }), scale_spinbutton);
 
 
-    GtkBox* smiles_display_box = (GtkBox*) gtk_builder_get_object(builder, "layla_smiles_display_box");
+    GtkGrid* smiles_display_grid = (GtkGrid*) gtk_builder_get_object(builder, "layla_smiles_display_grid");
     g_signal_connect(canvas, "smiles-changed", G_CALLBACK(+[](CootLigandEditorCanvas* self, gpointer user_data){
-        GtkBox* display_box = GTK_BOX(user_data);
-        auto smiles_map = coot_ligand_editor_canvas_get_smiles(self);
-        // todo
-    }), smiles_display_box);
+        GtkGrid* display_grid = GTK_GRID(user_data);
+        // for(auto* i = gtk_widget_get_first_child(GTK_WIDGET(display_grid)); i != nullptr; gtk_widget_get_next_sibling(GTK_WIDGET(display_grid))) {
+        //     gtk_grid_remove(display_grid, i);
+        // }
 
-    // The text view code will be removed soon
-    GtkTextView* smiles_display = (GtkTextView*) gtk_builder_get_object(builder, "layla_smiles_textview");
-    g_signal_connect(canvas, "smiles-changed", G_CALLBACK(+[](CootLigandEditorCanvas* self, gpointer user_data){
-        GtkTextView* view = GTK_TEXT_VIEW(user_data);
         auto smiles_map = coot_ligand_editor_canvas_get_smiles(self);
-        std::string smiles;
         for(const auto& [mol_idx, smiles_code] : smiles_map) {
-            smiles += std::to_string(mol_idx) + ": " + smiles_code + "\n";
+            
+            // todo
         }
-        GtkTextBuffer* buf = gtk_text_view_get_buffer(view);
-        gtk_text_buffer_set_text(buf,smiles.c_str(),-1);
-    }), smiles_display);
+    }), smiles_display_grid);
 
     GtkNotebook* qed_notebook = (GtkNotebook*) gtk_builder_get_object(builder, "layla_qed_notebook");
 
