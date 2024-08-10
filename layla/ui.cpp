175c072db465315bb2b483d5a2dd54fe5ea9e3c1
Jakub Smulski <hgonomeg@gmail.com>
2024-08-10 22:48:03 +0200

    Add styles for smiles display

--- layla/ui.cpp
+++ layla/ui.cpp
@@ -211,7 +211,7 @@ GtkApplicationWindow* coot::layla::setup_main_window(GtkApplication* app, GtkBui
                 gtk_css_provider_load_from_string(provider, ".smiles_label { font-family: monospace; }");
                 GtkStyleContext *context = gtk_widget_get_style_context(GTK_WIDGET(smiles_label));
                 gtk_style_context_add_provider (context, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-                gtk_style_context_add_class (context, "smiles_label");
+                gtk_style_context_add_class (context, "smiles_label");      
 
                 gtk_grid_attach(display_grid, GTK_WIDGET(smiles_label), 1, mol_idx, 1, 1);
             }
