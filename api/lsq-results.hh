ab850d25154b215464909835293e328a5a019029
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-11-05 14:44:27 +0000

    Add documentation for lsq-results.hh

--- api/lsq-results.hh
+++ api/lsq-results.hh
@@ -1,7 +1,12 @@
 
+#include <vector>
+
 class lsq_results_t {
    public:
+   //! the 9-element rotation matrix
+   //! Note to self: what is the ordering?
    std::vector<double> rotation_matrix;
+   //! the translation vector
    std::vector<double> translation;
    bool empty() { return rotation_matrix.empty(); }
 };
