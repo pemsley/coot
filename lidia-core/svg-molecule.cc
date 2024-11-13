d47af3b98c8ecf905deee5c4dfbe621ad768c591
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-11-13 11:33:43 +0000

    Include scaling of coordinates for previous triple-bond edit

--- lidia-core/svg-molecule.cc
+++ lidia-core/svg-molecule.cc
@@ -548,11 +548,11 @@ svg_bond_t::draw_bond(const svg_atom_t &at_1, const svg_atom_t &at_2,
 	 // cairo_stroke(cr);
 
          std::string bond_string;
-         bond_string = make_bond_line_string(p1, p2, bond_colour);
+         bond_string = make_bond_line_string(sc_p1, sc_p2, bond_colour);
          s += bond_string;
-         bond_string = make_bond_line_string(p3, p4, bond_colour);
+         bond_string = make_bond_line_string(sc_p3, sc_p4, bond_colour);
          s += bond_string;
-         bond_string = make_bond_line_string(p5, p6, bond_colour);
+         bond_string = make_bond_line_string(sc_p5, sc_p6, bond_colour);
          s += bond_string;
 
       }
