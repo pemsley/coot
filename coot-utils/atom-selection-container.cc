380dce66c1e80a78e6a8aa64db5fcfd6bcaf730f
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2024-12-17 21:12:24 +0000

    Remove debugging in gemmi-related transfer_links lambda function

--- coot-utils/atom-selection-container.cc
+++ coot-utils/atom-selection-container.cc
@@ -170,8 +170,9 @@ get_atom_selection(std::string pdb_name,
 
       std::vector<mmdb::Link> mmdb_links;
       for (const gemmi::Connection &con : st.connections) {
-         if (true) // debugging
-            std::cout << con.name << " " << con.partner1.str() << " " << con.partner2.str() << std::endl;
+         if (false) // debugging
+            std::cout << "INFO:: gemmi connection: " << con.name
+                      << " " << con.partner1.str() << " " << con.partner2.str() << std::endl;
          mmdb::Link l;
          std::string atom_name = con.partner1.atom_name;
          bool atom_1_is_metal = false;
