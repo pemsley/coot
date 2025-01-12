e4c7f481133d8ee3edaf682c6d122392d6b95f84
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2025-01-12 13:40:25 +0000

    Bring single-bonds-only into dictionary bonds
    Maybe useful for rounded-end-cap bonds

--- geometry/protein-geometry.hh
+++ geometry/protein-geometry.hh
@@ -280,6 +280,11 @@ namespace coot {
       }
       void set_atom_1_atom_id(const std::string &id) { set_atom_id_1(id); }
       void set_atom_2_atom_id(const std::string &id) { set_atom_id_2(id); }
+      // this function called by dictionary parser after all the bonds have been read
+      void set_only_bond(const std::string &pos, bool b) {
+         if (pos == "first")  atom_has_only_this_non_hydrogen_bond_first  = b;
+         if (pos == "second") atom_has_only_this_non_hydrogen_bond_second = b;
+      }
       friend std::ostream& operator<<(std::ostream &s, const dict_bond_restraint_t &rest);
    };
    std::ostream& operator<<(std::ostream &s, const dict_bond_restraint_t &rest);
@@ -584,7 +589,7 @@ namespace coot {
       }
    public:
       enum aromaticity_t { NON_AROMATIC, AROMATIC, UNASSIGNED };
-      enum { IDEAL_MODEL_POS, REAL_MODEL_POS}; 
+      enum { IDEAL_MODEL_POS, REAL_MODEL_POS};
       std::string atom_id;
       std::string atom_id_4c;
       std::string type_symbol;
@@ -721,7 +726,7 @@ namespace coot {
                if (pair_in.at_2 == at_1) {
                   shared_atom = at_1;
                }
-            } 
+            }
             return shared_atom;
          }
       };
@@ -740,7 +745,7 @@ namespace coot {
 
 
       void write_cif_pdbx_chem_comp_descriptor(mmdb::mmcif::Data *data) const;
-      
+
       // imol_enc can be the model molecule number or
       // -1 for all
       // -2 for auto
@@ -1271,7 +1276,7 @@ namespace coot {
             return 0;
       }
    };
-   
+
    class read_refmac_mon_lib_info_t {
    public:
       unsigned int n_atoms;
@@ -1295,7 +1300,7 @@ namespace coot {
    // class protein_geometry     the container class
    // ------------------------------------------------------------------------
    // ------------------------------------------------------------------------
-   // 
+   //
    // consider molecule_geometry
    class protein_geometry {
 
@@ -1638,14 +1643,14 @@ namespace coot {
       // the chiral restraint for this comp_id(s) may need filtering
       // (i.e. removing some of them if they are not real chiral centres
       // (e.g. from prodrg restraints)).
-      // 
+      //
       void filter_chiral_centres(int imol, const std::vector<std::string> & comp_id_for_filtering);
 
       // Return a filtered list, that is don't include chiral centers that
       // are connected to more than one hydrogen.
-      // 
+      //
       std::vector<dict_chiral_restraint_t> filter_chiral_centres(const dictionary_residue_restraints_t &restraints);
-      
+
       void assign_link_chiral_volume_targets();
       int read_number;
 
@@ -1686,9 +1691,9 @@ namespace coot {
       // return empty file name on failure.
       std::string comp_id_to_file_name(const std::string &comp_id) const;
 
-#ifdef HAVE_CCP4SRS      
+#ifdef HAVE_CCP4SRS
       ccp4srs::Manager *ccp4srs;
-#endif      
+#endif
 
       void add_molecule_number_to_entries(const std::vector<std::string> &comp_ids, int imol_enc);
 
@@ -1701,7 +1706,7 @@ namespace coot {
          read_number = 0;
          set_verbose(1);
          parse_metal_NOS_distance_tables();
-#if HAVE_CCP4SRS         
+#if HAVE_CCP4SRS
          ccp4srs = NULL;
 #endif
          fill_default_non_auto_load_residue_names();
@@ -1730,10 +1735,14 @@ namespace coot {
       // for the link_id (and that's fine)
       std::pair<chem_mod, chem_mod> get_chem_mods_for_link(const std::string &link_id) const;
       void debug_mods() const;
-      
-      
+
+      // when making the bonds mesh, we would like to have a mesh that includes a hemisphere
+      // where there are atoms that have only one non-Hydrogen bond. So the dictionary
+      // needs to know where such atoms here. We set them in this function.
+      void set_only_bonds(int dict_idx);
+
       // Refmac monomer lib things
-      // 
+      //
       // Return the number of bond restraints
       //
       read_refmac_mon_lib_info_t
@@ -1755,15 +1764,15 @@ namespace coot {
                r = dict_link_res_restraints[id];
                break;
             }
-         } 
+         }
          return r;
       }
 
       // return "" on comp_id not found, else return the file name.
-      // 
+      //
       std::string get_cif_file_name(const std::string &comp_id,
                                     int imol_enc) const;
-      
+
       int link_size() const { return dict_link_res_restraints.size(); }
       void info() const;
       std::string three_letter_code(const unsigned int &i) const;
@@ -1813,7 +1822,7 @@ namespace coot {
       // as above, except filter out of the returned vectors torsions
       // that move (or are based on) hydrogens.
       // return 2-3 filtered torsions
-      // 
+      //
       std::vector <dict_torsion_restraint_t>
       get_monomer_torsions_from_geometry(const std::string &monomer_type,
                                          int imol_enc,
