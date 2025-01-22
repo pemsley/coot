e505c2e7a4fe6549976b91fb561a7d821543f7ca
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2025-01-22 19:56:59 +0000

    Convert flev to use new-style compose argument for svg

--- pli/flev.cc
+++ pli/flev.cc
@@ -692,12 +692,13 @@ pli::fle_view_with_rdkit_internal(mmdb::Manager *mol,
                   }
 
                   scale_factor = 1.0;
-                  svg_container_t svgc = flev.mol.make_svg(scale_factor, dark_background_flag);
+                  bool add_background = false;
+                  svg_container_t svgc = flev.mol.make_svg(scale_factor, dark_background_flag, add_background);
 
                   if (wrap_in_refresh_html) {
                      // 20250106-PE: hack the bounds for now
                      svgc.set_bounds(-9, -7, 20, 20);
-                     std::string s = svgc.compose();
+                     std::string s = svgc.compose(true);
                      unsigned int refresh_delta = 1;
                      std::string html_top = "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" ";
                      html_top += "content=\"" + std::to_string(refresh_delta);
@@ -707,7 +708,7 @@ pli::fle_view_with_rdkit_internal(mmdb::Manager *mol,
                      s = ss;
                      write_string_to_file(ss, "flev-test-1.svg.html");
                   } else {
-                     write_string_to_file(svgc.compose(), "flev-test-1.svg");
+                     write_string_to_file(svgc.compose(true), "flev-test-1.svg");
                   }
 
                   mmdb::Residue *residue_flat = coot::make_residue(rdkm, mol_2d_depict_conformer, "XXX");
@@ -788,7 +789,7 @@ pli::fle_view_with_rdkit_internal(mmdb::Manager *mol,
                   if (wrap_in_refresh_html) {
                      // 20250106-PE: hack the bounds for now
                      svgc.set_bounds(-20, -20, 40, 40);
-                     std::string s = svgc.compose();
+                     std::string s = svgc.compose(true);
                      unsigned int refresh_delta = 1;
                      std::string html_top = "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" ";
                      html_top += "content=\"" + std::to_string(refresh_delta);
@@ -798,7 +799,7 @@ pli::fle_view_with_rdkit_internal(mmdb::Manager *mol,
                      s = ss;
                      write_string_to_file(ss, "flev-test-all-parts-svg.html");
                   } else {
-                     write_string_to_file(svgc.compose(), "flev-test-all-parts.svg");
+                     write_string_to_file(svgc.compose(true), "flev-test-all-parts.svg");
                   }
 
                   if (output_format == "png") flev.write_png(output_image_file_name);
