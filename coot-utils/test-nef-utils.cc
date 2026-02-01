ea1b3bea3daeed27b5b785bcbac566832834f66b
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2026-02-01 08:25:57 +0000

    Make test-nef-utils print some distance restraints

--- coot-utils/test-nef-utils.cc
+++ coot-utils/test-nef-utils.cc
@@ -1,13 +1,32 @@
 
 #include "nef-utils.hh"
+#include <iostream>
 
 int main(int argc, char **argv) {
 
    int status = 0;
-   if (argc >1) {
-      std::string filepath = argv[0];
-      nef::NEFParser nef_parser = nef::NEFParser::from_file(filepath);
-      status = 1;
+   if (argc > 1) {
+      std::string filepath = argv[1];
+      try {
+         nef::NEFParser nef_parser = nef::NEFParser::from_file(filepath);
+         nef_parser.parse();
+
+         const auto& restraints = nef_parser.restraints();
+         std::cout << "\nDistance restraints (first 10):\n";
+         std::cout << std::string(70, '-') << "\n";
+         size_t count = 0;
+         for (const auto& r : restraints) {
+            if (count++ >= 10) break;
+            std::cout << r.to_string() << "\n";
+         }
+         std::cout << "\nTotal distance restraints: " << restraints.size() << "\n";
+         status = 1;
+      } catch (const std::exception& e) {
+         std::cerr << "Error: " << e.what() << "\n";
+         status = 0;
+      }
+   } else {
+      std::cerr << "Usage: " << argv[0] << " <nef_file>\n";
    }
    return status;
 }
