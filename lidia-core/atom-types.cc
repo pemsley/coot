f20abbdcc24e2447a030f17d42b8bbe838993591
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2026-02-07 12:05:16 +0000

    Whitespace clean-up and remove debugging atom-types.cc

--- lidia-core/atom-types.cc
+++ lidia-core/atom-types.cc
@@ -39,9 +39,9 @@ coot::set_energy_lib_atom_types(RDKit::ROMol *mol) {
       unsigned int idx_0;
       unsigned int idx_1;
       idx_pair(unsigned int idx_0_in,
-	       unsigned int idx_1_in) :
-	 idx_0(idx_0_in),
-	 idx_1(idx_1_in) {}
+               unsigned int idx_1_in) :
+         idx_0(idx_0_in),
+         idx_1(idx_1_in) {}
    };
    class atom_typing {
    public:
@@ -50,19 +50,19 @@ coot::set_energy_lib_atom_types(RDKit::ROMol *mol) {
       std::string smarts_string;
       atom_typing() {}
       atom_typing(const std::string &energy_type_in,
-		  const std::string &smarts_string_in,
-		  unsigned int atom_index_in) :
-	 energy_type(energy_type_in),
-	 smarts_string(smarts_string_in) {
-	 atom_index.push_back(atom_index_in);
+                  const std::string &smarts_string_in,
+                  unsigned int atom_index_in) :
+         energy_type(energy_type_in),
+         smarts_string(smarts_string_in) {
+         atom_index.push_back(atom_index_in);
       }
       atom_typing(const std::string &energy_type_in,
-		  const std::string &smarts_string_in,
-		  idx_pair atom_indices_in) :
-	 energy_type(energy_type_in),
-	 smarts_string(smarts_string_in) {
-	 atom_index.push_back(atom_indices_in.idx_0);
-	 atom_index.push_back(atom_indices_in.idx_1);
+                  const std::string &smarts_string_in,
+                  idx_pair atom_indices_in) :
+         energy_type(energy_type_in),
+         smarts_string(smarts_string_in) {
+         atom_index.push_back(atom_indices_in.idx_0);
+         atom_index.push_back(atom_indices_in.idx_1);
       }
    };
 
@@ -253,8 +253,8 @@ coot::set_energy_lib_atom_types(RDKit::ROMol *mol) {
       ele_smarts[i] = atom_typing(util::upcase(eles[i]), "["+eles[i]+"]", 0);
 
    smarts_list.insert(smarts_list.end(),
-		      ele_smarts.begin(),
-		      ele_smarts.end());
+                      ele_smarts.begin(),
+                      ele_smarts.end());
 
    for (std::size_t ism=0; ism<smarts_list.size(); ism++) {
       const atom_typing &smarts_type = smarts_list[ism];
@@ -267,30 +267,30 @@ coot::set_energy_lib_atom_types(RDKit::ROMol *mol) {
       // int matched = false;
       //
       if (n_matched > 0) {
-	 for (unsigned int im=0; im<matches.size(); im++) {
-	    for (std::size_t j=0; j<smarts_type.atom_index.size(); j++) {
-	       unsigned int match_atom_index = smarts_type.atom_index[j];
-	       unsigned int idx_this_atom_1 = matches[im][match_atom_index].first;
-	       unsigned int idx_this_atom_2 = matches[im][match_atom_index].second;
-	       if (true)
-		  std::cout << "query " << smarts_list[ism].smarts_string
-			    << " matches idx pair " << idx_this_atom_1 << " " << idx_this_atom_2
-			    << " " << smarts_type.energy_type
-			    << std::endl;
-	       RDKit::Atom *at_p = mol->getAtomWithIdx(idx_this_atom_2);
-	       // if this atom has a energy_type already, pass, else set it
-	       try {
-		  std::string e;
-		  at_p->getProp("type_energy", e);
-		  std::cout << "already has type_energy \"" << e << "\""<< std::endl;
-	       }
-	       catch (const KeyErrorException &e) {
-		  std::cout << "setting type_energy " << smarts_type.energy_type
-		            << " for atom " << idx_this_atom_2 << std::endl;
-		  at_p->setProp("type_energy", smarts_type.energy_type);
-	       }
-	    }
-	 }
+         for (unsigned int im=0; im<matches.size(); im++) {
+            for (std::size_t j=0; j<smarts_type.atom_index.size(); j++) {
+               unsigned int match_atom_index = smarts_type.atom_index[j];
+               unsigned int idx_this_atom_1 = matches[im][match_atom_index].first;
+               unsigned int idx_this_atom_2 = matches[im][match_atom_index].second;
+               if (false)
+                  std::cout << "query " << smarts_list[ism].smarts_string
+                            << " matches idx pair " << idx_this_atom_1 << " " << idx_this_atom_2
+                            << " " << smarts_type.energy_type
+                            << std::endl;
+               RDKit::Atom *at_p = mol->getAtomWithIdx(idx_this_atom_2);
+               // if this atom has a energy_type already, pass, else set it
+               try {
+                  std::string e;
+                  at_p->getProp("type_energy", e);
+                  std::cout << "already has type_energy \"" << e << "\""<< std::endl;
+               }
+               catch (const KeyErrorException &e) {
+                  std::cout << "setting type_energy " << smarts_type.energy_type
+                            << " for atom " << idx_this_atom_2 << std::endl;
+                  at_p->setProp("type_energy", smarts_type.energy_type);
+               }
+            }
+         }
       }
    }
 }
@@ -301,34 +301,34 @@ coot::set_energy_lib_atom_types(RDKit::ROMol *mol) {
 //
 void
 coot::set_dictionary_atom_types_from_mol(dictionary_residue_restraints_t *dictionary,
-					 const RDKit::ROMol *mol) {
+                                         const RDKit::ROMol *mol) {
 
    unsigned int n_mol_atoms = mol->getNumAtoms();
 
    if (false)
       std::cout << "here in set_dictionary_atom_types_from_mol() with "
-		<< n_mol_atoms << " atoms" << std::endl;
+                << n_mol_atoms << " atoms" << std::endl;
 
    for (unsigned int iat=0; iat<n_mol_atoms; iat++) {
       const RDKit::Atom *at_p = mol->getAtomWithIdx(iat);
       try {
-	 std::string type;
-	 std::string atom_id;
-	 at_p->getProp("type_energy", type);
-	 // std::cout << "debug:: here with type " << type << std::endl;
-	 // at_p->getProp("atom_id", atom_id);
-	 at_p->getProp("name", atom_id); // should be atom_id
-	 for (std::size_t j=0; j<dictionary->atom_info.size(); j++) {
-	    if (dictionary->atom_info[j].atom_id_4c == atom_id) {
-	       dictionary->atom_info[j].type_energy = type;
-	       // std::cout << "debug:: for atom " << atom_id << " added type " << type << std::endl;
-	       break;
-	    }
-	 }
+         std::string type;
+         std::string atom_id;
+         at_p->getProp("type_energy", type);
+         // std::cout << "debug:: here with type " << type << std::endl;
+         // at_p->getProp("atom_id", atom_id);
+         at_p->getProp("name", atom_id); // should be atom_id
+         for (std::size_t j=0; j<dictionary->atom_info.size(); j++) {
+            if (dictionary->atom_info[j].atom_id_4c == atom_id) {
+               dictionary->atom_info[j].type_energy = type;
+               // std::cout << "debug:: for atom " << atom_id << " added type " << type << std::endl;
+               break;
+            }
+         }
       }
       catch (const KeyErrorException &ke) {
-	 std::cout << "WARNING:: " << ke.what() << " in set_dictionary_atom_types_from_mol() for atom index "
-		   << iat << std::endl;
+         std::cout << "WARNING:: " << ke.what() << " in set_dictionary_atom_types_from_mol() for atom index "
+                   << iat << std::endl;
       }
    }
 }
