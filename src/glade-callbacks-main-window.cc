c93fb68e7e2ed484bbacfef8211371615c8a37a6
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2026-02-09 17:10:34 +0000

    Better handling for the case where the new monomer type does not exist in the dictionary for on_add_PTM_apply_button_clicked()

--- src/glade-callbacks-main-window.cc
+++ src/glade-callbacks-main-window.cc
@@ -643,6 +643,10 @@ void on_generic_overlay_frame_cancel_button_clicked(GtkButton       *button,
    }
 }
 
+// 20260209-PE this is not in a header (yet)
+void get_monomer_dictionary_in_subthread(const std::string &comp_id,
+					 bool run_get_monomer_post_fetch_flag);
+
 extern "C" G_MODULE_EXPORT
 void on_add_PTM_apply_button_clicked(GtkButton *button,
                                      gpointer user_data) {
@@ -660,10 +664,20 @@ void on_add_PTM_apply_button_clicked(GtkButton *button,
                const char *id = gtk_combo_box_get_active_id(GTK_COMBO_BOX(combobox));
                if (id) {
                   std::string new_residue_type(id);
-                  std::string chain_id = residue_p->GetChainID();
-                  int res_no = residue_p->GetSeqNum();
-                  g.molecules[imol].mutate_by_overlap(chain_id, res_no, new_residue_type);
-                  g.graphics_draw();
+                  bool new_residue_type_exists = false;
+                  int imol_enc_any = coot::protein_geometry::IMOL_ENC_ANY;
+                  std::pair<bool, std::string> mn = g.Geom_p()->get_monomer_name(new_residue_type, imol_enc_any);
+                  if (mn.first)
+                     new_residue_type_exists = true;
+                  if (new_residue_type_exists) {
+                     std::string chain_id = residue_p->GetChainID();
+                     int res_no = residue_p->GetSeqNum();
+                     g.molecules[imol].mutate_by_overlap(chain_id, res_no, new_residue_type);
+                     g.graphics_draw();
+                  } else {
+                     get_monomer_dictionary_in_subthread(new_residue_type, false);
+                     ephemeral_overlay_label("Fetching dictionary - try again");
+                  }
                }
             }
          }
