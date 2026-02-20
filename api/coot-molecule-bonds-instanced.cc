ce07f4474fc673d65145867dffb70a4304cec5c4
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2026-02-20 21:48:41 +0000

    Make the empty thermal elllipsoids more chunky

--- api/coot-molecule-bonds-instanced.cc
+++ api/coot-molecule-bonds-instanced.cc
@@ -188,7 +188,7 @@ make_instanced_graphical_bonds_spherical_atoms(coot::instanced_mesh_t &m, // add
    // 2026-02-04-PE I added some logic to change the width for the case where we
    // are *only* drawing the ellipsoid bands.
    float ellipsoid_band_thickness = 0.02f; // default, black.
-   if (render_aniso_atoms_as_empty) ellipsoid_band_thickness = 0.01;
+   if (render_aniso_atoms_as_empty) ellipsoid_band_thickness = 0.04;
    cylinder cylinder_for_bands(std::make_pair(glm::vec3(0.0f, 0.0f,  ellipsoid_band_thickness),
                                               glm::vec3(0.0f, 0.0f, -ellipsoid_band_thickness)),
                                1.0f, 1.0f, 2.0f * ellipsoid_band_thickness, 32, 2);
