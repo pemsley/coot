4defd70ac27e3226e8c39fd94117f1c468159c88
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2026-03-05 10:48:13 +0000

    Reduce the step size of front clipping plane change
    in perspective mode.

--- src/graphics-info-opengl.cc
+++ src/graphics-info-opengl.cc
@@ -547,8 +547,8 @@ graphics_info_t::increase_clipping_front() {
 
    if (perspective_projection_flag) {
       double l = eye_position.z;
-      float screen_z_near_perspective_limit = l * 0.99;
-      float v = screen_z_near_perspective * 1.05263;
+      float screen_z_near_perspective_limit = l * 0.995;
+      float v = screen_z_near_perspective * 1.02;
       if (v < screen_z_near_perspective_limit) {
          if (v > 2.0)
             screen_z_near_perspective = v;
@@ -584,8 +584,8 @@ graphics_info_t::decrease_clipping_front() {
 
    if (perspective_projection_flag) {
       double l = eye_position.z;
-      float screen_z_near_perspective_limit = l * 0.99;
-      float v = screen_z_near_perspective * 0.95;
+      float screen_z_near_perspective_limit = l * 0.995;
+      float v = screen_z_near_perspective * 0.98;
       if (v < screen_z_near_perspective_limit) {
          if (v > 2.0) {
             screen_z_near_perspective = v;
