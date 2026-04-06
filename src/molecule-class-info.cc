a1a7d70058ac4f71a90e45270236c760b43f357c
Paul Emsley <pemsley@mrc-lmb.cam.ac.uk>
2026-04-06 03:11:49 +0100

    Fix atom indexing error in update_extra_restraints_representation_geman_mcclure()

--- src/molecule-class-info.cc
+++ src/molecule-class-info.cc
@@ -5146,10 +5146,10 @@ molecule_class_info_t::update_extra_restraints_representation_geman_mcclure() {
          }
       }
       if (! ifound_2) {
-         int idx = full_atom_spec_to_atom_index(rest.atom_1);
+         int idx = full_atom_spec_to_atom_index(rest.atom_2);
          if (idx != -1) {
             if (idx < atom_sel.n_selected_atoms) {
-               at_1 = atom_sel.atom_selection[idx];
+               at_2 = atom_sel.atom_selection[idx];
                if (rest.atom_2.matches_spec(at_2)) {
                   p2 = clipper::Coord_orth(at_2->x, at_2->y, at_2->z);
                   ifound_2 = true;
