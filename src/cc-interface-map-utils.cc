17c6a56cb5f59132eff5325a258a687d2fd2b871
Yoshitaka Moriwaki <virgospica93@gmail.com>
2026-08-12 09:22:41 +0900

    Fix memory allocation for em_placement_data_t in emplacement_by_phaser function

--- src/cc-interface-map-utils.cc
+++ src/cc-interface-map-utils.cc
@@ -881,7 +881,7 @@ void emplacement_by_phaser(const std::string &half_map_1_file_name, const std::s
       std::cout << "WARNING:: " << em_placement_output_file_name << " exists and can't be moved"<< std::endl;
    } else {
       if (is_valid_model_molecule(imol_model)) {
-         em_placement_data_t *data_p = new em_placement_data_t(em_placement_output_file_name, 0);
+         em_placement_data_t *data_p = new em_placement_data_t{em_placement_output_file_name};
          std::string model_file_name = "input-model-for-emplacement.pdb";
          write_pdb_file(imol_model, model_file_name.c_str());
          std::thread thread(run_subprocess, half_map_1_file_name, half_map_2_file_name, model_file_name, search_centre);
